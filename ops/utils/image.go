a65ec810d6d03955f0fe34df40543db50e0e4b62
aalu1418 <aalu1418@gmail.com>
2021-12-10 11:02:06 -0500

    additional logging to pulling + building images

--- ops/utils/image.go
+++ ops/utils/image.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+
 	"github.com/pulumi/pulumi-docker/sdk/v3/go/docker"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -15,17 +17,19 @@ type Image struct {
 
 // Pull retrieves the specified container image
 func (i *Image) Pull(ctx *pulumi.Context) error {
+	msg := LogStatus(fmt.Sprintf("Pulling %s", i.Name))
 	img, err := docker.NewRemoteImage(ctx, i.Name, &docker.RemoteImageArgs{
 		Name:        pulumi.String(i.Tag),
 		KeepLocally: pulumi.BoolPtr(true),
 	})
 	i.Img = img
-	return err
+	return msg.Check(err)
 }
 
 // Build the image for the specified dockerfile in the YAML config
 func (i *Image) Build(ctx *pulumi.Context, context string, dockerfile string) error {
 	// build local image
+	msg := LogStatus(fmt.Sprintf("Building %s", i.Name))
 	img, err := docker.NewImage(ctx, i.Name, &docker.ImageArgs{
 		ImageName: pulumi.String(i.Tag),
 		// LocalImageName: pulumi.String(i.Tag),
@@ -37,5 +41,5 @@ func (i *Image) Build(ctx *pulumi.Context, context string, dockerfile string) er
 		},
 	})
 	i.Local = img
-	return err
+	return msg.Check(err)
 }

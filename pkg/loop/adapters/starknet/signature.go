30d787a100873a9a4f55dec5891e3c6b2d0b7148
krehermann <keith.rehermann@protonmail.com>
2023-05-30 16:28:21 -0600

    add starknet signature proto messages and wrappers for d/encoding

--- pkg/loop/adapters/starknet/signature.go
+++ pkg/loop/adapters/starknet/signature.go
@@ -1,10 +1,10 @@
 package starknet
 
 import (
-	"bytes"
 	"fmt"
 	"math/big"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/smartcontractkit/chainlink-relay/pkg/loop/internal/pb"
 )
 
@@ -14,57 +14,33 @@ const (
 )
 
 type Signature struct {
-	x, y *pb.BigInt
+	sig *pb.StarknetSignature
 }
 
-// encodes starknet doublet of big.Ints into []byte slice
-// the first 32 bytes are the padded bytes of x
-// the second 32 bytes are the padded bytes of y
 func (s *Signature) Bytes() ([]byte, error) {
-	buf := new(bytes.Buffer)
-	n, err := buf.Write(padBytes(s.x.Int().Bytes(), maxPointByteLen))
-	if err != nil {
-		return nil, fmt.Errorf("error writing 'x' component of signature: %w", err)
-	}
-	if n != maxPointByteLen {
-		return nil, fmt.Errorf("unexpected write length of 'x' component of signature: wrote %d expected %d", n, maxPointByteLen)
-	}
+	return proto.Marshal(s.sig)
 
-	n, err = buf.Write(padBytes(s.y.Int().Bytes(), maxPointByteLen))
-	if err != nil {
-		return nil, fmt.Errorf("error writing 'y' component of signature: %w", err)
-	}
-	if n != maxPointByteLen {
-		return nil, fmt.Errorf("unexpected write length of 'y' component of signature: wrote %d expected %d", n, maxPointByteLen)
-	}
-
-	if buf.Len() != signatureLen {
-		return nil, fmt.Errorf("error in signature length")
-	}
-	return buf.Bytes(), nil
 }
 
 func (s *Signature) Ints() (x *big.Int, y *big.Int, err error) {
-	if s.x == nil || s.y == nil {
+	if s.sig == nil {
 		return nil, nil, fmt.Errorf("signature uninitialized")
 	}
 
-	return s.x.Int(), s.y.Int(), nil
+	return s.sig.X.Int(), s.sig.Y.Int(), nil
 }
 
 // b is expected to encode x,y components in accordance with [signature.Bytes]
 func SignatureFromBytes(b []byte) (*Signature, error) {
-
-	if len(b) != signatureLen {
-		return nil, fmt.Errorf("expected signature length %d got %d", signatureLen, len(b))
+	starkPb := &pb.StarknetSignature{}
+	err := proto.Unmarshal(b, starkPb)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling bytes to signature: %w", err)
 	}
 
-	x := b[:maxPointByteLen]
-	y := b[maxPointByteLen:]
-
-	return SignatureFromBigInts(
-		new(big.Int).SetBytes(x),
-		new(big.Int).SetBytes(y))
+	return &Signature{
+		sig: starkPb,
+	}, nil
 }
 
 // x,y must be non-negative numbers
@@ -74,20 +50,12 @@ func SignatureFromBigInts(x *big.Int, y *big.Int) (*Signature, error) {
 		return nil, fmt.Errorf("Cannot create signature from negative values (x,y), (%v, %v)", x, y)
 	}
 
+	starkPb := &pb.StarknetSignature{
+		X: pb.NewBigIntFromInt(x),
+		Y: pb.NewBigIntFromInt(y),
+	}
 	return &Signature{
-		x: pb.NewBigIntFromInt(x),
-		y: pb.NewBigIntFromInt(y),
+		sig: starkPb,
 	}, nil
 
-}
-
-// pad bytes to specific length
-func padBytes(a []byte, length int) []byte {
-	if len(a) < length {
-		pad := make([]byte, length-len(a))
-		return append(pad, a...)
-	}
-
-	// return original if length is >= to specified length
-	return a
 }
--- pkg/loop/adapters/starknet/signature_test.go
+++ pkg/loop/adapters/starknet/signature_test.go
@@ -14,18 +14,21 @@ func TestSignature(t *testing.T) {
 	require.NoError(t, err)
 
 	x, y, err := s.Ints()
+	require.NoError(t, err)
 	require.Equal(t, big.NewInt(7), x)
 	require.Equal(t, big.NewInt(11), y)
 
 	b, err := s.Bytes()
 	require.NoError(t, err)
 	require.NotNil(t, b)
-	require.Len(t, b, signatureLen)
 
 	roundTrip, err := SignatureFromBytes(b)
 	require.NoError(t, err)
-	require.Equal(t, s.x, roundTrip.x)
-	require.Equal(t, s.y, roundTrip.y)
+	// compare fields because comparing proto objects directly gave incorrect failures
+	require.EqualValues(t, s.sig.X.GetValue(), roundTrip.sig.X.GetValue())
+	require.EqualValues(t, s.sig.X.GetNegative(), roundTrip.sig.X.GetNegative())
+	require.EqualValues(t, s.sig.Y.GetValue(), roundTrip.sig.Y.GetValue())
+	require.EqualValues(t, s.sig.Y.GetNegative(), roundTrip.sig.Y.GetNegative())
 
 	// no negative allowed
 	s, err = SignatureFromBigInts(big.NewInt(-7),

c9cd7710bc047aeffa06a5e2187496d65e9949cf
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2023-07-31 23:57:03 +0600

    Fees test

--- pkg/reportingplugins/mercury/v1/fees_test.go
+++ pkg/reportingplugins/mercury/v1/fees_test.go
@@ -1,7 +1,17 @@
 package mercury_v1
 
-import "testing"
+import (
+	"math/big"
+	"testing"
+)
 
 func Test_Fees(t *testing.T) {
-	t.Skip("TODO")
+	t.Run("CalculateFee", func(t *testing.T) {
+		tokenPriceInUSD := big.NewInt(655000000)
+		var baseUSDFeeCents uint32 = 100
+		fee := CalculateFee(tokenPriceInUSD, baseUSDFeeCents)
+		if fee.Cmp(big.NewInt(6.55e18)) != 0 {
+			t.Errorf("Expected fee to be 6550000000000000000, got %v", fee)
+		}
+	})
 }

121badd77c9c7951bf3cf4cde63d6635e2dc5f5c
krehermann <keith.rehermann@protonmail.com>
2023-08-24 16:14:11 -0600

    temporary work to get go workspace in order

--- pkg/loop/internal/types.go
+++ pkg/loop/internal/types.go
@@ -20,6 +20,12 @@ type Relayer interface {
 	NewMercuryProvider(context.Context, types.RelayArgs, types.PluginArgs) (types.MercuryProvider, error)
 	NewFunctionsProvider(context.Context, types.RelayArgs, types.PluginArgs) (types.FunctionsProvider, error)
 
+	GetChainStatus(ctx context.Context) (types.ChainStatus, error)
+	ListNodeStatuses(ctx context.Context, page_size int32, page_token string) (stats []types.NodeStatus, next_page_token string, err error)
+	// choose different name than SendTx to avoid collison during refactor.
+	Transact(ctx context.Context, from, to string, amount *big.Int, balanceCheck bool) error
+
+	// TODO remove in BCF-2441
 	ChainStatus(ctx context.Context, id string) (types.ChainStatus, error)
 	ChainStatuses(ctx context.Context, offset, limit int) (chains []types.ChainStatus, count int, err error)
 

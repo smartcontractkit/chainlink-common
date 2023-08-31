f489929cb40931643176e112d2e79b115faa6b58
krehermann <keith.rehermann@protonmail.com>
2023-08-31 13:13:27 -0600

    cleanup

--- pkg/loop/internal/types.go
+++ pkg/loop/internal/types.go
@@ -18,18 +18,4 @@ type Relayer interface {
 	NewMedianProvider(context.Context, types.RelayArgs, types.PluginArgs) (types.MedianProvider, error)
 	NewMercuryProvider(context.Context, types.RelayArgs, types.PluginArgs) (types.MercuryProvider, error)
 	NewFunctionsProvider(context.Context, types.RelayArgs, types.PluginArgs) (types.FunctionsProvider, error)
-	/*
-			GetChainStatus(ctx context.Context) (types.ChainStatus, error)
-			ListNodeStatuses(ctx context.Context, page_size int32, page_token string) (stats []types.NodeStatus, next_page_token string, err error)
-			// choose different name than SendTx to avoid collison during refactor.
-			Transact(ctx context.Context, from, to string, amount *big.Int, balanceCheck bool) error
-
-		// TODO remove in BCF-2441
-		ChainStatus(ctx context.Context, id string) (types.ChainStatus, error)
-		ChainStatuses(ctx context.Context, offset, limit int) (chains []types.ChainStatus, count int, err error)
-
-		NodeStatuses(ctx context.Context, offset, limit int, chainIDs ...string) (nodes []types.NodeStatus, count int, err error)
-
-		SendTx(ctx context.Context, chainID, from, to string, amount *big.Int, balanceCheck bool) error
-	*/
 }

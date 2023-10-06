411d16b0aa05459cc69ee1a3822e032545998ea8
george-dorin <george.dorin@smartcontract.com>
2023-10-06 15:25:27 +0300

    Remove MonitoringEndpointGenerator from telem interface

--- pkg/types/telemetry.go
+++ pkg/types/telemetry.go
@@ -8,7 +8,6 @@ import (
 
 type Telemetry interface {
 	Send(ctx context.Context, contractID string, telemetryType string, network string, chainID string, payload []byte) error
-	GenMonitoringEndpoint(contractID string, telemType string, network string, chainID string) commontypes.MonitoringEndpoint
 }
 
 // MonitoringEndpointGenerator almost identical to synchronization.MonitoringEndpointGenerator except for the telemetry type being string after https://github.com/smartcontractkit/chainlink/pull/10623 gets merged

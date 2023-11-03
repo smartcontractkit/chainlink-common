75137b13dff145e3fb27bad5cdd35f6d6bebd7dd
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2023-11-02 21:03:10 -0700

    Remove commented EventQuery methods from ChainReader
    We can add these back later once we're ready to use ChainReader with more than just Median

--- pkg/types/chain_reader.go
+++ pkg/types/chain_reader.go
@@ -5,25 +5,6 @@ import (
 	"time"
 )
 
-/*
-type BigInt big.Int
-type String string
-
-// Union of BigInt & String
-type BlockID interface {
-	bi() *BigInt
-	s() String
-}
-
-func (b *BigInt) bi() *BigInt {
-	return b
-}
-
-func (b String) s() String {
-	return b
-}
-*/
-
 type ErrorChainReaderUnsupported struct{}
 
 func (e ErrorChainReaderUnsupported) Error() string {
@@ -73,12 +54,6 @@ func (NotASliceError) Error() string {
 }
 
 type ChainReader interface {
-	//RegisterEventFilter(ctx context.Context, filterName string, filter EventFilter, startingBlock BlockID) error
-	//UnregisterEventFilter(ctx context.Context, filterName string) error
-	//QueryEvents(ctx context.Context, query EventQuery) ([]Event, error)
-	// The params argument of GetLatestValue() can be any object which maps a set of generic parameters into chain specific parameters defined in RelayConfig. It must be something which can be passed into
-	// json.Marshal(). Typically would be either an anonymous map such as `map[string]any{"baz": 42, "test": true}}` or something which implements the `MarshalJSON()` method (satisfying `Marshaller` interface).
-	//
 	// returnVal should satisfy Marshaller interface.
 	GetLatestValue(ctx context.Context, bc BoundContract, method string, params, returnVal any) error
 	Codec
@@ -108,10 +83,3 @@ type EventFilter struct {
 	KeysList    [][]string // 2D list of indexed search keys, outer dim = AND, inner dim = OR.  Params[0] is the name of the event (or "event type"), rest are any narrowing parameters that may help further specify the event
 	Retention   time.Duration
 }
-
-//type EventQuery struct {
-//	FromBlock BlockID
-//	ToBlock   BlockID
-//	Filter    EventFilter
-//	Pending   bool
-//}

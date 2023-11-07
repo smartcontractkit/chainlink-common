ee31659d24ed0eb8a38e9b0d969edfac26354347
george-dorin <george.dorin@smartcontract.com>
2023-11-07 17:47:14 +0200

    Initial draft

--- pkg/loop/internal/relayer.go
+++ pkg/loop/internal/relayer.go
@@ -447,3 +447,12 @@ func healthReport(s map[string]string) (hr map[string]error) {
 	}
 	return hr
 }
+
+// RegisterStandAloneProvider register the servers needed for a plugin provider,
+// this is a workaround to test the Node API on EVM until the EVM relayer is loopifyed
+func RegisterStandAloneProvider(s *grpc.Server, p types.PluginProvider) {
+	pb.RegisterServiceServer(s, &serviceServer{srv: p})
+	pb.RegisterOffchainConfigDigesterServer(s, &offchainConfigDigesterServer{impl: p.OffchainConfigDigester()})
+	pb.RegisterContractConfigTrackerServer(s, &contractConfigTrackerServer{impl: p.ContractConfigTracker()})
+	pb.RegisterContractTransmitterServer(s, &contractTransmitterServer{impl: p.ContractTransmitter()})
+}

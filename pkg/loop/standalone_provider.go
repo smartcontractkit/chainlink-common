12644f22e9a53bc10d74aa774ee9b48fa2d24bf4
george-dorin <george.dorin@smartcontract.com>
2023-11-08 16:24:36 +0200

    Add tests

--- pkg/loop/standalone_provider.go
+++ pkg/loop/standalone_provider.go
@@ -11,13 +11,13 @@ import (
 )
 
 // RegisterStandAloneProvider register the servers needed for a plugin provider,
-// this is a workaround to test the Node API on EVM until the EVM relayer is loopifyed
+// this is a workaround to test the Node API medianpoc on EVM until the EVM relayer is loopifyed
 func RegisterStandAloneProvider(s *grpc.Server, p types.PluginProvider, pType types.OCR2PluginType) error {
 	switch pType {
 	case types.Median:
 		mp, ok := p.(types.MedianProvider)
 		if !ok {
-			return errors.New(fmt.Sprintf("expected median provider got %t", p))
+			return errors.New(fmt.Sprintf("expected median provider got %T", p))
 		}
 		internal.RegisterStandAloneMedianProvider(s, mp)
 		return nil

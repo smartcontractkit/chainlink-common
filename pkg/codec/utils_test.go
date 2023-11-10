aac74ed480dc636d905e7d58fb7c10f92b2179cf
Ryan Tinianov <tinianov@live.com>
2023-11-10 11:02:31 -0500

    Add helper methods to merge fields from arrays and to split fields to arrays, to be used by encoders and decoders that are passed arrays instead of an object.  Functions does this for encoding and decoding

--- pkg/codec/utils_test.go
+++ pkg/codec/utils_test.go
@@ -6,8 +6,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink-relay/pkg/codec"
+	"github.com/smartcontractkit/chainlink-relay/pkg/types"
 )
 
 func TestFitsInNBitsSigned(t *testing.T) {
@@ -25,5 +27,89 @@ func TestFitsInNBitsSigned(t *testing.T) {
 	t.Run("Too small", func(t *testing.T) {
 		bi := big.NewInt(math.MinInt16 - 1)
 		assert.False(t, codec.FitsInNBitsSigned(16, bi))
+	})
+}
+
+func TestMergeValueFields(t *testing.T) {
+	t.Parallel()
+	t.Run("Merges fields", func(t *testing.T) {
+		input := []map[string]any{
+			{"Foo": int32(1), "Bar": "Hi"},
+			{"Foo": int32(2), "Bar": "How"},
+			{"Foo": int32(3), "Bar": "Are"},
+			{"Foo": int32(4), "Bar": "You?"},
+		}
+
+		output, err := codec.MergeValueFields(input)
+		require.NoError(t, err)
+
+		expected := map[string]any{
+			"Foo": []int32{1, 2, 3, 4},
+			"Bar": []string{"Hi", "How", "Are", "You?"},
+		}
+		assert.Equal(t, expected, output)
+	})
+
+	t.Run("Returns error if keys are not the same", func(t *testing.T) {
+		input := []map[string]any{
+			{"Foo": int32(1), "Bar": "Hi"},
+			{"Zap": 2, "Foo": int32(2), "Bar": "How"},
+		}
+
+		_, err := codec.MergeValueFields(input)
+
+		assert.IsType(t, types.InvalidTypeError{}, err)
+	})
+
+	t.Run("Returns error if values are not compatible types", func(t *testing.T) {
+		input := []map[string]any{
+			{"Foo": int32(1), "Bar": "Hi"},
+			{"Foo": int32(2), "Bar": int32(3)},
+		}
+
+		_, err := codec.MergeValueFields(input)
+
+		assert.IsType(t, types.InvalidTypeError{}, err)
+	})
+}
+
+func TestSplitValueField(t *testing.T) {
+	t.Parallel()
+	t.Run("Returns slit field values", func(t *testing.T) {
+		input := map[string]any{
+			"Foo": []int32{1, 2, 3, 4},
+			"Bar": [4]string{"Hi", "How", "Are", "You?"},
+		}
+
+		output, err := codec.SplitValueFields(input)
+		require.NoError(t, err)
+
+		expected := []map[string]any{
+			{"Foo": int32(1), "Bar": "Hi"},
+			{"Foo": int32(2), "Bar": "How"},
+			{"Foo": int32(3), "Bar": "Are"},
+			{"Foo": int32(4), "Bar": "You?"},
+		}
+		assert.Equal(t, expected, output)
+	})
+
+	t.Run("Returns error if lengths do not match", func(t *testing.T) {
+		input := map[string]any{
+			"Foo": []int32{1, 2, 3},
+			"Bar": []string{"Hi", "How", "Are", "You?"},
+		}
+
+		_, err := codec.SplitValueFields(input)
+		assert.IsType(t, types.InvalidTypeError{}, err)
+	})
+
+	t.Run("Returns error if item is not an array or slice", func(t *testing.T) {
+		input := map[string]any{
+			"Foo": int32(3),
+			"Bar": []string{"Hi", "How", "Are", "You?"},
+		}
+
+		_, err := codec.SplitValueFields(input)
+		assert.IsType(t, types.NotASliceError{}, err)
 	})
 }

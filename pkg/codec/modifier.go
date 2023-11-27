3c45666610f736540118f1ed5b27beeddad5fce2
Ryan Tinianov <tinianov@live.com>
2023-11-27 14:30:15 -0500

    Create a modifier interface and a modifier for hard-coding values

---
+++ pkg/codec/modifier.go
@@ -0,0 +1,17 @@
+package codec
+
+import (
+	"reflect"
+)
+
+type Modifier interface {
+	RetypeForOffChain(onChainType reflect.Type) (reflect.Type, error)
+
+	// TransformForOnChain transforms a type returned from AdjustForInput into the outputType.
+	// You may also pass a pointer to the type returned by AdjustForInput to get a pointer to outputType.
+	TransformForOnChain(offChainValue any) (any, error)
+
+	// TransformForOffChain is the reverse of renameTransform input.
+	// It is used to send back the object after it has been decoded
+	TransformForOffChain(onChainValue any) (any, error)
+}

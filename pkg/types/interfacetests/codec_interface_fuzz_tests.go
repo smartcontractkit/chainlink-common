0d13e4669960894ac3abb7284bd8c3394b3c7799
Ryan Tinianov <tinianov@live.com>
2024-01-14 15:12:23 -0500

    Add fuzzing for decoding random bytes

--- pkg/types/interfacetests/codec_interface_fuzz_tests.go
+++ pkg/types/interfacetests/codec_interface_fuzz_tests.go
@@ -12,9 +12,12 @@ import (
 
 func RunCodecInterfaceFuzzTests(f *testing.F, tester CodecInterfaceTester) {
 	// A flattening of the TestStruct, replacing the Account/Accounts with just an int that can be used to make an account
-	f.Add(int32(0), "foo", uint8(0), []byte{} /*[]int8 is not supported, byte is the same size*/, 0, 1, int64(1), []byte{} /*fixed size isn't allowed*/, 1, "foo")
-	f.Fuzz(func(t *testing.T, field int32, differentField string, oracleId uint8, oracleIds []byte, accountSeed, accountsSeed int, bigField int64, fixedBytes []byte, i int, s string) {
-		t.Run("Encode and decode produce the same result", func(t *testing.T) {
+	// And an extra []byte to test decoding on its own.
+	f.Add(int32(0), "foo", uint8(0), []byte{} /*[]int8 is not supported, byte is the same size*/, 0, 1, int64(1), []byte{} /*fixed size isn't allowed*/, 1, "foo", []byte{})
+	f.Fuzz(func(t *testing.T, field int32, differentField string, oracleId uint8, oracleIds []byte, accountSeed, accountsSeed int, bigField int64, fixedBytes []byte, i int, s string, decode []byte) {
+		// Cannot run more than one test or it hangs (currently a limit of fuzz testing), instead, just call it fuzz codec and do it all at once.
+		t.Run("Fuzz codec", func(t *testing.T) {
+			// Encode decode gets the same result
 			tester.Setup(t)
 			oids := [32]commontypes.OracleID{}
 			for index, id := range oracleIds {
@@ -59,6 +62,10 @@ func RunCodecInterfaceFuzzTests(f *testing.F, tester CodecInterfaceTester) {
 				decoded.BigField = testStruct.BigField
 			}
 			require.Equal(t, testStruct, decoded)
+
+			// Decode random bytes doesn't panic
+			// _ to get rid of compiler warnings, we just want to make sure of no panic!
+			_ = codec.Decode(ctx, decode, decoded, TestItemType)
 		})
 	})
 }

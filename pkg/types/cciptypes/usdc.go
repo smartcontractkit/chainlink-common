15107bc9d6554cc51dc56cac30ff2ff3831efc5d
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-02-08 22:49:35 +0100

    hash -> string

--- pkg/types/cciptypes/usdc.go
+++ pkg/types/cciptypes/usdc.go
@@ -7,5 +7,5 @@ import (
 type USDCReader interface {
 	// GetLastUSDCMessagePriorToLogIndexInTx returns the last USDC message that was sent
 	// before the provided log index in the given transaction.
-	GetLastUSDCMessagePriorToLogIndexInTx(ctx context.Context, logIndex int64, txHash Hash) ([]byte, error)
+	GetLastUSDCMessagePriorToLogIndexInTx(ctx context.Context, logIndex int64, txHash string) ([]byte, error)
 }

f590fdea5450c972cba7f0d2aea4f749ec7111f8
krehermann <krehermann@smartcontract.com>
2024-02-12 17:10:47 -0700

    more types

--- pkg/loop/internal/pb/ccip/generate.go
+++ pkg/loop/internal/pb/ccip/generate.go
@@ -3,5 +3,8 @@
 //go:generate protoc --proto_path=.:.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative offramp.proto
 //go:generate protoc --proto_path=.:.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative commitstore.proto
 //go:generate protoc --proto_path=.:.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative gaspriceestimator.proto
+//go:generate protoc --proto_path=.:.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pricegetter.proto
+//go:generate protoc --proto_path=.:.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative priceregistry.proto
+//go:generate protoc --proto_path=.:.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative tokendata.proto
 
 package ccippb

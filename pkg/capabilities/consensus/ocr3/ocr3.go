9a8aa062971bb30284af908ad2ac9e0ce161fdc3
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2024-02-15 19:18:14 +0000

    Thread through capability registry

--- pkg/capabilities/consensus/ocr3/ocr3.go
+++ pkg/capabilities/consensus/ocr3/ocr3.go
@@ -6,6 +6,7 @@ import (
 	"github.com/smartcontractkit/chainlink-common/pkg/capabilities/consensus/ocr3/types"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-common/pkg/loop"
+	"github.com/smartcontractkit/chainlink-common/pkg/loop/reportingplugins"
 	commontypes "github.com/smartcontractkit/chainlink-common/pkg/types"
 	"github.com/smartcontractkit/chainlink-common/pkg/values"
 )
@@ -14,22 +15,32 @@ type OCR3Capability struct {
 	lggr logger.Logger
 	loop.Plugin
 	factory EncoderFactory
+	reportingplugins.PluginProviderServer
 }
 
 type EncoderFactory func(config *values.Map) (types.Encoder, error)
 
 func NewOCR3(lggr logger.Logger, factory EncoderFactory) *OCR3Capability {
 	return &OCR3Capability{
-		lggr:    lggr,
-		Plugin:  loop.Plugin{Logger: lggr},
-		factory: factory,
+		lggr:                 lggr,
+		Plugin:               loop.Plugin{Logger: lggr},
+		factory:              factory,
+		PluginProviderServer: reportingplugins.PluginProviderServer{},
 	}
 }
 
-func (o *OCR3Capability) NewReportingPluginFactory(ctx context.Context, cfg commontypes.ReportingPluginServiceConfig, provider commontypes.PluginProvider, pipelineRunner commontypes.PipelineRunnerService, telemetry commontypes.TelemetryClient, errorLog commontypes.ErrorLog) (commontypes.OCR3ReportingPluginFactory, error) {
+func (o *OCR3Capability) NewReportingPluginFactory(ctx context.Context, cfg commontypes.ReportingPluginServiceConfig, provider commontypes.PluginProvider, pipelineRunner commontypes.PipelineRunnerService, telemetry commontypes.TelemetryClient, errorLog commontypes.ErrorLog, capabilityRegistry commontypes.CapabilitiesRegistry) (commontypes.OCR3ReportingPluginFactory, error) {
 	conf := &config{Logger: o.lggr, EncoderFactory: o.factory}
 	factory, err := newFactoryService(conf)
-	// TODO capabilityRegistry.Add(factory.capability)
+	if err != nil {
+		return nil, err
+	}
+
+	err = capabilityRegistry.Add(ctx, factory.capability)
+	if err != nil {
+		return nil, err
+	}
+
 	o.SubService(factory)
 	return factory, err
 }

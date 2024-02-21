064453621546212ca990baba979f07c1dfff05e4
Patrick Dowell <patrick.dowell@smartcontract.com>
2024-02-21 11:46:37 -0800

    Incorporating feedback from Henry's review

--- pkg/capabilities/triggers/mercury_trigger.go
+++ pkg/capabilities/triggers/mercury_trigger.go
@@ -2,11 +2,18 @@ package triggers
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"strconv"
 	"sync"
+	"time"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/capabilities"
+	"github.com/smartcontractkit/chainlink-common/pkg/values"
 )
+
+// TODO: Register the capiability in the NewDelegateFunction in chalink/core/services/workflows.delegate.go
 
 var mercuryInfo = capabilities.MustNewCapabilityInfo(
 	"mercury-trigger",
@@ -15,7 +22,8 @@ var mercuryInfo = capabilities.MustNewCapabilityInfo(
 	"v1.0.0",
 )
 
-type MercuryTrigger struct {
+// This Trigger Service allows for the registration and deregistration of triggers. You can also send reports to the service.
+type MercuryTriggerService struct {
 	capabilities.CapabilityInfo
 	chans               map[workflowID]chan<- capabilities.CapabilityResponse
 	feedIdsForTriggerId map[string][]uint64
@@ -30,39 +38,50 @@ type MercuryReport struct {
 	observationTimestamp uint32
 }
 
-type MercuryTriggerReport struct {
-	triggerType string
-	id          string
-	timestamp   string
+type MercuryTriggerEvent struct {
+	triggerType string // "mercury"
+	id          string // "sha256 of payload.feedId + payload.timestamp"
+	timestamp   string // "current time"
 	payload     MercuryReport
 }
 
-var _ capabilities.TriggerCapability = (*MercuryTrigger)(nil)
+var _ capabilities.TriggerCapability = (*MercuryTriggerService)(nil)
 
-func NewMercuryTrigger() *MercuryTrigger {
-	return &MercuryTrigger{
-		CapabilityInfo: mercuryInfo,
-		chans:          map[workflowID]chan<- capabilities.CapabilityResponse{},
+func NewMercuryTriggerService() *MercuryTriggerService {
+	return &MercuryTriggerService{
+		CapabilityInfo:      mercuryInfo,
+		chans:               map[workflowID]chan<- capabilities.CapabilityResponse{},
+		feedIdsForTriggerId: make(map[string][]uint64),
+		feedIdToWorkflowId:  make(map[uint64]string),
 	}
 }
 
-// This function needs to send a report, which will be a part of the CapabilityResponse
-func (o *MercuryTrigger) SendReport(ctx context.Context, wid string, event capabilities.CapabilityResponse) error {
+// maybe use mercury-pipline reports.go report struct instead of MercuryReport struct, and then we can use the pipeline report generators
+func (o *MercuryTriggerService) ProcessReport(report MercuryReport) error {
 	o.mu.Lock()
 	defer o.mu.Unlock()
 
-	// Decode the TriggerEvent on the CapabilityResponse.
-	// unwrap event.Value
-	val, err := event.Value.Unwrap()
+	currentTime := time.Now()
+	unixTimestampMillis := currentTime.UnixNano() / int64(time.Millisecond)
+	feedId := report.feedId
+
+	// Create a new CapabilityResponse with the MercuryTriggerEvent
+	triggerEvent := MercuryTriggerEvent{
+		triggerType: "mercury",
+		id:          sha256Hash(strconv.FormatUint(feedId, 10) + strconv.FormatUint(uint64(report.observationTimestamp), 10)),
+		timestamp:   strconv.FormatInt(unixTimestampMillis, 10),
+		payload:     report,
+	}
+
+	val, err := values.Wrap(triggerEvent)
 	if err != nil {
 		return err
 	}
-	// Convert val to MercuryTriggerReport
-	report, ok := val.(MercuryTriggerReport)
-	if !ok {
-		return fmt.Errorf("could not convert val to MercuryTriggerReport")
+
+	// Create a new CapabilityResponse with the MercuryTriggerEvent
+	event := capabilities.CapabilityResponse{
+		Value: val,
 	}
-	feedId := report.payload.feedId
 
 	// If the FeedId is in the feedIdsForTriggerId map, then send the event to the channel.
 	if _, ok := o.feedIdToWorkflowId[feedId]; ok {
@@ -77,25 +96,25 @@ func (o *MercuryTrigger) SendReport(ctx context.Context, wid string, event capab
 	return nil
 }
 
-func (o *MercuryTrigger) RegisterTrigger(ctx context.Context, callback chan<- capabilities.CapabilityResponse, req capabilities.CapabilityRequest) error {
+func (o *MercuryTriggerService) RegisterTrigger(ctx context.Context, callback chan<- capabilities.CapabilityResponse, req capabilities.CapabilityRequest) error {
 	wid := req.Metadata.WorkflowID
 
 	o.mu.Lock()
 	defer o.mu.Unlock()
 
-	o.chans[workflowID(wid)] = callback
+	o.chans[workflowID(wid)] = callback // TODO: if this workflowID is already in the map, throw an error?
 	// set feedIdsForTriggerId
-	triggerId := o.GetTriggerId(req)
-	feedIds := o.GetFeedIds(req)
+	triggerId := o.GetTriggerId(req) // TODO: This is manadatory, so throw an error if its not present or already exists
+	feedIds := o.GetFeedIds(req)     // TODO: what if feedIds is empty? should we throw an error or allow it?
 	o.feedIdsForTriggerId[triggerId] = feedIds
 	// set feedIdToWorkflowId for each feedId
 	for _, feedId := range feedIds {
-		o.feedIdToWorkflowId[feedId] = wid
+		o.feedIdToWorkflowId[feedId] = wid // TODO: should this be an array?
 	}
 	return nil
 }
 
-func (o *MercuryTrigger) UnregisterTrigger(ctx context.Context, req capabilities.CapabilityRequest) error {
+func (o *MercuryTriggerService) UnregisterTrigger(ctx context.Context, req capabilities.CapabilityRequest) error {
 	wid := req.Metadata.WorkflowID
 
 	o.mu.Lock()
@@ -105,22 +124,22 @@ func (o *MercuryTrigger) UnregisterTrigger(ctx context.Context, req capabilities
 	if ok {
 		close(ch)
 	}
+
+	triggerId := o.GetTriggerId(req)
 	// delete feedIdToWorkflowId for each feedId
-	feedIds := o.GetFeedIds(req)
+	feedIds := o.feedIdsForTriggerId[triggerId]
 	for _, feedId := range feedIds {
 		delete(o.feedIdToWorkflowId, feedId)
 	}
 
 	delete(o.chans, workflowID(wid))
-	// delete feedIdsForTriggerId
-	triggerId := o.GetTriggerId(req)
 	delete(o.feedIdsForTriggerId, triggerId)
 
 	return nil
 }
 
 // Get array of feedIds from CapabilityRequest req
-func (o *MercuryTrigger) GetFeedIds(req capabilities.CapabilityRequest) []uint64 {
+func (o *MercuryTriggerService) GetFeedIds(req capabilities.CapabilityRequest) []uint64 {
 	feedIds := make([]uint64, 0)
 	// Unwrap the inputs which should return pair (map, nil) and then get the feedIds from the map
 	if inputs, err := req.Inputs.Unwrap(); err == nil {
@@ -133,7 +152,7 @@ func (o *MercuryTrigger) GetFeedIds(req capabilities.CapabilityRequest) []uint64
 }
 
 // Get the triggerId from the CapabilityRequest req map
-func (o *MercuryTrigger) GetTriggerId(req capabilities.CapabilityRequest) string {
+func (o *MercuryTriggerService) GetTriggerId(req capabilities.CapabilityRequest) string {
 	var triggerId string
 	// Unwrap the inputs which should return pair (map, nil) and then get the triggerId from the map
 	if inputs, err := req.Inputs.Unwrap(); err == nil {
@@ -143,3 +162,11 @@ func (o *MercuryTrigger) GetTriggerId(req capabilities.CapabilityRequest) string
 	}
 	return triggerId
 }
+
+func sha256Hash(s string) string {
+	hash := sha256.New()
+	hash.Write([]byte(s))
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+// TODO: Capability Validation API stub out here

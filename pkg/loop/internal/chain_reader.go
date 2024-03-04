db8447c7bf72da6fb079d6e375077dc61ea116d4
ilija <pavlovicilija42@gmail.com>
2024-03-04 16:23:52 +0100

    Add queryKeys test

--- pkg/loop/internal/chain_reader.go
+++ pkg/loop/internal/chain_reader.go
@@ -7,6 +7,8 @@ import (
 
 	jsonv2 "github.com/go-json-experiment/json"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/fxamacker/cbor/v2"
@@ -111,6 +113,39 @@ func (c *chainReaderClient) GetLatestValue(ctx context.Context, contractName, me
 	return DecodeVersionedBytes(retVal, reply.RetVal)
 }
 
+func (c *chainReaderClient) QueryKeys(ctx context.Context, queryFilter types.QueryFilter) ([]types.Event, error) {
+	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! chainReaderClient Query Keys")
+	err := logQueryFilterValues(queryFilter)
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func logQueryFilterValues(queryFilter types.QueryFilter) error {
+	switch filter := queryFilter.(type) {
+	case *types.AndFilter:
+		for _, f := range filter.Filters {
+			fmt.Println("And Filter subfilter is: ^^^^^^^^^^^^^^^^^^^^^^^ ", f)
+			err := logQueryFilterValues(f)
+			if err != nil {
+				return err
+			}
+			fmt.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+		}
+		return nil
+	case *types.AddressFilter:
+		fmt.Println("****************************")
+		fmt.Println("Address Filter values are: ", filter.Address)
+		fmt.Println("****************************")
+
+		return nil
+	default:
+		return status.Errorf(codes.InvalidArgument, fmt.Sprintf("Unknown filter type %T ", queryFilter))
+	}
+
+	return nil
+}
 func (c *chainReaderClient) Bind(ctx context.Context, bindings []types.BoundContract) error {
 	pbBindings := make([]*pb.BoundContract, len(bindings))
 	for i, b := range bindings {
@@ -157,6 +192,42 @@ func (c *chainReaderServer) GetLatestValue(ctx context.Context, request *pb.GetL
 	}
 
 	return &pb.GetLatestValueReply{RetVal: encodedRetVal}, nil
+}
+
+func (c *chainReaderServer) QueryKeys(ctx context.Context, request *pb.QueryKeysRequest) (*pb.QueryKeysReply, error) {
+	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Query Keys server")
+
+	queryFilter, err := parseQueryFilterRequest(request)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = c.impl.QueryKeys(ctx, queryFilter)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.QueryKeysReply{RetVal: nil}, nil
+}
+
+func parseQueryFilterRequest(request *pb.QueryKeysRequest) (types.QueryFilter, error) {
+	switch filter := request.Filter.(type) {
+	case *pb.QueryKeysRequest_AndFilter:
+		var parsedQueryFilters []types.QueryFilter
+		for _, subQueryFilterRequest := range filter.AndFilter.Filters {
+			parsedQueryFilter, err := parseQueryFilterRequest(subQueryFilterRequest)
+			if err != nil {
+				return nil, err
+			}
+			parsedQueryFilters = append(parsedQueryFilters, parsedQueryFilter)
+		}
+		return &types.AndFilter{Filters: parsedQueryFilters}, nil
+	case *pb.QueryKeysRequest_AddressFilter:
+		return &types.AddressFilter{Address: filter.AddressFilter.Addresses}, nil
+	case *pb.QueryKeysRequest_KeysFilter:
+		return &types.KeysFilter{EventSig: filter.KeysFilter.EventSig}, nil
+	default:
+		return nil, status.Errorf(codes.InvalidArgument, "Unknown filter type")
+	}
 }
 
 func (c *chainReaderServer) Bind(ctx context.Context, bindings *pb.BindRequest) (*emptypb.Empty, error) {
--- pkg/types/chain_reader.go
+++ pkg/types/chain_reader.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"context"
+	"math/big"
 	"time"
 )
 
@@ -41,8 +42,21 @@ type ChainReader interface {
 	GetLatestValue(ctx context.Context, contractName string, method string, params, returnVal any) error
 
 	// Bind will override current bindings for the same contract, if one has been set and will return an error if the
-	// contract is not known by the ChainReader, or if the address is invalid
+	// contract is not known by the ChainReader, or if the Address is invalid
 	Bind(ctx context.Context, bindings []BoundContract) error
+
+	QueryKeys(ctx context.Context, queryFilter QueryFilter) ([]Event, error)
+
+	//// give it same event filter and
+	//QueryKeysByValue()
+	//QueryKeysExcluding()
+	//RegisterFilter()
+	//
+	////  we don't want to tackle querying for arbitrary data right now so these should do
+	//// CCIP specific evm log data searching, these can be made into one function
+	//GetCommitReportMatchingSeqNum()
+	//GetSendRequestsBetweenSeqNums()
+	//GetCommitReportGreaterThanSeqNum()
 }
 
 type BoundContract struct {
@@ -52,20 +66,207 @@ type BoundContract struct {
 }
 
 type Event struct {
-	ChainID           string
-	EventIndexInBlock string
-	Address           string
-	TxHash            string
-	BlockHash         string
-	BlockNumber       int64
-	BlockTimestamp    time.Time
-	CreatedAt         time.Time
-	Keys              []string
-	Data              []byte
+	ChainID        string
+	SequenceCursor string
+	timestamp      time.Time
+	Data           []byte
 }
 
+// Multiple EventFilters per QueryEvent call?
+
 type EventFilter struct {
-	AddressList []string   // contract address
-	KeysList    [][]string // 2D list of indexed search keys, outer dim = AND, inner dim = OR.  Params[0] is the name of the event (or "event type"), rest are any narrowing parameters that may help further specify the event
+	Name        string     // ??
+	AddressList []string   // contract Address
+	KeysList    [][]string // 2D list of indexed search Keys, outer dim = AND, inner dim = OR.  Params[0] is the name of the event (or "event type"), rest are any narrowing parameters that may help further specify the event
 	Retention   time.Duration
+}
+
+type EventQuery struct {
+	TxHash             string
+	FromBlock, ToBlock *big.Int // either a hash or a big int
+	FromTime, ToTime   time.Time
+	Filter             EventFilter
+	Finality           string
+}
+
+type ComparisonOperator int
+
+const (
+	Eq ComparisonOperator = iota
+	Neq
+	Gt
+	Lt
+	Gte
+	Lte
+)
+
+type SortDirection int
+
+const (
+	Asc SortDirection = iota
+	Desc
+)
+
+var (
+	DefaultSortAndLimit = SortAndLimit{
+		sortBy: []SortBy{
+			{field: "block_number", dir: Asc},
+			// sequence should convert to chain specific sequence unique identifier
+			{field: "sequence", dir: Asc},
+		},
+		limit: 0,
+	}
+)
+
+type SortAndLimit struct {
+	sortBy []SortBy
+	limit  int
+}
+
+type SortBy struct {
+	field string
+	dir   SortDirection
+}
+
+func NewSortAndLimit(limit int, sortBy ...SortBy) SortAndLimit {
+	return SortAndLimit{sortBy: sortBy, limit: limit}
+}
+
+func NewSortBy(field string, dir SortDirection) SortBy {
+	return SortBy{field: field, dir: dir}
+}
+
+type QueryFilter interface {
+	accept(visitor Visitor)
+}
+
+type AndFilter struct {
+	Filters []QueryFilter
+}
+
+func NewAndFilter(filters ...QueryFilter) *AndFilter {
+	return &AndFilter{Filters: filters}
+}
+
+func NewBasicAndFilter(address string, eventSig string, filters ...QueryFilter) *AndFilter {
+	allFilters := make([]QueryFilter, 0, len(filters)+2)
+	allFilters = append(allFilters, NewAddressFilter(address), NewKeysFilter(eventSig))
+	allFilters = append(allFilters, filters...)
+	return NewAndFilter(allFilters...)
+}
+
+func AppendedNewFilter(root *AndFilter, other ...QueryFilter) *AndFilter {
+	filters := make([]QueryFilter, 0, len(root.Filters)+len(other))
+	filters = append(filters, root.Filters...)
+	filters = append(filters, other...)
+	return NewAndFilter(filters...)
+}
+
+func (f *AndFilter) accept(visitor Visitor) {
+	visitor.VisitAndFilter(*f)
+}
+
+type AddressFilter struct {
+	Address []string
+}
+
+func NewAddressFilter(address ...string) *AddressFilter {
+	return &AddressFilter{Address: address}
+}
+
+func (f *AddressFilter) accept(visitor Visitor) {
+	visitor.VisitAddressFilter(*f)
+}
+
+type KeysFilter struct {
+	EventSig []string
+}
+
+func NewKeysFilter(keys ...string) *KeysFilter {
+	return &KeysFilter{EventSig: keys}
+}
+
+func (f *KeysFilter) accept(visitor Visitor) {
+	visitor.VisitKeysFilter(*f)
+}
+
+type KeysByValueFilter struct {
+	Keys   []string
+	Values [][]string
+}
+
+func NewKeysByValueFilter(keys []string, values [][]string) *KeysByValueFilter {
+	return &KeysByValueFilter{Keys: keys, Values: values}
+}
+
+func (f *KeysByValueFilter) accept(visitor Visitor) {
+	visitor.VisitKeysByValueFilter(*f)
+}
+
+type ConfirmationFilter struct {
+	confs string
+}
+
+func NewConfirmationFilter(confs string) *ConfirmationFilter {
+	return &ConfirmationFilter{confs: confs}
+}
+
+func (f *ConfirmationFilter) accept(visitor Visitor) {
+	visitor.VisitConfirmationFilter(*f)
+}
+
+func NewBlockFilter(block int64, operator ComparisonOperator) *BlockFilter {
+	return &BlockFilter{operator, block}
+}
+
+func NewBlockRangeFilter(start, end int64) *AndFilter {
+	return NewAndFilter(
+		NewBlockFilter(start, Gte),
+		NewBlockFilter(end, Lte),
+	)
+}
+
+type BlockFilter struct {
+	operator ComparisonOperator
+	block    int64
+}
+
+func (f *BlockFilter) accept(visitor Visitor) {
+	visitor.VisitBlockFilter(*f)
+}
+
+func NewTimeStampFilter(timestamp time.Time, operator ComparisonOperator) *TimestampFilter {
+	return &TimestampFilter{operator, timestamp}
+}
+
+type TimestampFilter struct {
+	operator  ComparisonOperator
+	timestamp time.Time
+}
+
+func (f *TimestampFilter) accept(visitor Visitor) {
+	visitor.VisitTimestampFilter(*f)
+}
+
+func NewTxHashFilter(txHash string) *TxHashFilter {
+	return &TxHashFilter{txHash}
+}
+
+type TxHashFilter struct {
+	txHash string
+}
+
+func (f *TxHashFilter) accept(visitor Visitor) {
+	visitor.VisitTxHashFilter(*f)
+}
+
+type Visitor interface {
+	VisitAndFilter(node AndFilter)
+	VisitAddressFilter(node AddressFilter)
+	VisitKeysFilter(node KeysFilter)
+	VisitKeysByValueFilter(node KeysByValueFilter)
+	VisitBlockFilter(node BlockFilter)
+	VisitConfirmationFilter(node ConfirmationFilter)
+	VisitTimestampFilter(node TimestampFilter)
+	VisitTxHashFilter(node TxHashFilter)
 }

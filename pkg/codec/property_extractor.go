977fae21957dd4cc1a6b7e332a4b0bb39658841c
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2024-03-06 14:38:46 -0600

    Expand Decode Features to more Types
    The type to type conversion mapping not only allows properties from struct, but
    also maintaining the original input type:
    
    []someStructWithIntProp -> []int
    [][]someStructWithIntProp -> [][]int
    *someStructWithIntProp -> *int
    
    Mapping from off-chain to on-chain type also maintains original structure but
    defaults field values, except for the extracted field.

--- pkg/codec/property_extractor.go
+++ pkg/codec/property_extractor.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/mitchellh/mapstructure"
+
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
 )
 
@@ -14,67 +15,243 @@ import (
 // return a single element. Calling TransformToOnchain will result in unset properties.
 func NewPropertyExtractor(fieldName string) Modifier {
 	m := &propertyExtractor{
-		modifierBase: modifierBase[bool]{
-			fields:           map[string]bool{fieldName: false},
-			onToOffChainType: map[reflect.Type]reflect.Type{},
-			offToOnChainType: map[reflect.Type]reflect.Type{},
-		},
-		fieldName: fieldName,
-	}
-
-	m.modifyFieldForInput = func(_ string, field *reflect.StructField, _ string, _ bool) error {
-		return nil
+		onToOffChainType: map[reflect.Type]reflect.Type{},
+		offToOnChainType: map[reflect.Type]reflect.Type{},
+		fieldName:        fieldName,
 	}
 
 	return m
 }
 
 type propertyExtractor struct {
-	modifierBase[bool]
-	fieldName string
+	onToOffChainType map[reflect.Type]reflect.Type
+	offToOnChainType map[reflect.Type]reflect.Type
+	fieldName        string
+}
+
+func (e *propertyExtractor) RetypeToOffChain(onChainType reflect.Type, itemType string) (reflect.Type, error) {
+	if e.fieldName == "" {
+		return nil, fmt.Errorf("%w: field name required for extraction", types.ErrInvalidConfig)
+	}
+
+	if cached, ok := e.onToOffChainType[onChainType]; ok {
+		return cached, nil
+	}
+
+	switch onChainType.Kind() {
+	case reflect.Pointer:
+		elm, err := e.RetypeToOffChain(onChainType.Elem(), "")
+		if err != nil {
+			return nil, err
+		}
+
+		ptr := reflect.PointerTo(elm)
+		e.onToOffChainType[onChainType] = ptr
+		e.offToOnChainType[ptr] = onChainType
+
+		return ptr, nil
+	case reflect.Slice:
+		elm, err := e.RetypeToOffChain(onChainType.Elem(), "")
+		if err != nil {
+			return nil, err
+		}
+
+		sliceType := reflect.SliceOf(elm)
+		e.onToOffChainType[onChainType] = sliceType
+		e.offToOnChainType[sliceType] = onChainType
+
+		return sliceType, nil
+	case reflect.Array:
+		elm, err := e.RetypeToOffChain(onChainType.Elem(), "")
+		if err != nil {
+			return nil, err
+		}
+
+		arrayType := reflect.ArrayOf(onChainType.Len(), elm)
+		e.onToOffChainType[onChainType] = arrayType
+		e.offToOnChainType[arrayType] = onChainType
+
+		return arrayType, nil
+	case reflect.Struct:
+		return e.getPropTypeFromStruct(onChainType)
+	default:
+		return nil, fmt.Errorf("%w: cannot retype the kind %v", types.ErrInvalidType, onChainType.Kind())
+	}
 }
 
 func (e *propertyExtractor) TransformToOnChain(offChainValue any, _ string) (any, error) {
-	parts := strings.Split(e.fieldName, ".")
-	fieldName := parts[len(parts)-1]
+	rItem := reflect.ValueOf(offChainValue)
 
-	fields := []reflect.StructField{
-		{Name: fieldName, Type: reflect.TypeOf(offChainValue)},
+	toType, ok := e.offToOnChainType[rItem.Type()]
+	if !ok {
+		return reflect.Value{}, fmt.Errorf("%w: cannot retype %v", types.ErrInvalidType, rItem.Type())
 	}
 
-	onChainStruct := reflect.StructOf(fields)
-	output := reflect.New(onChainStruct)
-	iOutput := reflect.Indirect(output)
-	iOutput.FieldByName(fieldName).Set(reflect.ValueOf(offChainValue))
+	output, err := expandWithMapsHelper(rItem, toType, e.fieldName)
+	if err != nil {
+		return reflect.Value{}, err
+	}
 
-	return iOutput.Interface(), nil
+	return output.Interface(), nil
 }
 
 func (e *propertyExtractor) TransformToOffChain(onChainValue any, _ string) (any, error) {
 	rItem := reflect.ValueOf(onChainValue)
 
-	_, ok := e.onToOffChainType[rItem.Type()]
+	toType, ok := e.onToOffChainType[rItem.Type()]
 	if !ok {
 		return reflect.Value{}, fmt.Errorf("%w: cannot retype %v", types.ErrInvalidType, rItem.Type())
 	}
 
-	switch rItem.Kind() {
-	case reflect.Struct:
-	case reflect.Pointer:
-		rItem = rItem.Elem()
-		if rItem.Kind() != reflect.Struct {
-			return reflect.Value{}, fmt.Errorf("%w: must be struct or pointer to struct to extract value", types.ErrInvalidType)
-		}
-	default:
-		return reflect.Value{}, fmt.Errorf("%w: must be struct or pointer to struct to extract value", types.ErrInvalidType)
-	}
-
-	valueMapping := map[string]any{}
-	if err := mapstructure.Decode(rItem.Interface(), &valueMapping); err != nil {
+	output, err := extractWithMapsHelper(rItem, toType, e.fieldName)
+	if err != nil {
 		return reflect.Value{}, err
 	}
 
-	path := strings.Split(e.fieldName, ".")
+	return output.Interface(), nil
+}
+
+func (e *propertyExtractor) getPropTypeFromStruct(onChainType reflect.Type) (reflect.Type, error) {
+	filedLocations, err := getFieldIndices(onChainType)
+	if err != nil {
+		return nil, err
+	}
+
+	parts := strings.Split(e.fieldName, ".")
+	fieldName := parts[len(parts)-1]
+	parts = parts[:len(parts)-1]
+
+	curLocations := filedLocations
+	for _, part := range parts {
+		if curLocations, err = curLocations.populateSubFields(part); err != nil {
+			return nil, err
+		}
+	}
+
+	curLocations.updateTypeFromSubkeyMods(fieldName)
+	field, ok := curLocations.fieldByName(fieldName)
+	if !ok {
+		return nil, fmt.Errorf("%w: field not found in on-chain type %s", types.ErrInvalidType, e.fieldName)
+	}
+
+	e.onToOffChainType[onChainType] = field.Type
+	e.offToOnChainType[field.Type] = onChainType
+
+	return field.Type, nil
+}
+
+func expandWithMapsHelper(rItem reflect.Value, toType reflect.Type, field string) (reflect.Value, error) {
+	switch toType.Kind() {
+	case reflect.Pointer:
+		if toType.Elem().Kind() == reflect.Struct {
+			into := reflect.New(toType.Elem())
+			err := setFieldValue(rItem.Elem().Interface(), into.Interface(), field)
+			return into, err
+		}
+
+		tmp, err := expandWithMapsHelper(rItem.Elem(), toType.Elem(), field)
+		result := reflect.New(toType.Elem())
+		reflect.Indirect(result).Set(tmp)
+
+		return result, err
+	case reflect.Struct:
+		into := reflect.New(toType)
+		err := setFieldValue(rItem.Interface(), into.Interface(), field)
+		return into.Elem(), err
+	case reflect.Slice:
+		length := rItem.Len()
+		into := reflect.MakeSlice(toType, length, length)
+		err := expandMany(rItem, into, field)
+		return into, err
+	case reflect.Array:
+		into := reflect.New(toType).Elem()
+		err := expandMany(rItem, into, field)
+		return into, err
+	default:
+		return reflect.Value{}, fmt.Errorf("%w: cannot retype", types.ErrInvalidType)
+	}
+}
+
+func extractWithMapsHelper(
+	rItem reflect.Value,
+	toType reflect.Type,
+	field string,
+) (reflect.Value, error) {
+	switch rItem.Kind() {
+	case reflect.Pointer:
+		elm := rItem.Elem()
+		if elm.Kind() == reflect.Struct {
+			tmp, err := extractElement(rItem.Interface(), field)
+			result := reflect.New(toType.Elem())
+			reflect.Indirect(result).Set(tmp)
+
+			return result, err
+		}
+
+		tmp, err := extractWithMapsHelper(elm, toType.Elem(), field)
+		result := reflect.New(toType.Elem())
+		reflect.Indirect(result).Set(tmp)
+
+		return result, err
+	case reflect.Struct:
+		return extractElement(rItem.Interface(), field)
+	case reflect.Slice:
+		length := rItem.Len()
+		into := reflect.MakeSlice(toType, length, length)
+		err := extractMany(rItem, into, field)
+		return into, err
+	case reflect.Array:
+		into := reflect.New(toType).Elem()
+		err := extractMany(rItem, into, field)
+		return into, err
+	default:
+		return reflect.Value{}, fmt.Errorf("%w: cannot retype %v", types.ErrInvalidType, rItem.Type())
+	}
+}
+
+func extractMany(rInput, rOutput reflect.Value, field string) error {
+	length := rInput.Len()
+
+	for i := 0; i < length; i++ {
+		inTmp := rInput.Index(i)
+		outTmp := rOutput.Index(i)
+
+		output, err := extractWithMapsHelper(inTmp, outTmp.Type(), field)
+		if err != nil {
+			return err
+		}
+
+		outTmp.Set(output)
+	}
+
+	return nil
+}
+
+func expandMany(rInput, rOutput reflect.Value, field string) error {
+	length := rInput.Len()
+
+	for i := 0; i < length; i++ {
+		inTmp := rInput.Index(i)
+		outTmp := rOutput.Index(i)
+
+		output, err := expandWithMapsHelper(inTmp, outTmp.Type(), field)
+		if err != nil {
+			return err
+		}
+
+		outTmp.Set(output)
+	}
+
+	return nil
+}
+
+func extractElement(src any, field string) (reflect.Value, error) {
+	valueMapping := map[string]any{}
+	if err := mapstructure.Decode(src, &valueMapping); err != nil {
+		return reflect.Value{}, err
+	}
+
+	path := strings.Split(field, ".")
 	name := path[len(path)-1]
 	path = path[:len(path)-1]
 
@@ -84,15 +261,49 @@ func (e *propertyExtractor) TransformToOffChain(onChainValue any, _ string) (any
 	}
 
 	if len(extractMaps) != 1 {
-		return reflect.Value{}, fmt.Errorf("%w: cannot find %s", types.ErrInvalidType, e.fieldName)
+		return reflect.Value{}, fmt.Errorf("%w: cannot find %s", types.ErrInvalidType, field)
 	}
 
 	em := extractMaps[0]
 
 	item, ok := em[name]
 	if !ok {
-		return reflect.Value{}, fmt.Errorf("%w: cannot find %s", types.ErrInvalidType, e.fieldName)
+		return reflect.Value{}, fmt.Errorf("%w: cannot find %s", types.ErrInvalidType, field)
 	}
 
-	return item, nil
+	return reflect.ValueOf(item), nil
+}
+
+func setFieldValue(src, dest any, field string) error {
+	valueMapping := map[string]any{}
+	if err := mapstructure.Decode(dest, &valueMapping); err != nil {
+		return fmt.Errorf("%w: %w", types.ErrInvalidType, err)
+	}
+
+	path := strings.Split(field, ".")
+	name := path[len(path)-1]
+	path = path[:len(path)-1]
+
+	extractMaps, err := getMapsFromPath(valueMapping, path)
+	if err != nil {
+		return err
+	}
+
+	if len(extractMaps) != 1 {
+		return fmt.Errorf("%w: only 1 extract map expected", types.ErrInvalidType)
+	}
+
+	extractMaps[0][name] = src
+
+	conf := &mapstructure.DecoderConfig{Result: &dest, Squash: true}
+	decoder, err := mapstructure.NewDecoder(conf)
+	if err != nil {
+		return fmt.Errorf("%w: %w", types.ErrInvalidType, err)
+	}
+
+	if err = decoder.Decode(valueMapping); err != nil {
+		return fmt.Errorf("%w: %w", types.ErrInvalidType, err)
+	}
+
+	return nil
 }
--- pkg/codec/property_extractor_test.go
+++ pkg/codec/property_extractor_test.go
@@ -22,21 +22,41 @@ func TestPropertyExtractor(t *testing.T) {
 		B testStruct
 	}
 
+	onChainType := reflect.TypeOf(nestedTestStruct{})
+
 	extractor := codec.NewPropertyExtractor("A")
 	invalidExtractor := codec.NewPropertyExtractor("A.B")
 	nestedExtractor := codec.NewPropertyExtractor("B.B")
 
-	t.Run("RetypeToOffChain keeps equal structure to onchain", func(t *testing.T) {
+	t.Run("RetypeToOffChain sets the type for offchain to the onchain property", func(t *testing.T) {
 		offChainType, err := extractor.RetypeToOffChain(reflect.TypeOf(nestedTestStruct{}), "")
 		require.NoError(t, err)
+		require.Equal(t, reflect.TypeOf(""), offChainType)
+	})
 
-		require.Equal(t, 2, offChainType.NumField())
-		f0 := offChainType.Field(0)
-		assert.Equal(t, "A", f0.Name)
-		assert.Equal(t, reflect.TypeOf(""), f0.Type)
-		f1 := offChainType.Field(1)
-		assert.Equal(t, "B", f1.Name)
-		assert.Equal(t, reflect.TypeOf(testStruct{}), f1.Type)
+	t.Run("RetypeToOffChain works on pointers", func(t *testing.T) {
+		offChainType, err := extractor.RetypeToOffChain(reflect.TypeOf(&nestedTestStruct{}), "")
+		require.NoError(t, err)
+
+		str := ""
+
+		require.Equal(t, reflect.TypeOf(&str), offChainType)
+	})
+
+	t.Run("RetypeToOffChain works on slices", func(t *testing.T) {
+		offChainType, err := extractor.RetypeToOffChain(reflect.TypeOf([]nestedTestStruct{}), "")
+		require.NoError(t, err)
+
+		require.Equal(t, reflect.TypeOf([]string{}), offChainType)
+	})
+
+	t.Run("RetypeToOffChain works on arrays", func(t *testing.T) {
+		arrayLen := 3
+		offChainType, err := extractor.RetypeToOffChain(reflect.ArrayOf(arrayLen, onChainType), "")
+		require.NoError(t, err)
+
+		require.Equal(t, reflect.Array, offChainType.Kind())
+		require.Equal(t, arrayLen, offChainType.Len())
 	})
 
 	t.Run("RetypeToOffChain returns error for missing field", func(t *testing.T) {
@@ -60,16 +80,18 @@ func TestPropertyExtractor(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, "test", offChainValue)
 
-		reducedOnChain, err := extractor.TransformToOnChain("value", "")
+		lossyOnChain, err := extractor.TransformToOnChain("value", "")
 		require.NoError(t, err)
 
-		expected := struct {
-			A string
-		}{
+		expected := nestedTestStruct{
 			A: "value",
+			B: testStruct{
+				A: false,
+				B: 0,
+			},
 		}
 
-		assert.Equal(t, reducedOnChain, expected)
+		assert.Equal(t, expected, lossyOnChain)
 	})
 
 	t.Run("TransformToOnChain and TransformToOffChain works on nested structs", func(t *testing.T) {
@@ -88,20 +110,22 @@ func TestPropertyExtractor(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, int64(42), offChainValue)
 
-		reducedOnChain, err := nestedExtractor.TransformToOnChain(int64(3), "")
+		lossyOnChain, err := nestedExtractor.TransformToOnChain(int64(3), "")
 		require.NoError(t, err)
 
-		expected := struct {
-			B int64
-		}{
-			B: 3,
+		expected := nestedTestStruct{
+			A: "",
+			B: testStruct{
+				A: false,
+				B: 3,
+			},
 		}
 
-		assert.Equal(t, reducedOnChain, expected)
+		assert.Equal(t, expected, lossyOnChain)
 	})
 
 	t.Run("TransformToOnChain and TransformToOffChain works on pointers", func(t *testing.T) {
-		_, err := nestedExtractor.RetypeToOffChain(reflect.TypeOf(&nestedTestStruct{}), "")
+		_, err := nestedExtractor.RetypeToOffChain(reflect.PointerTo(onChainType), "")
 		require.NoError(t, err)
 
 		onChainValue := &nestedTestStruct{
@@ -114,17 +138,113 @@ func TestPropertyExtractor(t *testing.T) {
 
 		offChainValue, err := nestedExtractor.TransformToOffChain(onChainValue, "")
 		require.NoError(t, err)
-		require.Equal(t, int64(42), offChainValue)
 
-		reducedOnChain, err := nestedExtractor.TransformToOnChain(int64(3), "")
+		expectedVal := int64(42)
+		require.Equal(t, &expectedVal, offChainValue)
+
+		value := int64(3)
+
+		lossyOnChain, err := nestedExtractor.TransformToOnChain(&value, "")
 		require.NoError(t, err)
 
-		expected := struct {
-			B int64
-		}{
-			B: 3,
+		expected := &nestedTestStruct{
+			A: "",
+			B: testStruct{
+				A: false,
+				B: 3,
+			},
 		}
 
-		assert.Equal(t, reducedOnChain, expected)
+		assert.Equal(t, expected, lossyOnChain)
+	})
+
+	t.Run("TransformToOnChain and TransformToOffChain works on slices", func(t *testing.T) {
+		_, err := nestedExtractor.RetypeToOffChain(reflect.TypeOf([]nestedTestStruct{}), "")
+		require.NoError(t, err)
+
+		input := []nestedTestStruct{
+			{A: "test0", B: testStruct{A: true, B: 42}},
+			{A: "test1", B: testStruct{A: true, B: 43}},
+		}
+
+		expected := []int64{42, 43}
+
+		actual, err := nestedExtractor.TransformToOffChain(input, "")
+		require.NoError(t, err)
+		assert.Equal(t, expected, actual)
+
+		lossyOnChain, err := nestedExtractor.TransformToOnChain([]int64{3, 4}, "")
+		require.NoError(t, err)
+
+		expectedLossy := []nestedTestStruct{
+			{A: "", B: testStruct{A: false, B: 3}},
+			{A: "", B: testStruct{A: false, B: 4}},
+		}
+
+		assert.Equal(t, expectedLossy, lossyOnChain)
+	})
+
+	t.Run("TransformToOnChain and TransformToOffChain works on slices of slices", func(t *testing.T) {
+		_, err := nestedExtractor.RetypeToOffChain(reflect.TypeOf([][]nestedTestStruct{}), "")
+		require.NoError(t, err)
+
+		input := [][]nestedTestStruct{
+			{
+				{A: "test00", B: testStruct{A: true, B: 42}},
+				{A: "test01", B: testStruct{A: true, B: 43}},
+			},
+			{
+				{A: "test10", B: testStruct{A: true, B: 44}},
+				{A: "test11", B: testStruct{A: true, B: 45}},
+			},
+		}
+
+		expected := [][]int64{{42, 43}, {44, 45}}
+
+		actual, err := nestedExtractor.TransformToOffChain(input, "")
+		require.NoError(t, err)
+		assert.Equal(t, expected, actual)
+
+		lossyOnChain, err := nestedExtractor.TransformToOnChain([][]int64{{3, 4}, {5, 6}}, "")
+		require.NoError(t, err)
+
+		expectedLossy := [][]nestedTestStruct{
+			{
+				{A: "", B: testStruct{A: false, B: 3}},
+				{A: "", B: testStruct{A: false, B: 4}},
+			},
+			{
+				{A: "", B: testStruct{A: false, B: 5}},
+				{A: "", B: testStruct{A: false, B: 6}},
+			},
+		}
+
+		assert.Equal(t, expectedLossy, lossyOnChain)
+	})
+
+	t.Run("TransformToOnChain and TransformToOffChain works on arrays", func(t *testing.T) {
+		_, err := nestedExtractor.RetypeToOffChain(reflect.ArrayOf(2, onChainType), "")
+		require.NoError(t, err)
+
+		input := [2]nestedTestStruct{
+			{A: "test0", B: testStruct{A: true, B: 42}},
+			{A: "test1", B: testStruct{A: true, B: 43}},
+		}
+
+		expected := [2]int64{42, 43}
+
+		actual, err := nestedExtractor.TransformToOffChain(input, "")
+		require.NoError(t, err)
+		assert.Equal(t, expected, actual)
+
+		lossyOnChain, err := nestedExtractor.TransformToOnChain([2]int64{3, 4}, "")
+		require.NoError(t, err)
+
+		expectedLossy := [2]nestedTestStruct{
+			{A: "", B: testStruct{A: false, B: 3}},
+			{A: "", B: testStruct{A: false, B: 4}},
+		}
+
+		assert.Equal(t, expectedLossy, lossyOnChain)
 	})
 }

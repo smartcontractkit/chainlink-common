f2090c1472788eadcf0c3f69a8edc1a679abd74d
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2024-03-20 10:53:50 -0400

    Add batching tests

--- pkg/sqlutil/batch.go
+++ pkg/sqlutil/batch.go
@@ -1,10 +1,18 @@
 package sqlutil
 
+import "errors"
+
 // BatchFunc is the function to execute on each batch of records, should return the count of records affected
 type BatchFunc func(offset, limit uint) (count uint, err error)
 
+const batchSizeErr = "batch size cannot be zero"
+
 // Batch is an iterator for batches of records
 func Batch(cb BatchFunc, batchSize uint) error {
+	if batchSize == 0 {
+		return errors.New(batchSizeErr)
+	}
+
 	offset := uint(0)
 	limit := batchSize
 

8dee765d99a0214c5735ed848f52905afae19308
aalu1418 <50029043+aalu1418@users.noreply.github.com>
2024-04-05 12:11:01 -0600

    split into separate func

--- pkg/monitoring/multi_feed_monitor.go
+++ pkg/monitoring/multi_feed_monitor.go
@@ -49,71 +49,75 @@ func (m *multiFeedMonitor) Run(ctx context.Context, data RDDData) {
 	var subs utils.Subprocesses
 	defer subs.Wait()
 
-FEED_LOOP:
+	// setup monitors for each feed
 	for _, feedConfig := range data.Feeds {
 		feedLogger := logger.With(m.log,
 			"feed_name", feedConfig.GetName(),
 			"feed_id", feedConfig.GetID(),
 			"network", m.chainConfig.GetNetworkName(),
 		)
-		// Create data sources
-		pollers := []Poller{}
-		for _, sourceFactory := range m.sourceFactories {
-			source, err := sourceFactory.NewSource(SourceParams{
-				ChainConfig: m.chainConfig,
-				FeedConfig:  feedConfig,
-				Nodes:       data.Nodes,
-			})
-			if err != nil {
-				feedLogger.Errorw("failed to create source", "error", err, "source-type", fmt.Sprintf("%T", sourceFactory))
-				continue
-			}
-			poller := NewSourcePoller(
-				source,
-				logger.With(m.log, "component", "chain-poller", "source", sourceFactory.GetType()),
-				m.chainConfig.GetPollInterval(),
-				m.chainConfig.GetReadTimeout(),
-				m.bufferCapacity,
-			)
-			pollers = append(pollers, poller)
+		m.createMonitor(ctx, feedLogger, &subs, feedConfig, data.Nodes)
+	}
+}
+
+// createDataSource reusable internal method to create data sources
+func (m *multiFeedMonitor) createMonitor(ctx context.Context, lgr Logger, subs *utils.Subprocesses, feedConfig FeedConfig, nodes []NodeConfig) {
+	pollers := []Poller{}
+	for _, sourceFactory := range m.sourceFactories {
+		source, err := sourceFactory.NewSource(SourceParams{
+			ChainConfig: m.chainConfig,
+			FeedConfig:  feedConfig,
+			Nodes:       nodes,
+		})
+		if err != nil {
+			lgr.Errorw("failed to create source", "error", err, "source-type", fmt.Sprintf("%T", sourceFactory))
+			continue
 		}
-		if len(pollers) == 0 {
-			feedLogger.Errorw("not tracking feed because all sources failed to initialize")
-			continue FEED_LOOP
-		}
-		// Create exporters
-		exporters := []Exporter{}
-		for _, exporterFactory := range m.exporterFactories {
-			exporter, err := exporterFactory.NewExporter(ExporterParams{
-				m.chainConfig,
-				feedConfig,
-				data.Nodes,
-			})
-			if err != nil {
-				feedLogger.Errorw("failed to create new exporter", "error", err, "exporter-type", fmt.Sprintf("%T", exporterFactory))
-				continue
-			}
-			exporters = append(exporters, exporter)
-		}
-		if len(exporters) == 0 {
-			feedLogger.Errorw("not tracking feed because all exporters failed to initialize")
-			continue FEED_LOOP
-		}
-		// Run poller goroutines.
-		for _, poller := range pollers {
-			poller := poller
-			subs.Go(func() {
-				poller.Run(ctx)
-			})
-		}
-		// Run feed monitor.
-		feedMonitor := NewFeedMonitor(
-			logger.With(m.log, "component", "feed-monitor"),
-			pollers,
-			exporters,
+		poller := NewSourcePoller(
+			source,
+			logger.With(m.log, "component", "chain-poller", "source", sourceFactory.GetType()),
+			m.chainConfig.GetPollInterval(),
+			m.chainConfig.GetReadTimeout(),
+			m.bufferCapacity,
 		)
+		pollers = append(pollers, poller)
+	}
+	if len(pollers) == 0 {
+		lgr.Errorw("not tracking feed because all sources failed to initialize")
+		return
+	}
+	// Create exporters
+	exporters := []Exporter{}
+	for _, exporterFactory := range m.exporterFactories {
+		exporter, err := exporterFactory.NewExporter(ExporterParams{
+			m.chainConfig,
+			feedConfig,
+			nodes,
+		})
+		if err != nil {
+			lgr.Errorw("failed to create new exporter", "error", err, "exporter-type", fmt.Sprintf("%T", exporterFactory))
+			continue
+		}
+		exporters = append(exporters, exporter)
+	}
+	if len(exporters) == 0 {
+		lgr.Errorw("not tracking feed because all exporters failed to initialize")
+		return
+	}
+	// Run poller goroutines.
+	for _, poller := range pollers {
+		poller := poller
 		subs.Go(func() {
-			feedMonitor.Run(ctx)
+			poller.Run(ctx)
 		})
 	}
+	// Run feed monitor.
+	feedMonitor := NewFeedMonitor(
+		logger.With(m.log, "component", "feed-monitor"),
+		pollers,
+		exporters,
+	)
+	subs.Go(func() {
+		feedMonitor.Run(ctx)
+	})
 }

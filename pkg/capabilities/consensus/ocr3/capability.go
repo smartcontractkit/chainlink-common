31a3bc11ed3c90045b3ec6cf5977a598187eb178
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2024-05-29 19:05:48 +0100

    [KS-249] Fix context cancelation bug by waiting for Execute to complete

--- pkg/capabilities/consensus/ocr3/capability.go
+++ pkg/capabilities/consensus/ocr3/capability.go
@@ -190,8 +190,19 @@ func (o *capability) Execute(ctx context.Context, r capabilities.CapabilityReque
 				Err:   nil,
 			},
 		}
-		err = o.transmitResponse(ctx, out)
-		return nil, err
+		o.transmitCh <- out
+
+		// Return a dummy response back to the caller
+		// This allows the transmitter to block on a response before
+		// returning from Transmit()
+		// TODO(cedric): our current stream-based implementation for the Execute
+		// returns immediately without waiting for the server-side to complete. This
+		// breaks the API since callers can no longer rely on a non-error response
+		// from Execute() serving as an acknowledgement that the request in being handled.
+		callbackCh := make(chan capabilities.CapabilityResponse, 1)
+		callbackCh <- capabilities.CapabilityResponse{}
+		close(callbackCh)
+		return callbackCh, nil
 	case methodStartRequest:
 		// Receives and stores an observation to do consensus on
 		// Receives an aggregation method; at this point the method has been validated

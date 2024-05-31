9d5c57358e4d97256ef29e421becfc1d248e0d90
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2024-05-31 13:09:57 -0600

    Cached Contract Reader
    The base implementation of `ContractReader` does not include caching. This struct
    wraps a `ContractReader` and provides basic caching strategies such as polling on
    an interval and caching the result as well as falling-through when a cached value is
    stale based on a configured value.
    
    Caching is managed per contract/method where params must match exactly on previously
    requested values. If no cache matches occur, the request falls-through to the wrapped
    `ContractReader`.
    
    The functions `QueryKey` and `Bind` always fall-through to the wrapped `ContractReader`.

---
+++ pkg/types/contract_reader_cache.go
@@ -0,0 +1,427 @@
+package types
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/services"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/query"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/query/primitives"
+	"github.com/smartcontractkit/chainlink-common/pkg/utils"
+)
+
+var (
+	ErrNotExist        = errors.New("data does not exist for key")
+	ErrTypeMismatch    = errors.New("data type mismatch")
+	ErrInvalidStrategy = errors.New("invalid cache strategy")
+)
+
+const (
+	CachedContractReaderName = "CachedContractReader"
+
+	cleanInterval = time.Second
+	pollInterval  = time.Second
+)
+
+type TriggerAndCache struct {
+	Params any
+	Value  any
+}
+
+type PollAndCache struct {
+	TriggerAndCache
+	Interval time.Duration
+}
+
+type FallthroughOnStale struct {
+	Staleness time.Duration
+}
+
+type NoCache struct{}
+
+var _ ContractReader = &CachedContractReader{}
+var _ services.Service = &CachedContractReader{}
+
+type CachedContractReader struct {
+	UnimplementedContractReader
+
+	// injected dependencies
+	base ContractReader
+
+	// internal configuration properties
+	strategies map[string]any
+	polling    []cacheKey
+	triggers   []cacheKey
+
+	// active data
+	data     map[cacheKey]*cachedValue
+	subprocs []chan struct{}
+	state    services.StateMachine
+
+	// properties for thread safety
+	strategyLock sync.RWMutex
+	dataLock     sync.RWMutex
+	mu           sync.RWMutex
+}
+
+func NewCachedContractReader(base ContractReader) *CachedContractReader {
+	return &CachedContractReader{
+		base:       base,
+		strategies: make(map[string]any),
+		data:       make(map[cacheKey]*cachedValue),
+		polling:    make([]cacheKey, 0),
+		triggers:   make([]cacheKey, 0),
+		subprocs:   make([]chan struct{}, 0, 2),
+	}
+}
+
+// GetLatestValue implements the ContractReader interface and caches values.
+func (c *CachedContractReader) GetLatestValue(
+	ctx context.Context,
+	readIdentifier string,
+	confidenceLevel primitives.ConfidenceLevel,
+	params, returnVal any,
+) error {
+	strategy, hasConfig := c.getStrategy(readIdentifier)
+	if !hasConfig {
+		return c.fallThrough(ctx, readIdentifier, confidenceLevel, params, returnVal)
+	}
+
+	switch typed := strategy.(type) {
+	case PollAndCache:
+		// the strategy params should match the incoming params
+		if !reflect.DeepEqual(typed.Params, params) {
+			return c.fallThrough(ctx, readIdentifier, confidenceLevel, params, returnVal)
+		}
+
+		key := newCacheKey(readIdentifier, confidenceLevel, params, 0)
+		if err := c.getFromCache(key, returnVal); err == nil || !errors.Is(err, ErrNotExist) {
+			return err
+		}
+
+		return c.fallThroughAndCache(ctx, key, returnVal)
+	case FallthroughOnStale:
+		key := newCacheKey(readIdentifier, confidenceLevel, params, typed.Staleness)
+		if err := c.getFromCache(key, returnVal); err == nil || !errors.Is(err, ErrNotExist) {
+			return err
+		}
+
+		return c.fallThroughAndCache(ctx, key, returnVal)
+	case NoCache:
+		return c.fallThrough(ctx, readIdentifier, confidenceLevel, params, returnVal)
+	}
+
+	return c.base.GetLatestValue(ctx, readIdentifier, confidenceLevel, params, returnVal)
+}
+
+// QueryKey method implements the ContractReader interface and passes through the cache.
+func (c *CachedContractReader) QueryKey(
+	ctx context.Context,
+	contract BoundContract,
+	filter query.KeyFilter,
+	limitAndSort query.LimitAndSort,
+	sequenceDataType any,
+) ([]Sequence, error) {
+	return c.base.QueryKey(ctx, contract, filter, limitAndSort, sequenceDataType)
+}
+
+// Bind implements the ChainReader interface and passes through values to the base ChainReader. No caching applied.
+func (c *CachedContractReader) Bind(ctx context.Context, bindings []BoundContract) error {
+	return c.base.Bind(ctx, bindings)
+}
+
+// Unbind implements the ChainReader interface and passes through values to the base ChainReader. No caching applied.
+func (c *CachedContractReader) Unbind(ctx context.Context, bindings []BoundContract) error {
+	return c.base.Bind(ctx, bindings)
+}
+
+func (c *CachedContractReader) Start(ctx context.Context) error {
+	return c.state.StartOnce(CachedContractReaderName, func() error {
+		c.startSubproc(c.clean, cleanInterval)
+		c.startSubproc(c.pollAndCache, pollInterval)
+
+		return nil
+	})
+}
+
+func (c *CachedContractReader) Close() error {
+	return c.state.StopOnce(CachedContractReaderName, func() error {
+		for _, chClose := range c.subprocs {
+			chClose <- struct{}{}
+		}
+
+		return nil
+	})
+}
+
+func (c *CachedContractReader) Ready() error { return nil }
+
+func (c *CachedContractReader) Name() string { return CachedContractReaderName }
+
+func (c *CachedContractReader) HealthReport() map[string]error {
+	return map[string]error{c.Name(): nil}
+}
+
+func (c *CachedContractReader) SetCacheStrategy(readIdentifier string, confidence primitives.ConfidenceLevel, strategy any) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	switch typed := strategy.(type) {
+	case PollAndCache:
+		c.polling = append(c.polling, newCacheKey(readIdentifier, confidence, typed.Params, 0))
+	case TriggerAndCache:
+		c.triggers = append(c.triggers, newCacheKey(readIdentifier, confidence, typed.Params, 0))
+	case FallthroughOnStale, NoCache:
+		// nothing to set up
+	default:
+		return ErrInvalidStrategy
+	}
+
+	c.setStrategy(readIdentifier, strategy)
+
+	return nil
+}
+
+func (c *CachedContractReader) TriggerWithContext(ctx context.Context) {
+	c.mu.RLock()
+	defer c.mu.Unlock()
+
+	for _, key := range c.triggers {
+		if ctx.Err() != nil {
+			break
+		}
+
+		config, ok := c.getStrategy(key.readIdentifier)
+		if !ok {
+			continue
+		}
+
+		triggerConfig := config.(TriggerAndCache)
+
+		_ = c.fallThroughAndCache(ctx, key, triggerConfig.Value)
+	}
+}
+
+func (c *CachedContractReader) getFromCache(key cacheKey, returnVal any) error {
+	c.dataLock.RLock()
+	data, hasData := c.data[key]
+	c.dataLock.RUnlock()
+
+	if !hasData {
+		return ErrNotExist
+	}
+
+	if key.staleness > 0 {
+		// do staleness check
+		if time.Since(data.LastUpdate()) > key.staleness {
+			return ErrNotExist
+		}
+	}
+
+	retVal := reflect.ValueOf(returnVal)
+	dataVal := reflect.ValueOf(data.Value())
+
+	if retVal.Type() != dataVal.Type() {
+		return fmt.Errorf("%w: cached value and return value do not have the same type", ErrTypeMismatch)
+	}
+
+	reflect.Indirect(retVal).Set(reflect.Indirect(dataVal))
+
+	return nil
+}
+
+func (c *CachedContractReader) fallThroughAndCache(ctx context.Context, key cacheKey, returnVal any) error {
+	data := c.mustGetData(key)
+
+	data.SetLastUpdate(time.Now())
+
+	retBaseType := reflect.Indirect(reflect.New(reflect.Indirect(reflect.ValueOf(returnVal)).Type()))
+	savedVal := reflect.New(retBaseType.Type())
+
+	reflect.Indirect(savedVal).Set(retBaseType)
+
+	cachedValue := savedVal.Interface()
+
+	if err := c.fallThrough(ctx, key.readIdentifier, key.confidence, key.params, cachedValue); err != nil {
+		return err
+	}
+
+	reflect.Indirect(reflect.ValueOf(returnVal)).Set(reflect.Indirect(reflect.ValueOf(cachedValue)))
+	data.SetValue(cachedValue)
+
+	return nil
+}
+
+func (c *CachedContractReader) fallThrough(
+	ctx context.Context,
+	readIdentifier string,
+	confidenceLevel primitives.ConfidenceLevel,
+	params, returnVal any,
+) error {
+	return c.base.GetLatestValue(ctx, readIdentifier, confidenceLevel, params, returnVal)
+}
+
+func (c *CachedContractReader) startSubproc(fn func(), frequency time.Duration) {
+	c.subprocs = append(c.subprocs, make(chan struct{}))
+
+	go func(chClose <-chan struct{}, fn func(), freq time.Duration) {
+		for {
+			timer := time.NewTimer(utils.WithJitter(freq))
+
+			select {
+			case <-timer.C:
+				timer.Stop()
+				fn()
+			case <-chClose:
+				timer.Stop()
+
+				return
+			}
+		}
+	}(c.subprocs[len(c.subprocs)-1], fn, frequency)
+}
+
+func (c *CachedContractReader) clean() {
+	forRemoval := make([]cacheKey, 0)
+
+	c.dataLock.RLock()
+
+	for key, data := range c.data {
+		if key.staleness == 0 {
+			continue
+		}
+
+		if time.Since(data.LastUpdate()) > key.staleness {
+			forRemoval = append(forRemoval, key)
+		}
+	}
+
+	c.dataLock.RUnlock()
+
+	c.dataLock.Lock()
+
+	for _, key := range forRemoval {
+		delete(c.data, key)
+	}
+
+	c.dataLock.Unlock()
+}
+
+func (c *CachedContractReader) pollAndCache() {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	ctx, cancel := context.WithTimeout(context.Background(), pollInterval)
+
+	for _, key := range c.polling {
+		if ctx.Err() != nil {
+			break
+		}
+
+		config, ok := c.getStrategy(key.readIdentifier)
+		if !ok {
+			continue
+		}
+
+		pollConfig := config.(PollAndCache)
+		data := c.mustGetData(key)
+
+		if time.Since(data.LastUpdate()) < pollConfig.Interval {
+			continue
+		}
+
+		_ = c.fallThroughAndCache(ctx, key, pollConfig.Value)
+	}
+
+	cancel()
+}
+
+func (c *CachedContractReader) getStrategy(readIdentifier string) (any, bool) {
+	c.strategyLock.RLock()
+	defer c.strategyLock.RUnlock()
+
+	strategy, ok := c.strategies[readIdentifier]
+
+	return strategy, ok
+}
+
+func (c *CachedContractReader) setStrategy(readIdentifier string, strategy any) {
+	c.strategyLock.Lock()
+	defer c.strategyLock.Unlock()
+
+	c.strategies[readIdentifier] = strategy
+}
+
+func (c *CachedContractReader) mustGetData(key cacheKey) *cachedValue {
+	c.dataLock.RLock()
+	data, hasData := c.data[key]
+	c.dataLock.RUnlock()
+
+	if !hasData {
+		data = &cachedValue{}
+
+		c.dataLock.Lock()
+		c.data[key] = data
+		c.dataLock.Unlock()
+	}
+
+	return data
+}
+
+func newCacheKey(readIdentifier string, confidence primitives.ConfidenceLevel, params any, staleness time.Duration) cacheKey {
+	return cacheKey{
+		readIdentifier: readIdentifier,
+		confidence:     confidence,
+		params:         params,
+		staleness:      staleness,
+	}
+}
+
+type cacheKey struct {
+	readIdentifier string
+	confidence     primitives.ConfidenceLevel
+	params         any
+	staleness      time.Duration
+}
+
+func (k cacheKey) String() string {
+	return fmt.Sprintf("%s::%s::%+v", k.readIdentifier, reflect.TypeOf(k.params), k.params)
+}
+
+type cachedValue struct {
+	value      any
+	lastUpdate time.Time
+	mu         sync.RWMutex
+}
+
+func (v *cachedValue) LastUpdate() time.Time {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	return v.lastUpdate
+}
+
+func (v *cachedValue) SetLastUpdate(updated time.Time) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	v.lastUpdate = updated
+}
+
+func (v *cachedValue) Value() any {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	return v.value
+}
+
+func (v *cachedValue) SetValue(value any) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	v.value = value
+}
---
+++ pkg/types/contract_reader_cache_test.go
@@ -0,0 +1,251 @@
+package types_test
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/types"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/query"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/query/primitives"
+	"github.com/smartcontractkit/chainlink-common/pkg/utils/tests"
+)
+
+var (
+	testAddress  = "0x42"
+	testContract = "anyContract"
+	testReadName = "readName"
+	testBinding  = types.BoundContract{
+		Name:    testContract,
+		Address: testAddress,
+	}
+)
+
+func TestCachedContractReader_DefaultFallthrough(t *testing.T) {
+	t.Parallel()
+
+	mcr := newTestReader()
+	crCache := types.NewCachedContractReader(mcr)
+	ctx := tests.Context(t)
+
+	var output int64
+
+	err := crCache.GetLatestValue(
+		ctx,
+		testBinding.ReadIdentifier(testReadName),
+		primitives.Finalized,
+		map[string]any{},
+		&output,
+	)
+
+	require.ErrorIs(t, err, types.ErrInvalidEncoding)
+}
+
+func TestContractReader_FallthroughOnStale(t *testing.T) {
+	t.Parallel()
+
+	mcr := newTestReader()
+	crCache := types.NewCachedContractReader(mcr)
+	ctx := tests.Context(t)
+
+	strategy := types.FallthroughOnStale{
+		Staleness: 5 * time.Second,
+	}
+
+	readIdentifier := testBinding.ReadIdentifier(testReadName)
+
+	// setting the cache strategy should not return an error
+	require.NoError(t, crCache.SetCacheStrategy(readIdentifier, primitives.Finalized, strategy))
+
+	callFunc := func(t *testing.T, val int64, callCount int) {
+		t.Helper()
+
+		var output map[string]any
+
+		params := map[string]any{"param1": "value1"}
+
+		require.NoError(t, crCache.GetLatestValue(ctx, readIdentifier, primitives.Finalized, params, &output))
+		require.NoError(t, crCache.GetLatestValue(ctx, readIdentifier, primitives.Finalized, params, &output))
+		require.Equal(t, callCount, mcr.CallCount())
+
+		assert.Equal(t, val, output)
+	}
+
+	callFunc(t, 8, 1)
+	time.Sleep(strategy.Staleness)
+	callFunc(t, 9, 2)
+}
+
+func TestContractReader_PollAndCache(t *testing.T) {
+	t.Parallel()
+
+	mcr := newTestReader()
+	crCache := types.NewCachedContractReader(mcr)
+	ctx := tests.Context(t)
+
+	strategy := types.PollAndCache{
+		TriggerAndCache: types.TriggerAndCache{
+			Params: map[string]any{"param1": "value1"},
+			Value:  reflect.New(reflect.TypeOf(map[string]any{})).Interface(),
+		},
+		Interval: 5 * time.Second,
+	}
+
+	require.NoError(t, crCache.SetCacheStrategy(testBinding.ReadIdentifier(testReadName), primitives.Finalized, strategy))
+
+	t.Cleanup(func() {
+		require.NoError(t, crCache.Close())
+	})
+
+	require.NoError(t, crCache.Start(ctx))
+
+	// wait for a single polling interval
+	time.Sleep(time.Second)
+	callAndAssert(ctx, t, crCache, mcr, 1, map[string]any{"ret1": "latestValue1"}, strategy.Params)
+
+	// request with different params falls through
+	callAndAssert(ctx, t, crCache, mcr, 2, map[string]any{"ret2": "latestValue2"}, map[string]any{"param2": "value2"})
+	time.Sleep(strategy.Interval)
+
+	// after polling interval
+	callAndAssert(ctx, t, crCache, mcr, 3, map[string]any{"ret1": "latestValue1"}, strategy.Params)
+}
+
+func callAndAssert(
+	ctx context.Context,
+	t *testing.T,
+	crCache *types.CachedContractReader,
+	mcr *staticContractReader,
+	expectedCount int,
+	expected any,
+	params any,
+) {
+	t.Helper()
+
+	var output map[string]any
+
+	require.NoError(t, crCache.GetLatestValue(ctx, testBinding.ReadIdentifier(testReadName), primitives.Finalized, params, &output))
+	assert.Equal(t, expected, output)
+	assert.Equal(t, expectedCount, mcr.CallCount())
+}
+
+func newTestReader() *staticContractReader {
+	// ContractReader is a static implementation of [types.ContractReader], [testtypes.Evaluator] and [types.PluginProvider
+	// it is used for testing the [types.PluginProvider] interface
+	return &staticContractReader{
+		address:        testAddress,
+		contractName:   testContract,
+		contractMethod: testReadName,
+		latestValue:    map[string]any{"ret1": "latestValue1", "ret2": "latestValue2"},
+		params:         map[string]any{"param1": "value1", "param2": "value2"},
+	}
+}
+
+// staticContractReader is a static implementation of ContractReaderTester
+type staticContractReader struct {
+	types.UnimplementedContractReader
+
+	address        string
+	contractName   string
+	contractMethod string
+	latestValue    map[string]any
+	params         map[string]any
+
+	callCount int
+}
+
+var _ types.ContractReader = &staticContractReader{}
+
+func (c staticContractReader) Start(_ context.Context) error { return nil }
+
+func (c staticContractReader) Close() error { return nil }
+
+func (c staticContractReader) Ready() error { panic("unimplemented") }
+
+func (c staticContractReader) Name() string { panic("unimplemented") }
+
+func (c staticContractReader) HealthReport() map[string]error { panic("unimplemented") }
+
+func (c staticContractReader) Bind(_ context.Context, _ []types.BoundContract) error {
+	return nil
+}
+
+func (c staticContractReader) Unbind(_ context.Context, _ []types.BoundContract) error {
+	return nil
+}
+
+func (c *staticContractReader) GetLatestValue(_ context.Context, readName string, _ primitives.ConfidenceLevel, params, returnVal any) error {
+	c.callCount++
+	comp := types.BoundContract{
+		Address: c.address,
+		Name:    c.contractName,
+	}.ReadIdentifier(c.contractMethod)
+
+	if !assert.ObjectsAreEqual(readName, comp) {
+		return fmt.Errorf("%w: expected report context %v but got %v", types.ErrInvalidType, comp, readName)
+	}
+
+	//gotParams, ok := params.(*map[string]string)
+	gotParams, ok := params.(*map[string]any)
+	if !ok {
+		return fmt.Errorf("%w: Invalid parameter type received in GetLatestValue. Expected %T but received %T", types.ErrInvalidEncoding, c.params, params)
+	}
+	if (*gotParams)["param1"] != c.params["param1"] || (*gotParams)["param2"] != c.params["param2"] {
+		return fmt.Errorf("%w: Wrong params value received in GetLatestValue. Expected %v but received %v", types.ErrInvalidEncoding, c.params, *gotParams)
+	}
+
+	ret, ok := returnVal.(*map[string]any)
+	if !ok {
+		return fmt.Errorf("%w: Wrong type passed for retVal param to GetLatestValue impl (expected %T instead of %T", types.ErrInvalidType, c.latestValue, returnVal)
+	}
+
+	if _, ok := (*gotParams)["param1"]; ok {
+		(*ret)["ret1"] = c.latestValue["ret1"]
+	}
+
+	if _, ok := (*gotParams)["param2"]; ok {
+		(*ret)["ret2"] = c.latestValue["ret2"]
+	}
+
+	return nil
+}
+
+func (c *staticContractReader) BatchGetLatestValues(_ context.Context, _ types.BatchGetLatestValuesRequest) (types.BatchGetLatestValuesResult, error) {
+	return nil, nil
+}
+
+func (c staticContractReader) QueryKey(_ context.Context, _ types.BoundContract, _ query.KeyFilter, _ query.LimitAndSort, _ any) ([]types.Sequence, error) {
+	return nil, nil
+}
+
+func (c *staticContractReader) CallCount() int {
+	return c.callCount
+}
+
+func (c staticContractReader) Evaluate(ctx context.Context, cr types.ContractReader) error {
+	gotLatestValue := make(map[string]any)
+
+	if err := cr.GetLatestValue(
+		ctx,
+		types.BoundContract{
+			Address: c.address,
+			Name:    c.contractName,
+		}.ReadIdentifier(c.contractMethod),
+		primitives.Unconfirmed,
+		&c.params,
+		&gotLatestValue,
+	); err != nil {
+		return fmt.Errorf("failed to call GetLatestValue(): %w", err)
+	}
+
+	if !assert.ObjectsAreEqual(gotLatestValue, c.latestValue) {
+		return fmt.Errorf("GetLatestValue: expected %v but got %v", c.latestValue, gotLatestValue)
+	}
+
+	return nil
+}

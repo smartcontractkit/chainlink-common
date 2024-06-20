1ab0cac7e83827e55a8c8699c2f1e2457271c890
Jordan Krage <jmank88@gmail.com>
2024-06-20 11:22:14 -0500

    mockery: use config file instead of generate statements

--- pkg/types/core/capabilities_registry.go
+++ pkg/types/core/capabilities_registry.go
@@ -6,7 +6,6 @@ import (
 	"github.com/smartcontractkit/chainlink-common/pkg/capabilities"
 )
 
-//go:generate mockery --quiet --name CapabilitiesRegistry --output ./mocks/ --case=underscore
 type CapabilitiesRegistry interface {
 	Get(ctx context.Context, ID string) (capabilities.BaseCapability, error)
 	GetTrigger(ctx context.Context, ID string) (capabilities.TriggerCapability, error)

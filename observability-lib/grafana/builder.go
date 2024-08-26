211450d46ca8fce85d23ee4a01e8cff6ddf36d58
Clement <clement.erena78@gmail.com>
2024-08-26 11:45:13 +0200

    feat(observability-lib): refactor + add alerting layer (#703)
    * feat(observability-lib): refactor + add alerting layer
    
    * feat(observability-lib): creating notification templates
    
    * feat(observability-lib): handle creating contact points
    
    * feat(observability-lib): handle notification policies
    
    * feat(observability-lib): slack notification template

---
+++ observability-lib/grafana/builder.go
@@ -0,0 +1,146 @@
+package grafana
+
+import (
+	"maps"
+
+	"github.com/grafana/grafana-foundation-sdk/go/alerting"
+	"github.com/grafana/grafana-foundation-sdk/go/cog"
+	"github.com/grafana/grafana-foundation-sdk/go/common"
+	"github.com/grafana/grafana-foundation-sdk/go/dashboard"
+)
+
+type Builder struct {
+	dashboardBuilder            *dashboard.DashboardBuilder
+	alertsBuilder               []*alerting.RuleBuilder
+	contactPointsBuilder        []*alerting.ContactPointBuilder
+	notificationPoliciesBuilder []*alerting.NotificationPolicyBuilder
+	panelCounter                uint32
+	alertsTags                  map[string]string
+}
+
+type BuilderOptions struct {
+	Tags     []string
+	Refresh  string
+	TimeFrom string
+	TimeTo   string
+	TimeZone string
+}
+
+func NewBuilder(options *DashboardOptions, builderOptions *BuilderOptions) *Builder {
+	if builderOptions.TimeZone == "" {
+		builderOptions.TimeZone = common.TimeZoneBrowser
+	}
+
+	return &Builder{
+		dashboardBuilder: dashboard.NewDashboardBuilder(options.Name).
+			Tags(builderOptions.Tags).
+			Refresh(builderOptions.Refresh).
+			Time(builderOptions.TimeFrom, builderOptions.TimeTo).
+			Timezone(builderOptions.TimeZone),
+		alertsTags: options.AlertsTags,
+	}
+}
+
+func (b *Builder) AddVars(items ...cog.Builder[dashboard.VariableModel]) {
+	for _, item := range items {
+		b.dashboardBuilder.WithVariable(item)
+	}
+}
+
+func (b *Builder) AddRow(title string) {
+	b.dashboardBuilder.WithRow(dashboard.NewRowBuilder(title))
+}
+
+func (b *Builder) GetPanelCounter() uint32 {
+	res := b.panelCounter
+	b.panelCounter = Inc(&b.panelCounter)
+	return res
+}
+
+func (b *Builder) AddPanel(panel ...*Panel) {
+	for _, item := range panel {
+		panelID := b.GetPanelCounter()
+		if item.statPanelBuilder != nil {
+			item.statPanelBuilder.Id(panelID)
+			b.dashboardBuilder.WithPanel(item.statPanelBuilder)
+		} else if item.timeSeriesPanelBuilder != nil {
+			item.timeSeriesPanelBuilder.Id(panelID)
+			b.dashboardBuilder.WithPanel(item.timeSeriesPanelBuilder)
+		} else if item.gaugePanelBuilder != nil {
+			item.gaugePanelBuilder.Id(panelID)
+			b.dashboardBuilder.WithPanel(item.gaugePanelBuilder)
+		} else if item.tablePanelBuilder != nil {
+			item.tablePanelBuilder.Id(panelID)
+			b.dashboardBuilder.WithPanel(item.tablePanelBuilder)
+		} else if item.logPanelBuilder != nil {
+			item.logPanelBuilder.Id(panelID)
+			b.dashboardBuilder.WithPanel(item.logPanelBuilder)
+		}
+		if item.alertBuilder != nil {
+			b.alertsBuilder = append(b.alertsBuilder, item.alertBuilder)
+		}
+	}
+}
+
+func (b *Builder) AddContactPoint(contactPoints ...*alerting.ContactPointBuilder) {
+	b.contactPointsBuilder = append(b.contactPointsBuilder, contactPoints...)
+}
+
+func (b *Builder) AddNotificationPolicy(notificationPolicies ...*alerting.NotificationPolicyBuilder) {
+	b.notificationPoliciesBuilder = append(b.notificationPoliciesBuilder, notificationPolicies...)
+}
+
+func (b *Builder) Build() (*Dashboard, error) {
+	db, errBuildDashboard := b.dashboardBuilder.Build()
+	if errBuildDashboard != nil {
+		return nil, errBuildDashboard
+	}
+
+	var alerts []alerting.Rule
+	for _, alertBuilder := range b.alertsBuilder {
+		alert, errBuildAlert := alertBuilder.Build()
+		if errBuildAlert != nil {
+			return nil, errBuildAlert
+		}
+
+		// Add common tags to alerts
+		if b.alertsTags != nil && len(b.alertsTags) > 0 {
+			tags := maps.Clone(b.alertsTags)
+			maps.Copy(tags, alert.Labels)
+
+			alertBuildWithTags := alertBuilder.Labels(tags)
+			alertWithTags, errBuildAlertWithTags := alertBuildWithTags.Build()
+			if errBuildAlertWithTags != nil {
+				return nil, errBuildAlertWithTags
+			}
+			alerts = append(alerts, alertWithTags)
+		} else {
+			alerts = append(alerts, alert)
+		}
+	}
+
+	var contactPoints []alerting.ContactPoint
+	for _, contactPointBuilder := range b.contactPointsBuilder {
+		contactPoint, errBuildContactPoint := contactPointBuilder.Build()
+		if errBuildContactPoint != nil {
+			return nil, errBuildContactPoint
+		}
+		contactPoints = append(contactPoints, contactPoint)
+	}
+
+	var notificationPolicies []alerting.NotificationPolicy
+	for _, notificationPolicyBuilder := range b.notificationPoliciesBuilder {
+		notificationPolicy, errBuildNotificationPolicy := notificationPolicyBuilder.Build()
+		if errBuildNotificationPolicy != nil {
+			return nil, errBuildNotificationPolicy
+		}
+		notificationPolicies = append(notificationPolicies, notificationPolicy)
+	}
+
+	return &Dashboard{
+		Dashboard:            &db,
+		Alerts:               alerts,
+		ContactPoints:        contactPoints,
+		NotificationPolicies: notificationPolicies,
+	}, nil
+}
--- observability-lib/k8s-resources/component_test.go
+++ observability-lib/k8s-resources/component_test.go
@@ -3,42 +3,23 @@ package k8sresources_test
 import (
 	"testing"
 
-	"github.com/grafana/grafana-foundation-sdk/go/cog"
-	"github.com/grafana/grafana-foundation-sdk/go/common"
-	"github.com/grafana/grafana-foundation-sdk/go/dashboard"
-
-	"github.com/smartcontractkit/chainlink-common/observability-lib/utils"
+	"github.com/smartcontractkit/chainlink-common/observability-lib/grafana"
+	k8sresources "github.com/smartcontractkit/chainlink-common/observability-lib/k8s-resources"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestBuildDashboard(t *testing.T) {
-	t.Run("BuildDashboard creates a dashboard", func(t *testing.T) {
-		builder := dashboard.NewDashboardBuilder("test")
-		utils.AddPanels(builder, []cog.Builder[dashboard.Panel]{
-			utils.StatPanel(
-				"Prometheus",
-				"Test",
-				"Test",
-				1,
-				1,
-				1,
-				"",
-				common.BigValueColorModeNone,
-				common.BigValueGraphModeNone,
-				common.BigValueTextModeName,
-				common.VizOrientationHorizontal,
-				utils.PrometheusQuery{
-					Query:  `test`,
-					Legend: "{{test}}",
-				}),
-		})
-
-		testBuild, err := builder.Build()
-		if err != nil {
-			t.Errorf("Error building dashboard: %v", err)
+func TestNewDashboard(t *testing.T) {
+	t.Run("NewDashboard creates a dashboard", func(t *testing.T) {
+		options := grafana.DashboardOptions{
+			Name:              "K8s resources",
+			Platform:          "kubernetes",
+			MetricsDataSource: grafana.NewDataSource("Prometheus", ""),
 		}
-
-		require.IsType(t, dashboard.Dashboard{}, testBuild)
+		testDashboard, err := k8sresources.NewDashboard(&options)
+		if err != nil {
+			t.Errorf("Error creating dashboard: %v", err)
+		}
+		require.IsType(t, grafana.Dashboard{}, *testDashboard)
 	})
 }

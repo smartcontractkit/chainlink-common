b31801cf080a7ff2231c91080b6aae5528b943a9
Ryan Tinianov <tinianov@live.com>
2024-08-27 12:36:40 -0400

    Return collection of errors from write files

--- pkg/utils/codegen/utils.go
+++ pkg/utils/codegen/utils.go
@@ -1,6 +1,7 @@
 package codegen
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path"
@@ -10,6 +11,7 @@ import (
 )
 
 func WriteFiles(dir, localPrefix string, files map[string]string) error {
+	var errs []error
 	for file, content := range files {
 		if !strings.HasPrefix(file, dir) {
 			file = dir + "/" + file
@@ -21,19 +23,22 @@ func WriteFiles(dir, localPrefix string, files map[string]string) error {
 			if err != nil {
 				// print an error, but also write the file so debugging the generator isn't a pain.
 				fmt.Printf("Error formatting file %s: %s\n", file, err)
+				errs = append(errs, err)
 			} else {
 				content = string(rawContent)
 			}
 		}
 
 		if err := os.MkdirAll(path.Dir(file), 0600); err != nil {
-			return err
-		}
-
-		if err := os.WriteFile(file, []byte(content), 0600); err != nil {
-			return err
+			errs = append(errs, err)
+		} else if err = os.WriteFile(file, []byte(content), 0600); err != nil {
+			errs = append(errs, err)
 		}
 	}
 
-	return nil
+	if len(errs) == 0 {
+		return nil
+	}
+
+	return errors.Join(errs...)
 }

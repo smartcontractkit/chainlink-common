b45e43fb2f9facd358faa2e71f703e02d0b1b0ee
Jordan Krage <jmank88@gmail.com>
2024-08-30 16:42:07 +0200

    switch to grpc-proxy fork; rm replace (#745)

--- pkg/loop/internal/relayerset/server.go
+++ pkg/loop/internal/relayerset/server.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/mwitkow/grpc-proxy/proxy"
+	"github.com/smartcontractkit/grpc-proxy/proxy"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"

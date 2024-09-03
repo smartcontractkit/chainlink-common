ca100dc1d3eee5b9483496c51b34045679dfa7b2
github-action-benchmark <github@users.noreply.github.com>
2024-09-03 18:42:53 +0000

    add Benchmark (go) benchmark result for 6488292a85e36d58e832b6de7af75fbecd035d22

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725298994095,
+  "lastUpdate": 1725388973735,
   "repoUrl": "https://github.com/smartcontractkit/chainlink-common",
   "entries": {
     "Benchmark": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 28207,
             "unit": "ns/op",
             "extra": "42346 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "121895364+martin-cll@users.noreply.github.com",
+            "name": "martin-cll",
+            "username": "martin-cll"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6488292a85e36d58e832b6de7af75fbecd035d22",
+          "message": "MERC-6190: Remove bid/ask fields from Mercury v4 schema (#736)\n\n* Remove bid/ask fields from Mercury v4 schema\r\n\r\n* Add back deleted fields as deprecated",
+          "timestamp": "2024-09-03T14:42:00-04:00",
+          "tree_id": "0580aaed6f02476864e3a5f436cdcd5b725fec0d",
+          "url": "https://github.com/smartcontractkit/chainlink-common/commit/6488292a85e36d58e832b6de7af75fbecd035d22"
+        },
+        "date": 1725388973376,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkKeystore_Sign/nop/in-process",
+            "value": 453.5,
+            "unit": "ns/op",
+            "extra": "2213182 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/hex/in-process",
+            "value": 513.2,
+            "unit": "ns/op",
+            "extra": "2316528 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/ed25519/in-process",
+            "value": 28276,
+            "unit": "ns/op",
+            "extra": "42405 times\n4 procs"
           }
         ]
       }

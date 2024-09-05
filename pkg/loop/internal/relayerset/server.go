dbcee02b229727d0aed75b6846e3e7e0f5fe4882
Sri Kidambi <1702865+kidambisrinivas@users.noreply.github.com>
2024-09-05 13:30:56 +0100

    Add documentation and comments

--- pkg/loop/internal/relayerset/server.go
+++ pkg/loop/internal/relayerset/server.go
@@ -139,6 +139,21 @@ func (s *Server) NewPluginProvider(ctx context.Context, req *relayerset.NewPlugi
 	return &relayerset.NewPluginProviderResponse{PluginProviderId: providerID}, nil
 }
 
+// RelayerSet is supposed to serve relayers, which then hold a ContractReader and ChainWriter. Serving NewContractReader
+// and NewChainWriter from RelayerSet is a way to save us from instantiating an extra server for the Relayer. Without
+// this approach, the calls we would make normally are
+//   - RelayerSet.Get -> Relayer
+//   - Relayer.NewContractReader -> ContractReader
+//
+// We could translate this to the GRPC world by having each call to RelayerSet.Get wrap the returned relayer in a server
+// and register that to the GRPC server. However this is actually pretty inefficient since a relayer object on its own
+// is not useful. Users will always want to use the relayer to instantiate a chainreader or chainwriter. So we can avoid
+// the intermediate server for the relayer by just storing a reference to the relayerSet client and the relayer we want
+// to fetch. I.e. the calls described above instead would become:
+//   - RelayerSet.Get -> (RelayerSetClient, RelayerID). Effectively this call just acts as check that Relayer exists
+//
+// RelayerClient.NewContractReader -> This is a call to RelayerSet.NewContractReader with (relayerID, []contractReaderConfig);
+// The implementation will then fetch the relayer and call NewContractReader on it
 func (s *Server) NewContractReader(ctx context.Context, req *relayerset.NewContractReaderRequest) (*relayerset.NewContractReaderResponse, error) {
 	relayer, err := s.getRelayer(ctx, req.RelayerId)
 	if err != nil {
@@ -167,6 +182,21 @@ func (s *Server) NewContractReader(ctx context.Context, req *relayerset.NewContr
 	return &relayerset.NewContractReaderResponse{ContractReaderId: id}, nil
 }
 
+// RelayerSet is supposed to serve relayers, which then hold a ContractReader and ChainWriter. Serving NewChainWriter
+// and NewChainWriter from RelayerSet is a way to save us from instantiating an extra server for the Relayer. Without
+// this approach, the calls we would make normally are
+//   - RelayerSet.Get -> Relayer
+//   - Relayer.NewChainWriter -> ChainWriter
+//
+// We could translate this to the GRPC world by having each call to RelayerSet.Get wrap the returned relayer in a server
+// and register that to the GRPC server. However this is actually pretty inefficient since a relayer object on its own
+// is not useful. Users will always want to use the relayer to instantiate a chainreader or chainwriter. So we can avoid
+// the intermediate server for the relayer by just storing a reference to the relayerSet client and the relayer we want
+// to fetch. I.e. the calls described above instead would become:
+//   - RelayerSet.Get -> (RelayerSetClient, RelayerID). Effectively this call just acts as check that Relayer exists
+//
+// RelayerClient.NewChainWriter -> This is a call to RelayerSet.NewChainWriter with (relayerID, []contractReaderConfig);
+// The implementation will then fetch the relayer and call NewChainWriter on it
 func (s *Server) NewChainWriter(ctx context.Context, req *relayerset.NewChainWriterRequest) (*relayerset.NewChainWriterResponse, error) {
 	relayer, err := s.getRelayer(ctx, req.RelayerId)
 	if err != nil {

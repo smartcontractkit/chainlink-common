09f6f028ae5bd37a38ec2206c69cd30d41765765
Ryan Tinianov <tinianov@live.com>
2024-09-12 18:05:04 -0400

    Attempt to clear CGO flags, except ffile-prefix-map

--- pkg/workflows/wasm/host/test/generate_wasm.sh
+++ pkg/workflows/wasm/host/test/generate_wasm.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 go install golang.org/dl/go1.22.7@latest
 go1.22.7 download
-# GO111MODULE must be the same to keep the build reproducible
-# We shouldn't need CGO, so disabling it assures we don't need have consistent GOGCCFLAGS
-GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 go1.22.7 env
-GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 go1.22.7 build -o ./test/cmd/testmodule.wasm ./test/cmd/main.go
+
+# leave the -ffile-prefix-map= field for $GOGCCFLAGS so that symbol tables are changed to /tmp/go-build by default
+# otherwise the table will vary based on file location.
+# Disable all other flags to make a consistent build.
+FFILE_PREFIX_MAP=$(echo $GOGCCFLAGS | grep -oP '-ffile-prefix-map=[^ ]+')
+GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 GONOPROXY='' GONOSUMDB='' GOGCCFLAGS="$FFILE_PREFIX_MAP" go1.22.7 env
+GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 GONOPROXY='' GONOSUMDB='' GOGCCFLAGS="$FFILE_PREFIX_MAP" go1.22.7 build -o ./test/cmd/testmodule.wasm ./test/cmd/main.go

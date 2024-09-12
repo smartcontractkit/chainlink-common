33456695f928cd679e9fcd99a9dc840e6bde1118
Jordan Krage <jmank88@gmail.com>
2024-09-12 09:24:23 -0500

    make generate w/ explicit 1.22.7

--- pkg/workflows/wasm/host/test/generate_wasm.sh
+++ pkg/workflows/wasm/host/test/generate_wasm.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-GOOS=wasip1 GOARCH=wasm go build -o ./test/cmd/testmodule.wasm ./test/cmd/main.go
+go install golang.org/dl/go1.22.7@latest
+go1.22.7 download
+GOOS=wasip1 GOARCH=wasm go1.22.7 build -o ./test/cmd/testmodule.wasm ./test/cmd/main.go

40150a50d7871f0db4efbd290df4ff7e21acc7ba
Ryan Tinianov <tinianov@live.com>
2024-09-12 17:23:17 -0400

    Add env call to see if something is different that causes a different binary

--- pkg/workflows/wasm/host/test/generate_wasm.sh
+++ pkg/workflows/wasm/host/test/generate_wasm.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 go install golang.org/dl/go1.22.7@latest
 go1.22.7 download
+GOOS=wasip1 GOARCH=wasm go1.22.7 env
 GOOS=wasip1 GOARCH=wasm go1.22.7 build -o ./test/cmd/testmodule.wasm ./test/cmd/main.go

0869cddb91a9bc8c9cf715b24515f611c43db50c
Ryan Tinianov <tinianov@live.com>
2024-09-12 17:47:38 -0400

    try to line environment variables up better

--- pkg/workflows/wasm/host/test/generate_wasm.sh
+++ pkg/workflows/wasm/host/test/generate_wasm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 go install golang.org/dl/go1.22.7@latest
 go1.22.7 download
-GOOS=wasip1 GOARCH=wasm go1.22.7 env
-GOOS=wasip1 GOARCH=wasm go1.22.7 build -o ./test/cmd/testmodule.wasm ./test/cmd/main.go
+# GO111MODULE must be the same to keep the build reproducible
+# We shouldn't need CGO, so disabling it assures we don't need have consistent GOGCCFLAGS
+GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 go1.22.7 env
+GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 go1.22.7 build -o ./test/cmd/testmodule.wasm ./test/cmd/main.go

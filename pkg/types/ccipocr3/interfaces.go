e90bd952035205f64eb4e6be7cf916974a5fe729
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-09-13 12:42:21 +0300

    add comments

--- pkg/types/ccipocr3/interfaces.go
+++ pkg/types/ccipocr3/interfaces.go
@@ -18,6 +18,9 @@ type MessageHasher interface {
 	Hash(context.Context, Message) (Bytes32, error)
 }
 
+// RMNCrypto provides a chain-agnostic interface for verifying RMN signatures.
+// For example, on EVM, RMN reports are abi-encoded prior to being signed.
+// On Solana, they would be borsh encoded instead, etc.
 type RMNCrypto interface {
 	// VerifyReportSignatures verifies each provided signature against the provided report and the signer addresses.
 	// If any signature is invalid (no matching signer address is found), an error is returned immediately.
--- pkg/types/ccipocr3/rmn_types.go
+++ pkg/types/ccipocr3/rmn_types.go
@@ -1,5 +1,6 @@
 package ccipocr3
 
+// RMNReport is the payload that is signed by the RMN nodes, transmitted and verified onchain.
 type RMNReport struct {
 	ReportVersion               string // e.g. "RMN_V1_6_ANY2EVM_REPORT".
 	DestChainID                 BigInt // If applies, a chain specific id, e.g. evm chain id otherwise empty.
@@ -10,6 +11,8 @@ type RMNReport struct {
 	LaneUpdates                 []RMNLaneUpdate
 }
 
+// RMNLaneUpdate represents an interval that has been observed by an RMN node.
+// It is part of the payload that is signed and transmitted onchain.
 type RMNLaneUpdate struct {
 	SourceChainSelector ChainSelector
 	OnRampAddress       Bytes // (for EVM should be abi-encoded)
@@ -18,6 +21,7 @@ type RMNLaneUpdate struct {
 	MerkleRoot          Bytes32
 }
 
+// // RMNECDSASignature represents the signature provided by RMN on the RMNReport structure.
 type RMNECDSASignature struct {
 	R Bytes32 `json:"r"`
 	S Bytes32 `json:"s"`

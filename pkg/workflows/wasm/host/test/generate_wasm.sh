ed80ba2bedcdbc0c591a458a7303581d5b9890b5
Ryan Tinianov <tinianov@live.com>
2024-09-13 13:40:01 -0400

    add trimp path and remove build id from

--- pkg/workflows/wasm/host/test/generate_wasm.sh
+++ pkg/workflows/wasm/host/test/generate_wasm.sh
@@ -2,5 +2,4 @@
 go install golang.org/dl/go1.22.7@latest
 go1.22.7 download
 
-GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 GONOPROXY='' GONOSUMDB='' GOGCCFLAGS="-fPIC -fno-caret-diagnostics -Qunused-arguments -fmessage-length=0 -ffile-prefix-map=/path/to/source=/tmp/build -gno-record-gcc-switches" go1.22.7 env
-GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 GONOPROXY='' GONOSUMDB='' GOGCCFLAGS="-fPIC -fno-caret-diagnostics -Qunused-arguments -fmessage-length=0 -ffile-prefix-map=/path/to/source=/tmp/build -gno-record-gcc-switches"  go1.22.7 build -o ./test/cmd/testmodule.wasm ./test/cmd/main.go
+GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 GONOPROXY='' GONOSUMDB='' GOGCCFLAGS="-fPIC -fno-caret-diagnostics -Qunused-arguments -fmessage-length=0 -ffile-prefix-map=/path/to/source=/tmp/build -gno-record-gcc-switches"  go1.22.7 build -trimpath -ldflags=-buildid= -o ./test/cmd/testmodule.wasm ./test/cmd/main.go

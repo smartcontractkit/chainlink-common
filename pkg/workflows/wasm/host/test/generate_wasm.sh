7f2e8c445af05cb57e1d77c954e18b4e7671d6f7
Ryan Tinianov <tinianov@live.com>
2024-09-13 09:22:03 -0400

    more explicit CGO flags...

--- pkg/workflows/wasm/host/test/generate_wasm.sh
+++ pkg/workflows/wasm/host/test/generate_wasm.sh
@@ -2,9 +2,5 @@
 go install golang.org/dl/go1.22.7@latest
 go1.22.7 download
 
-# leave the -ffile-prefix-map= field for $GOGCCFLAGS so that symbol tables are changed to /tmp/go-build by default
-# otherwise the table will vary based on file location.
-# Disable all other flags to make a consistent build.
-FFILE_PREFIX_MAP=$(echo $GOGCCFLAGS | grep -oP '-ffile-prefix-map=[^ ]+')
-GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 GONOPROXY='' GONOSUMDB='' GOGCCFLAGS="$FFILE_PREFIX_MAP" go1.22.7 env
-GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 GONOPROXY='' GONOSUMDB='' GOGCCFLAGS="$FFILE_PREFIX_MAP" go1.22.7 build -o ./test/cmd/testmodule.wasm ./test/cmd/main.go
+GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 GONOPROXY='' GONOSUMDB='' GOGCCFLAGS="-fPIC -fno-caret-diagnostics -Qunused-arguments -fmessage-length=0 -ffile-prefix-map=/path/to/source=/tmp/build -gno-record-gcc-switches" go1.22.7 env
+GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 GONOPROXY='' GONOSUMDB='' GOGCCFLAGS="-fPIC -fno-caret-diagnostics -Qunused-arguments -fmessage-length=0 -ffile-prefix-map=/path/to/source=/tmp/build -gno-record-gcc-switches"  go1.22.7 build -o ./test/cmd/testmodule.wasm ./test/cmd/main.go

87a7ec217ea0b1a326fbbe5bedd54ef519fe1185
Ryan Tinianov <tinianov@live.com>
2024-09-13 17:05:58 -0400

    Copy flags from discord, user suggested that they make the build reproducible

--- pkg/workflows/wasm/host/test/generate_wasm.sh
+++ pkg/workflows/wasm/host/test/generate_wasm.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 go install golang.org/dl/go1.22.7@latest
 go1.22.7 download
-
-GOOS=wasip1 GOARCH=wasm GO111MODULE='' CGO_ENABLED=0 GONOPROXY='' GONOSUMDB='' GOGCCFLAGS="-fPIC -fno-caret-diagnostics -Qunused-arguments -fmessage-length=0 -ffile-prefix-map=/path/to/source=/tmp/build -gno-record-gcc-switches"  go1.22.7 build -trimpath -ldflags=-buildid= -o ./test/cmd/testmodule.wasm ./test/cmd/main.go
+GOOS=wasip1 GOARCH=wasm go1.22.7 build -trimpath -ldflags="-w -s" -tags "" -o ./test/cmd/testmodule.wasm ./test/cmd/main.go

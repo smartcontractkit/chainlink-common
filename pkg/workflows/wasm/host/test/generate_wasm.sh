9a4b2400acb3990c2ba697b2e329f4f091ed6bc2
Ryan Tinianov <tinianov@live.com>
2024-09-14 12:11:54 -0400

    Closer to reprodcible...

--- pkg/workflows/wasm/host/test/generate_wasm.sh
+++ pkg/workflows/wasm/host/test/generate_wasm.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-# Find the root of the Go module
-MODULE_ROOT=$(go list -m -f "{{.Dir}}" github.com/smartcontractkit/chainlink-common)
-
 # Install the Go version if necessary
 go install golang.org/dl/go1.22.7@latest
 go1.22.7 download
 
-# Use the module root for all paths to ensure consistency
-GOOS=wasip1 GOARCH=wasm \
-go1.22.7 install -trimpath -ldflags="-w -s -buildid= -tags test" \
-    github.com/smartcontractkit/chainlink-common/pkg/workflows/wasm/host/test/cmd
+GOOS=wasip1 GOARCH=wasm go env
 
-# Move the output binary to the desired location, using the module root for consistent paths
-mv "$(go env GOPATH)/bin/wasip1_wasm/cmd" "$MODULE_ROOT/pkg/workflows/wasm/host/test/cmd/testmodule.wasm"
+GOOS=wasip1 GOARCH=wasm \
+go1.22.7 build -trimpath -ldflags="-w -s -buildid=" -tags "wasip1" \
+    -o testmodule.wasm github.com/smartcontractkit/chainlink-common/pkg/workflows/wasm/host/test/cmd

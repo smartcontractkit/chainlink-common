da56beab49533c47e36ae1a4c8bcecead52eb5f6
Ryan Tinianov <tinianov@live.com>
2024-09-14 23:11:15 -0400

    Attempt new flags

--- pkg/workflows/wasm/host/test/generate_wasm.sh
+++ pkg/workflows/wasm/host/test/generate_wasm.sh
@@ -4,8 +4,6 @@
 go install golang.org/dl/go1.22.7@latest
 go1.22.7 download
 
-GOOS=wasip1 GOARCH=wasm go env
-
 GOOS=wasip1 GOARCH=wasm \
-go1.22.7 build -trimpath -ldflags="-w -s -buildid=" -tags "wasip1" \
+go1.22.7 build -trimpath -ldflags="-w -s -buildid=" -tags "wasip1" -gcflags=all= -mod=readonly -a -p=1\
     -o testmodule.wasm github.com/smartcontractkit/chainlink-common/pkg/workflows/wasm/host/test/cmd

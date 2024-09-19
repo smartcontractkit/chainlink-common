164521b0d85b33c625f986ccbb688f8e04e57898
Sam Davies <samsondav@protonmail.com>
2024-09-19 14:26:26 -0400

    Add types for Retirement Report

--- pkg/types/provider_llo.go
+++ pkg/types/provider_llo.go
@@ -1,11 +1,21 @@
 package types
 
 import (
+	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
+
 	"github.com/smartcontractkit/chainlink-common/pkg/types/llo"
 )
 
+type LLOConfigProvider interface {
+	OffchainConfigDigester() ocrtypes.OffchainConfigDigester
+	// One instance will be run per config tracker
+	ContractConfigTrackers() []ocrtypes.ContractConfigTracker
+}
+
 type LLOProvider interface {
-	ConfigProvider
+	Service
+	LLOConfigProvider
+	ShouldRetireCache() llo.ShouldRetireCache
 	ContractTransmitter() llo.Transmitter
 	ChannelDefinitionCache() llo.ChannelDefinitionCache
 }

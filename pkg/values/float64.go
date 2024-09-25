10282bf15d4a0db9f29cca6268a1941021bfad39
Street <5597260+MStreet3@users.noreply.github.com>
2024-09-25 15:32:34 -0400

    feat(values): support float64 values (#799)

---
+++ pkg/values/float64.go
@@ -0,0 +1,38 @@
+package values
+
+import (
+	"errors"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/values/pb"
+)
+
+type Float64 struct {
+	Underlying float64
+}
+
+func NewFloat64(f float64) *Float64 {
+	return &Float64{Underlying: f}
+}
+
+func (f *Float64) proto() *pb.Value {
+	return pb.NewFloat64(f.Underlying)
+}
+
+func (f *Float64) Unwrap() (any, error) {
+	var to float64
+	return to, f.UnwrapTo(&to)
+}
+
+func (f *Float64) UnwrapTo(to any) error {
+	if f == nil {
+		return errors.New("cannot unwrap nil values.Float64")
+	}
+	return unwrapTo(f.Underlying, to)
+}
+
+func (f *Float64) copy() Value {
+	if f == nil {
+		return f
+	}
+	return &Float64{Underlying: f.Underlying}
+}

3e3b1ffa3e0b8d0339887b243e3d098fb7575933
ilija <pavlovicilija42@gmail.com>
2024-09-26 17:22:57 +0200

    Add GetLatestValue unit test for QueryKey

--- pkg/chainreader/contract_reader_test.go
+++ pkg/chainreader/contract_reader_test.go
@@ -9,11 +9,12 @@ import (
 
 	"github.com/smartcontractkit/chainlink-common/mocks"
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/query"
 	"github.com/smartcontractkit/chainlink-common/pkg/types/query/primitives"
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/tests"
 )
 
-func TestGetLatestValue(t *testing.T) {
+func TestContractReaderByIDsGetLatestValue(t *testing.T) {
 	ctx := tests.Context(t)
 	mockReader := new(mocks.ContractReader)
 	crByIDs := &contractReaderByIDs{
@@ -49,6 +50,57 @@ func TestGetLatestValue(t *testing.T) {
 	require.NoError(t, crByIDs.Unbind(ctx, map[string]types.BoundContract{bcCustomID2: bc2}))
 	mockReader.On("GetLatestValue", ctx, bc2.ReadIdentifier(readName2), primitives.ConfidenceLevel(""), nil, nil).Return(nil)
 	assert.Error(t, crByIDs.GetLatestValue(ctx, bcCustomID2, readName2, "", nil, nil))
+
+	mockReader.AssertExpectations(t)
+}
+
+func TestContractReaderByIDsQueryKey(t *testing.T) {
+	ctx := tests.Context(t)
+	mockReader := new(mocks.ContractReader)
+	crByIDs := &contractReaderByIDs{
+		bindings: sync.Map{},
+		cr:       mockReader,
+	}
+
+	bc1, bc2 := types.BoundContract{Address: "0x123", Name: "testContract1"}, types.BoundContract{Address: "0x321", Name: "testContract2"}
+	bc1CustomID, bcCustomID2 := "customID1", "customID2"
+	mockReader.On("Bind", ctx, []types.BoundContract{bc1, bc2}).Return(nil)
+	require.NoError(t, crByIDs.Bind(ctx, map[string]types.BoundContract{bc1CustomID: bc1, bcCustomID2: bc2}))
+
+	filter := query.KeyFilter{}
+	limitAndSort := query.LimitAndSort{}
+	var sequenceDataType any
+
+	// query contract1
+	expectedSequences := []types.Sequence{{Data: "sequenceData1"}}
+	mockReader.On("QueryKey", ctx, bc1, filter, limitAndSort, sequenceDataType).Return(expectedSequences, nil)
+	sequences, err := crByIDs.QueryKey(ctx, bc1CustomID, filter, limitAndSort, sequenceDataType)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedSequences, sequences)
+
+	// query contract2
+	expectedSequences2 := []types.Sequence{{Data: "sequenceData2"}}
+	mockReader.On("QueryKey", ctx, bc2, filter, limitAndSort, sequenceDataType).Return(expectedSequences2, nil)
+	sequences2, err := crByIDs.QueryKey(ctx, bcCustomID2, filter, limitAndSort, sequenceDataType)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedSequences2, sequences2)
+
+	// After unbinding contract1 shouldn't be registered and should return error
+	mockReader.On("Unbind", ctx, []types.BoundContract{bc1}).Return(nil)
+	require.NoError(t, crByIDs.Unbind(ctx, map[string]types.BoundContract{bc1CustomID: bc1}))
+	_, err = crByIDs.QueryKey(ctx, bc1CustomID, filter, limitAndSort, sequenceDataType)
+	assert.Error(t, err)
+
+	// contract2 should still be registered
+	mockReader.On("QueryKey", ctx, bc2, filter, limitAndSort, sequenceDataType)
+	_, err = crByIDs.QueryKey(ctx, bcCustomID2, filter, limitAndSort, sequenceDataType)
+	assert.NoError(t, err)
+
+	// After unbinding contract2 shouldn't be registered and should return error
+	mockReader.On("Unbind", ctx, []types.BoundContract{bc2}).Return(nil)
+	require.NoError(t, crByIDs.Unbind(ctx, map[string]types.BoundContract{bcCustomID2: bc2}))
+	_, err = crByIDs.QueryKey(ctx, bcCustomID2, filter, limitAndSort, sequenceDataType)
+	assert.Error(t, err)
 
 	mockReader.AssertExpectations(t)
 }

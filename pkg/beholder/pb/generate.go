0c91c77d3972cfc2ad3b7b6a83ecfd8ae6c02ade
patrickhuie19 <patrick.huie@smartcontract.com>
2024-10-03 15:10:56 -0400

    moving keystone to its own package

--- pkg/beholder/pb/generate.go
+++ pkg/beholder/pb/generate.go
@@ -1,4 +1,3 @@
 //go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative example.proto
-//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative keystone_custom.proto
 
 package pb

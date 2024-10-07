af3f7999e3ad8af3d295067ac3823c7f0a1d19bf
4of9 <177086174+4of9@users.noreply.github.com>
2024-10-07 14:58:55 -0400

    Add NodeAuthenticator

---
+++ pkg/beholder/node_auth.go
@@ -0,0 +1,83 @@
+package beholder
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+)
+
+// NodeAuthenticator implements PerRPCCredentials interface from google.golang.org/grpc/credentials
+type NodeAuthenticator struct {
+	staticAuthHeaders        map[string]string
+	requireTransportSecurity bool
+}
+
+func NewNodeAuthenticator(config Config) (*NodeAuthenticator, error) {
+	if err := validateNodeAuthConfig(config); err != nil {
+		return nil, err
+	}
+
+	return &NodeAuthenticator{
+		staticAuthHeaders:        deriveAuthHeaders(config),
+		requireTransportSecurity: !config.InsecureConnection,
+	}, nil
+}
+
+func (na *NodeAuthenticator) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	authHeaders := make(map[string]string)
+	for k, v := range na.staticAuthHeaders {
+		authHeaders[k] = v
+	}
+	return authHeaders, nil
+}
+
+func (na *NodeAuthenticator) RequireTransportSecurity() bool {
+	return na.requireTransportSecurity
+}
+
+func validateNodeAuthConfig(config Config) error {
+	if config.CSAAuthEnabled {
+		if config.CSAPublicKey == nil {
+			return fmt.Errorf("CSA auth is enabled but no CSA public key was provided")
+		}
+		if config.CSASigner == nil {
+			return fmt.Errorf("CSA auth is enabled but no CSA signer was provided")
+		}
+	}
+	return nil
+}
+
+// authHeaderKey is the name of the header that the node authenticator will use to send the auth token
+var authHeaderKey = "X-Beholder-Node-Auth-Token"
+
+// authHeaderVersion is the version of the auth header format
+var authHeaderVersion = "1"
+
+// deriveAuthHeaders creates the auth header map to be included on requests
+func deriveAuthHeaders(config Config) map[string]string {
+	authHeaders := make(map[string]string)
+
+	if !config.CSAAuthEnabled {
+		return authHeaders
+	}
+
+	authHeaders[authHeaderKey] = deriveAuthHeaderValue(config)
+
+	return authHeaders
+}
+
+// deriveAuthHeaderValue creates the auth header value to be included on requests.
+// The current format for the header is:
+//
+// <version>:<csa_public_key_hex>:<signature_hex>
+//
+// where the byte value of <csa_public_key_hex> is what's being signed
+func deriveAuthHeaderValue(config Config) string {
+	messageBytes := config.CSAPublicKey
+	messageHex := hex.EncodeToString(messageBytes)
+
+	signature := config.CSASigner(messageBytes)
+	signatureHex := hex.EncodeToString(signature)
+
+	return fmt.Sprintf("%s:%s:%s", authHeaderVersion, messageHex, signatureHex)
+}
---
+++ pkg/beholder/node_auth_test.go
@@ -0,0 +1,70 @@
+package beholder_test
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"testing"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/beholder"
+	"github.com/stretchr/testify/assert"
+)
+
+func defaultTestingConfig() beholder.Config {
+	return beholder.Config{
+		CSAAuthEnabled:     true,
+		CSAPublicKey:       []byte("test-public-key"),
+		CSASigner:          func([]byte) []byte { return []byte("test-signature") },
+		InsecureConnection: false,
+	}
+
+}
+
+func TestNodeAuthenticator_HappyPath(t *testing.T) {
+	config := defaultTestingConfig()
+	na, err := beholder.NewNodeAuthenticator(config)
+	assert.NoError(t, err)
+
+	// Test GetRequestMetadata
+	expectedMessage := hex.EncodeToString([]byte("test-public-key"))
+	expectedSignature := hex.EncodeToString([]byte("test-signature"))
+	expectedRequestMetadata := map[string]string{
+		"X-Beholder-Node-Auth-Token": fmt.Sprintf("1:%s:%s", expectedMessage, expectedSignature),
+	}
+	requestMetadata, err := na.GetRequestMetadata(context.Background())
+	assert.NoError(t, err)
+	assert.Equal(t, expectedRequestMetadata, requestMetadata)
+
+	// Test RequireTransportSecurity
+	assert.True(t, na.RequireTransportSecurity())
+}
+
+func TestNodeAuthenticator_NodeAuthConfig(t *testing.T) {
+	// Should error on nil public CSA key
+	c1 := defaultTestingConfig()
+	c1.CSAPublicKey = nil
+	_, err := beholder.NewNodeAuthenticator(c1)
+	assert.Error(t, err, "CSA auth is enabled but no CSA public key was provided")
+
+	// Should error on nil CSA signer
+	c2 := defaultTestingConfig()
+	c2.CSASigner = nil
+	_, err = beholder.NewNodeAuthenticator(c2)
+	assert.Error(t, err, "CSA auth is enabled but no CSA signer was provided")
+}
+
+func TestNodeAuthenticator_CSAAuthDisabled(t *testing.T) {
+	// CSA Auth disabled should accept nil values and return empty map for request metadata
+	c := defaultTestingConfig()
+	c.CSAAuthEnabled = false
+	c.CSAPublicKey = nil
+	c.CSASigner = nil
+
+	na, err := beholder.NewNodeAuthenticator(c)
+	assert.NoError(t, err)
+
+	expectedRequestMetadata := map[string]string{}
+	requestMetadata, err := na.GetRequestMetadata(context.Background())
+	assert.NoError(t, err)
+	assert.Equal(t, expectedRequestMetadata, requestMetadata)
+}

15e14794c64ccdcce5ca1d7630a4f7e95cb9f6af
4of9 <177086174+4of9@users.noreply.github.com>
2024-10-08 17:18:57 -0400

    Use Authenticator in Beholder

--- pkg/beholder/client.go
+++ pkg/beholder/client.go
@@ -47,6 +47,9 @@ type Client struct {
 	MeterProvider         otelmetric.MeterProvider
 	MessageLoggerProvider otellog.LoggerProvider
 
+	// Authenticator
+	Authenticator *Authenticator
+
 	// OnClose
 	OnClose func() error
 }
@@ -90,9 +93,14 @@ func newGRPCClient(cfg Config, otlploggrpcNew otlploggrpcFactory) (*Client, erro
 			return nil, err
 		}
 	}
+	authenticator, err := NewAuthenticator(cfg)
+	if err != nil {
+		return noop, err
+	}
 	sharedLogExporter, err := otlploggrpcNew(
 		otlploggrpc.WithTLSCredentials(creds),
 		otlploggrpc.WithEndpoint(cfg.OtelExporterGRPCEndpoint),
+		otlploggrpc.WithHeaders(authenticator.GetHeaders()),
 	)
 	if err != nil {
 		return nil, err
@@ -125,14 +133,14 @@ func newGRPCClient(cfg Config, otlploggrpcNew otlploggrpcFactory) (*Client, erro
 	logger := loggerProvider.Logger(defaultPackageName)
 
 	// Tracer
-	tracerProvider, err := newTracerProvider(cfg, baseResource, creds)
+	tracerProvider, err := newTracerProvider(cfg, baseResource, creds, authenticator)
 	if err != nil {
 		return nil, err
 	}
 	tracer := tracerProvider.Tracer(defaultPackageName)
 
 	// Meter
-	meterProvider, err := newMeterProvider(cfg, baseResource, creds)
+	meterProvider, err := newMeterProvider(cfg, baseResource, creds, authenticator)
 	if err != nil {
 		return nil, err
 	}
@@ -176,7 +184,7 @@ func newGRPCClient(cfg Config, otlploggrpcNew otlploggrpcFactory) (*Client, erro
 		}
 		return
 	}
-	return &Client{cfg, logger, tracer, meter, emitter, loggerProvider, tracerProvider, meterProvider, messageLoggerProvider, onClose}, nil
+	return &Client{cfg, logger, tracer, meter, emitter, loggerProvider, tracerProvider, meterProvider, messageLoggerProvider, authenticator, onClose}, nil
 }
 
 // Closes all providers, flushes all data and stops all background processes
@@ -258,13 +266,14 @@ type shutdowner interface {
 	Shutdown(ctx context.Context) error
 }
 
-func newTracerProvider(config Config, resource *sdkresource.Resource, creds credentials.TransportCredentials) (*sdktrace.TracerProvider, error) {
+func newTracerProvider(config Config, resource *sdkresource.Resource, creds credentials.TransportCredentials, authenticator *Authenticator) (*sdktrace.TracerProvider, error) {
 	ctx := context.Background()
 
 	// note: context is used internally
 	exporter, err := otlptracegrpc.New(ctx,
 		otlptracegrpc.WithTLSCredentials(creds),
 		otlptracegrpc.WithEndpoint(config.OtelExporterGRPCEndpoint),
+		otlptracegrpc.WithHeaders(authenticator.GetHeaders()),
 	)
 	if err != nil {
 		return nil, err
@@ -285,7 +294,7 @@ func newTracerProvider(config Config, resource *sdkresource.Resource, creds cred
 	return sdktrace.NewTracerProvider(opts...), nil
 }
 
-func newMeterProvider(config Config, resource *sdkresource.Resource, creds credentials.TransportCredentials) (*sdkmetric.MeterProvider, error) {
+func newMeterProvider(config Config, resource *sdkresource.Resource, creds credentials.TransportCredentials, authenticator *Authenticator) (*sdkmetric.MeterProvider, error) {
 	ctx := context.Background()
 
 	// note: context is unused internally
@@ -293,6 +302,7 @@ func newMeterProvider(config Config, resource *sdkresource.Resource, creds crede
 		ctx,
 		otlpmetricgrpc.WithTLSCredentials(creds),
 		otlpmetricgrpc.WithEndpoint(config.OtelExporterGRPCEndpoint),
+		otlpmetricgrpc.WithHeaders(authenticator.GetHeaders()),
 	)
 	if err != nil {
 		return nil, err

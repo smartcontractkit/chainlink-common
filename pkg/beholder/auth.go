88df7ab02bd80b886d7840ef0161a23b2031e6d8
4of9 <177086174+4of9@users.noreply.github.com>
2024-11-07 15:33:40 -0500

    Use ed25519 keys instead of signer

--- pkg/beholder/auth.go
+++ pkg/beholder/auth.go
@@ -1,6 +1,9 @@
 package beholder
 
-import "fmt"
+import (
+	"crypto/ed25519"
+	"fmt"
+)
 
 // authHeaderKey is the name of the header that the node authenticator will use to send the auth token
 var authHeaderKey = "X-Beholder-Node-Auth-Token"
@@ -14,9 +17,9 @@ var authHeaderVersion = "1"
 // <version>:<public_key_hex>:<signature_hex>
 //
 // where the byte value of <public_key_hex> is what's being signed
-func BuildAuthHeaders(signer func([]byte) []byte, pubKey []byte) map[string]string {
+func BuildAuthHeaders(privKey ed25519.PrivateKey, pubKey ed25519.PublicKey) map[string]string {
 	messageBytes := pubKey
-	signature := signer(messageBytes)
+	signature := ed25519.Sign(privKey, messageBytes)
 	headerValue := fmt.Sprintf("%s:%x:%x", authHeaderVersion, messageBytes, signature)
 
 	return map[string]string{authHeaderKey: headerValue}
--- pkg/beholder/auth_test.go
+++ pkg/beholder/auth_test.go
@@ -1,20 +1,23 @@
 package beholder
 
 import (
+	"crypto/ed25519"
+	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestBuildAuthHeaders(t *testing.T) {
-	mockPubKey := []byte("test-public-key")
-	mockSigner := func(data []byte) []byte {
-		return append(data, []byte("__test-signature")...)
-	}
+	csaPrivKeyHex := "1ac84741fa51c633845fa65c06f37a700303619135630a01f2d22fb98eb1c54ecab39509e63cfaa81c70e2c907391f96803aacb00db5619a5ace5588b4b08159"
+	csaPrivKeyBytes, err := hex.DecodeString(csaPrivKeyHex)
+	assert.NoError(t, err)
+	csaPrivKey := ed25519.PrivateKey(csaPrivKeyBytes)
+	csaPubKey := csaPrivKey.Public().(ed25519.PublicKey)
 
 	expectedHeaders := map[string]string{
-		"X-Beholder-Node-Auth-Token": "1:746573742d7075626c69632d6b6579:746573742d7075626c69632d6b65795f5f746573742d7369676e6174757265",
+		"X-Beholder-Node-Auth-Token": "1:cab39509e63cfaa81c70e2c907391f96803aacb00db5619a5ace5588b4b08159:4403178e299e9acc5b48ae97de617d3975c5d431b794cfab1d23eda01c194119b2360f5f74cfb3e4f706237ab57a0ba88ffd3f8addbc1e5197b3d3e13a1fc409",
 	}
 
-	assert.Equal(t, expectedHeaders, BuildAuthHeaders(mockSigner, mockPubKey))
+	assert.Equal(t, expectedHeaders, BuildAuthHeaders(csaPrivKey, csaPubKey))
 }

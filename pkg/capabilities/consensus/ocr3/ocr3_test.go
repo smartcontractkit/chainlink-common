26d4a0b45b233961f6148439a000a86006413613
Justin Kaseman <justinkaseman@live.com>
2024-12-02 09:24:04 -0800

    [CAPPL-205] Remove OCR3 capability from registry when closed (#953)
    * Add Remove method to capabilities registry interface
    
    * Remove OCR3 capability plugin from Capability Registry on close
    
    * Refactor to use context & remove from Registry within OCR3 Capability
    
    * (refactor): Simplify to passing ID as the input to Remove

--- pkg/capabilities/consensus/ocr3/ocr3_test.go
+++ pkg/capabilities/consensus/ocr3/ocr3_test.go
@@ -55,11 +55,18 @@ func TestOCR3_ReportingFactoryIsAService(t *testing.T) {
 	var rs core.RelayerSet
 	r := mocks.NewCapabilitiesRegistry(t)
 	r.On("Add", mock.Anything, o.config.capability).Return(nil)
+	r.On("Remove", mock.Anything, o.config.capability.ID).Return(nil)
 
 	factory, err := o.NewReportingPluginFactory(ctx, core.ReportingPluginServiceConfig{}, p, pr, tc, el, r, kv, rs)
 	require.NoError(t, err)
 
 	require.NoError(t, factory.Start(ctx))
 
+	r.AssertCalled(t, "Add", mock.Anything, o.config.capability)
 	assert.Nil(t, factory.Ready())
+
+	err = o.Close()
+	require.NoError(t, err)
+
+	r.AssertCalled(t, "Remove", mock.Anything, o.config.capability.ID)
 }

6ea39ee37030d324ba2fcae1f6d2b19d91d9cd8a
hendoxc <hagen.henderson@smartcontract.com>
2024-12-04 10:55:49 -0700

    INFOPLAT-1559 Adds auth header token expiry functionality
    - Adds function for signing publickey + timestamp.
    - Adjusts `BuildAuthHeaders` to take variadic args allowing backwards compatibiilty

--- pkg/beholder/auth.go
+++ pkg/beholder/auth.go
@@ -2,7 +2,9 @@ package beholder
 
 import (
 	"crypto/ed25519"
+	"encoding/binary"
 	"fmt"
+	"time"
 )
 
 // authHeaderKey is the name of the header that the node authenticator will use to send the auth token
@@ -11,17 +13,78 @@ var authHeaderKey = "X-Beholder-Node-Auth-Token"
 // authHeaderVersion is the version of the auth header format
 var authHeaderVersion = "1"
 
+// BuildAuthHeadersOpt are used to modify the behavior of the BuildAuthHeaders function
+type BuildAuthHeadersOpt func(*authHeaderConfig)
+
+type authHeaderConfig struct {
+	privKey   ed25519.PrivateKey
+	timestamp int64
+	version   string
+}
+
 // BuildAuthHeaders creates the auth header value to be included on requests.
-// The current format for the header is:
+// There are two formats for the header. Version `1` is:
 //
 // <version>:<public_key_hex>:<signature_hex>
 //
 // where the byte value of <public_key_hex> is what's being signed
-func BuildAuthHeaders(privKey ed25519.PrivateKey) map[string]string {
+// and <signature_hex> is the signature of the public key.
+// The version `2` is:
+//
+// <version>:<public_key_hex>:<timestamp>:<signature_hex>
+//
+// where the byte value of <public_key_hex> and <timestamp> are what's being signed
+func BuildAuthHeaders(privKey ed25519.PrivateKey, opts ...BuildAuthHeadersOpt) map[string]string {
+	// Defaults
+	cfg := &authHeaderConfig{
+		privKey:   privKey,
+		timestamp: time.Now().UnixMilli(),
+		version:   authHeaderVersion,
+	}
+	// Apply config options
+	for _, opt := range opts {
+		opt(cfg)
+	}
+	// Use the new version of the auth header with timestamps
+	if cfg.version == "2" {
+		return buildAuthHeadersV2(cfg.privKey, cfg.timestamp, cfg.version)
+	}
+	// Use the old version of the auth header as default
+	return buildAuthHeaderV1(cfg.privKey)
+}
+
+func buildAuthHeaderV1(privKey ed25519.PrivateKey) map[string]string {
+
 	pubKey := privKey.Public().(ed25519.PublicKey)
 	messageBytes := pubKey
 	signature := ed25519.Sign(privKey, messageBytes)
-	headerValue := fmt.Sprintf("%s:%x:%x", authHeaderVersion, messageBytes, signature)
 
-	return map[string]string{authHeaderKey: headerValue}
+	return map[string]string{authHeaderKey: fmt.Sprintf("%s:%x:%x", authHeaderVersion, messageBytes, signature)}
+}
+
+func buildAuthHeadersV2(privKey ed25519.PrivateKey, timestamp int64, version string) map[string]string {
+
+	pubKey := privKey.Public().(ed25519.PublicKey)
+
+	timestampUnixMsBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(timestampUnixMsBytes, uint64(timestamp))
+
+	messageBytes := append(pubKey, timestampUnixMsBytes...)
+	signature := ed25519.Sign(privKey, messageBytes)
+
+	return map[string]string{authHeaderKey: fmt.Sprintf("%s:%x:%d:%x", version, pubKey, timestamp, signature)}
+}
+
+// WithAuthHeaderTimestamp is an option to set the timestamp to be used in auth headers ~ default is time.Now().UnixMilli()
+func WithAuthHeaderTimestamp(timestamp int64) BuildAuthHeadersOpt {
+	return func(cfg *authHeaderConfig) {
+		cfg.timestamp = timestamp
+	}
+}
+
+// WithAuthHeaderV2 is an option to use version 2 of auth headers that uses timestamps
+func WithAuthHeaderV2() BuildAuthHeadersOpt {
+	return func(cfg *authHeaderConfig) {
+		cfg.version = "2"
+	}
 }
--- pkg/beholder/auth_test.go
+++ pkg/beholder/auth_test.go
@@ -2,13 +2,20 @@ package beholder
 
 import (
 	"crypto/ed25519"
+	"encoding/binary"
 	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
 	"testing"
+
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBuildAuthHeaders(t *testing.T) {
+func TestBuildAuthHeadersV1(t *testing.T) {
+
 	csaPrivKeyHex := "1ac84741fa51c633845fa65c06f37a700303619135630a01f2d22fb98eb1c54ecab39509e63cfaa81c70e2c907391f96803aacb00db5619a5ace5588b4b08159"
 	csaPrivKeyBytes, err := hex.DecodeString(csaPrivKeyHex)
 	assert.NoError(t, err)
@@ -19,4 +26,50 @@ func TestBuildAuthHeaders(t *testing.T) {
 	}
 
 	assert.Equal(t, expectedHeaders, BuildAuthHeaders(csaPrivKey))
+}
+
+func TestBuildAuthHeadersV2(t *testing.T) {
+
+	csaPrivKeyHex := "1ac84741fa51c633845fa65c06f37a700303619135630a01f2d22fb98eb1c54ecab39509e63cfaa81c70e2c907391f96803aacb00db5619a5ace5588b4b08159"
+	csaPrivKeyBytes, err := hex.DecodeString(csaPrivKeyHex)
+	assert.NoError(t, err)
+	csaPrivKey := ed25519.PrivateKey(csaPrivKeyBytes)
+	timestamp := time.Now().UnixMilli()
+
+	authHeaderMap := BuildAuthHeaders(csaPrivKey,
+		WithAuthHeaderV2(),
+		WithAuthHeaderTimestamp(timestamp),
+	)
+
+	authHeaderValue, ok := authHeaderMap[authHeaderKey]
+	assert.True(t, ok, "auth header should be present")
+
+	parts := strings.Split(authHeaderValue, ":")
+	assert.Len(t, parts, 4, "auth header v2 should have 4 parts")
+	// Check the parts
+	version, pubKeyHex, timestampStr, signatureHex := parts[0], parts[1], parts[2], parts[3]
+	assert.Equal(t, "2", version, "using WithAuthHeaderV2 should should have version 2")
+	assert.Equal(t, hex.EncodeToString(csaPrivKey.Public().(ed25519.PublicKey)), pubKeyHex)
+	assert.Equal(t, fmt.Sprintf("%d", timestamp), timestampStr)
+
+	// Decode the public key and signature
+	pubKeyBytes, err := hex.DecodeString(pubKeyHex)
+	assert.NoError(t, err)
+	assert.Equal(t, csaPrivKey.Public().(ed25519.PublicKey), ed25519.PublicKey(pubKeyBytes))
+
+	// Parse the timestamp
+	timestampParsed, err := strconv.ParseInt(timestampStr, 10, 64)
+	assert.NoError(t, err)
+	assert.Equal(t, timestamp, timestampParsed)
+	timestampBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(timestampBytes, uint64(timestampParsed))
+
+	// Reconstruct the message bytes
+	messageBytes := append(pubKeyBytes, timestampBytes...)
+
+	// Verify the signature
+	signatureBytes, err := hex.DecodeString(signatureHex)
+	assert.NoError(t, err)
+	assert.True(t, ed25519.Verify(pubKeyBytes, messageBytes, signatureBytes))
+
 }

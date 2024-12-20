7c7d06f0c7e2c160b9b7f2af994595f33e2fa42e
Gheorghe Strimtu <studentcuza@gmail.com>
2024-12-20 14:33:42 +0200

    INFOPLAT-1539  Beholder Log Batch Processor More Settings (#957)
    * Beholder Log Batch Processor More Settings
    
    * add settings for message emitter
    
    * remove settings
    
    * add settings
    
    * more settings
    
    * add too httpclient and test
    
    * fix ExampleConfig test
    
    * add new lines for spacing in Config
    
    * Add all beholder config options to loop/EnvConfig; set beholder config options from loop EnvConfig
    
    * Set EmitterExportTimeout, LogExportTimeout to 30sec which is OTel default
    
    * Update comment for EmitterBatchProcessor config option
    
    * Dont set batch processor options with invalid values
    
    ---------
    
    Co-authored-by: Pavel <177363085+pkcll@users.noreply.github.com>

--- pkg/beholder/config_test.go
+++ pkg/beholder/config_test.go
@@ -25,8 +25,11 @@ func ExampleConfig() {
 			otelattr.String("sender", "beholderclient"),
 		},
 		// Message Emitter
-		EmitterExportTimeout:  1 * time.Second,
-		EmitterBatchProcessor: true,
+		EmitterExportTimeout:      1 * time.Second,
+		EmitterExportMaxBatchSize: 512,
+		EmitterExportInterval:     1 * time.Second,
+		EmitterMaxQueueSize:       2048,
+		EmitterBatchProcessor:     true,
 		// OTel message log exporter retry config
 		LogRetryConfig: nil,
 		// Trace
@@ -39,8 +42,11 @@ func ExampleConfig() {
 		// OTel metric exporter retry config
 		MetricRetryConfig: nil,
 		// Log
-		LogExportTimeout:  1 * time.Second,
-		LogBatchProcessor: true,
+		LogExportTimeout:      1 * time.Second,
+		LogExportMaxBatchSize: 512,
+		LogExportInterval:     1 * time.Second,
+		LogMaxQueueSize:       2048,
+		LogBatchProcessor:     true,
 	}
 	fmt.Printf("%+v\n", config)
 	config.LogRetryConfig = &beholder.RetryConfig{
@@ -50,6 +56,6 @@ func ExampleConfig() {
 	}
 	fmt.Printf("%+v\n", *config.LogRetryConfig)
 	// Output:
-	// {InsecureConnection:true CACertFile: OtelExporterGRPCEndpoint:localhost:4317 OtelExporterHTTPEndpoint:localhost:4318 ResourceAttributes:[{Key:package_name Value:{vtype:4 numeric:0 stringly:beholder slice:<nil>}} {Key:sender Value:{vtype:4 numeric:0 stringly:beholderclient slice:<nil>}}] EmitterExportTimeout:1s EmitterBatchProcessor:true TraceSampleRatio:1 TraceBatchTimeout:1s TraceSpanExporter:<nil> TraceRetryConfig:<nil> MetricReaderInterval:1s MetricRetryConfig:<nil> MetricViews:[] LogExportTimeout:1s LogBatchProcessor:true LogRetryConfig:<nil> AuthPublicKeyHex: AuthHeaders:map[]}
+	// {InsecureConnection:true CACertFile: OtelExporterGRPCEndpoint:localhost:4317 OtelExporterHTTPEndpoint:localhost:4318 ResourceAttributes:[{Key:package_name Value:{vtype:4 numeric:0 stringly:beholder slice:<nil>}} {Key:sender Value:{vtype:4 numeric:0 stringly:beholderclient slice:<nil>}}] EmitterExportTimeout:1s EmitterExportInterval:1s EmitterExportMaxBatchSize:512 EmitterMaxQueueSize:2048 EmitterBatchProcessor:true TraceSampleRatio:1 TraceBatchTimeout:1s TraceSpanExporter:<nil> TraceRetryConfig:<nil> MetricReaderInterval:1s MetricRetryConfig:<nil> MetricViews:[] LogExportTimeout:1s LogExportInterval:1s LogExportMaxBatchSize:512 LogMaxQueueSize:2048 LogBatchProcessor:true LogRetryConfig:<nil> AuthPublicKeyHex: AuthHeaders:map[]}
 	// {InitialInterval:5s MaxInterval:30s MaxElapsedTime:1m0s}
 }

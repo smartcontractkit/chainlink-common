e1e47fdb11b0e6a7e0ad69a7ecc6bb53ee74bf66
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2025-02-21 13:49:03 -0400

    Adds consensus cap config proto (#1023)
    * Adds consensus cap config proto
    
    * Decodes `OffchainConfig` to set up limits
    
    * Updates to proto
    
    * Applies default to missing config
    
    * Minor nits
    
    * Updates `RequestTimeout` from chain config
    
    * Refactors `RequestTimeout` update
    
    * Adds proto comment
    
    * Make generate

--- pkg/capabilities/consensus/ocr3/types/generate.go
+++ pkg/capabilities/consensus/ocr3/types/generate.go
@@ -1,2 +1,3 @@
 //go:generate protoc --go_out=../../../../ --go_opt=paths=source_relative --go-grpc_out=../../../../ --go-grpc_opt=paths=source_relative --proto_path=../../../../ capabilities/consensus/ocr3/types/ocr3_types.proto values/pb/values.proto
+//go:generate protoc --go_out=../../../../ --go_opt=paths=source_relative --go-grpc_out=../../../../ --go-grpc_opt=paths=source_relative --proto_path=../../../../ capabilities/consensus/ocr3/types/ocr3_config_types.proto values/pb/values.proto
 package types

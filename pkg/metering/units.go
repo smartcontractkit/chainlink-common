3edcee8188f82fb8907ce8e4eaaafc46e6875548
patrickhuie19 <patrick.huie@smartcontract.com>
2025-03-31 16:13:39 -0400

    updating access pattern to metering.unit

--- pkg/metering/units.go
+++ pkg/metering/units.go
@@ -1,25 +1,20 @@
 package metering
 
-const (
-	payloadName = "payload"
-	payloadUnit = "bytes"
+var (
+	PayloadMeteringUnit = unit{Name: "payload", Unit: "bytes"}
 
-	computeName = "compute"
-	computeUnit = "seconds"
+	// ComputeMeteringUnit is an example. The compute cap will eventually be obsoleted
+	// by the CRE No-DAG SDK.
+	ComputeMeteringUnit = unit{Name: "compute", Unit: "seconds"}
 )
 
-type MeteringUnit struct {
+// unit provides exported Name and unit fields for
+// capability devs to consume when implementing
+// metering. Do not export.
+type unit struct {
+	// Name of the Metering Unit, i.e. payload, compute, storage
 	Name string
+
+	// Unit of the Metering Unit, i.e. bytes, seconds
 	Unit string
-}
-
-func NewPayloadMeteringUnit() *MeteringUnit {
-	return &MeteringUnit{payloadName, payloadUnit}
-}
-
-// NewComputeMeteringUnit is an example of how MeteringUnit
-// is used for different capabilities. The capability will
-// eventually be consumed by the power of the No-DAG SDK.
-func NewComputeMeteringUnit() *MeteringUnit {
-	return &MeteringUnit{computeName, computeUnit}
 }

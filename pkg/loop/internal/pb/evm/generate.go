950aef04e42de6b79b66bbd1d93d6dfddf22e6cd
ilija <pavlovicilija42@gmail.com>
2025-05-05 13:36:50 +0200

    Add EVM Cap gen and CRE Cap metadata

--- pkg/loop/internal/pb/evm/generate.go
+++ pkg/loop/internal/pb/evm/generate.go
@@ -1,3 +1,4 @@
-//go:generate protoc --proto_path=.:.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative evm.proto
+//go:generate protoc --proto_path=.:..:../../../../ --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative evm.proto
 
+//go:generate protoc --proto_path=.:..:../../../../ --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --plugin=protoc-gen-cre=../../../../capabilities/v2/protoc/protoc-gen-cre --cre_out=. evm.proto
 package evmpb

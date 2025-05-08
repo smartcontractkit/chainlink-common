92265eda48f94f6d8163b554c844cd1a30f9bed2
Michael Street <5597260+MStreet3@users.noreply.github.com>
2025-05-08 15:22:45 -0400

    feat: tests SetCapabilityExecutor

--- pkg/workflows/wasm/host/wasm_nodag_test.go
+++ pkg/workflows/wasm/host/wasm_nodag_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/google/uuid"
 	sdkpb "github.com/smartcontractkit/chainlink-common/pkg/workflows/sdk/v2/pb"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -63,37 +64,14 @@ func Test_NoDag_Run(t *testing.T) {
 	})
 
 	t.Run("Execute trigger", func(t *testing.T) {
-		ctx := t.Context()
-		// When a TriggerEvent occurs, Engine calls Execute with that Event.
-		trigger := &basictrigger.Outputs{CoolOutput: "Hi"}
-		wrapped, err := anypb.New(trigger)
-		require.NoError(t, err)
+		executeTrigger(t, m, 0)
+	})
 
-		// TODO test config
-		req := &wasmpb.ExecuteRequest{
-			Id: anyNoDagExecId,
-			Request: &wasmpb.ExecuteRequest_Trigger{
-				Trigger: &sdkpb.Trigger{
-					Id:      uint64(triggerIndex),
-					Payload: wrapped,
-				},
-			},
-		}
-		response, err := m.Execute(ctx, req)
-		require.NoError(t, err)
+	t.Run("Execute trigger with SetCapabilityExecutor", func(t *testing.T) {
+		// call with nil to use default implementation from module config
+		require.NoError(t, m.SetCapabilityExecutor(nil))
 
-		require.Equal(t, anyNoDagExecId, response.Id)
-		switch output := response.Result.(type) {
-		case *wasmpb.ExecutionResult_Value:
-			valuePb := output.Value
-			value, err := values.FromProto(valuePb)
-			require.NoError(t, err)
-			unwrapped, err := value.Unwrap()
-			require.NoError(t, err)
-			require.Equal(t, "Hiresponse-1response-2", unwrapped)
-		default:
-			t.Fatalf("unexpected response type %T", output)
-		}
+		executeTrigger(t, m, 0)
 	})
 }
 
@@ -101,7 +79,6 @@ func Test_NoDag_MultipleTriggers_Run(t *testing.T) {
 	t.Parallel()
 
 	mc := createNoDagMc(t)
-	triggerIndex := int(0)
 	capID := (&basictrigger.Basic{}).Trigger(&basictrigger.Config{}).CapabilityID()
 	binary := createTestBinary(nodagMultiTriggerBinaryCmd, nodagMultiTriggerBinaryLocation, true, t)
 
@@ -144,39 +121,110 @@ func Test_NoDag_MultipleTriggers_Run(t *testing.T) {
 	})
 
 	t.Run("Execute trigger", func(t *testing.T) {
-		ctx := t.Context()
-		// When a TriggerEvent occurs, Engine calls Execute with that Event.
-		trigger := &basictrigger.Outputs{CoolOutput: "Hi"}
-		wrapped, err := anypb.New(trigger)
+		executeTrigger(t, m, 1)
+	})
+}
+
+func Test_NoDag_SetCapabilityExecutor(t *testing.T) {
+	t.Parallel()
+
+	binary := createTestBinary(nodagBinaryCmd, nodagBinaryLocation, true, t)
+
+	// call with nil to use default implementation from module config
+	t.Run("OK-call SetCapabilityExecutor with nil uses default implementation", func(t *testing.T) {
+		mc := createNoDagMc(t)
+		m, err := NewModule(mc, binary)
 		require.NoError(t, err)
 
-		// Trigger second handler
-		triggerID := uint64(triggerIndex + 1)
-		req := &wasmpb.ExecuteRequest{
-			Id: anyNoDagExecId,
-			Request: &wasmpb.ExecuteRequest_Trigger{
-				Trigger: &sdkpb.Trigger{
-					Id:      triggerID,
-					Payload: wrapped,
-				},
+		m.Start()
+		defer m.Close()
+
+		require.NoError(t, m.SetCapabilityExecutor(nil))
+		executeTrigger(t, m, 0)
+	})
+
+	t.Run("OK-call to SetCapabilityExecutor overrides initial implementation", func(t *testing.T) {
+		numAwaits := 0
+		mc := &ModuleConfig{
+			Logger:         logger.Test(t),
+			IsUncompressed: true,
+			CallCapability: nil, // No hook is set on initial call, engine must set the callback hook
+			AwaitCapabilities: func(ctx context.Context, req *sdkpb.AwaitCapabilitiesRequest) (*sdkpb.AwaitCapabilitiesResponse, error) {
+				require.Equal(t, anyNoDagExecId, req.ExecId)
+				require.Len(t, req.Ids, 1)
+
+				numAwaits++
+				resp := &basicaction.Outputs{AdaptedThing: fmt.Sprintf("response-%d", numAwaits)}
+				payload, err := anypb.New(resp)
+				require.NoError(t, err)
+
+				return &sdkpb.AwaitCapabilitiesResponse{
+					Responses: map[string]*sdkpb.CapabilityResponse{
+						req.Ids[0][:]: {
+							Response: &sdkpb.CapabilityResponse_Payload{
+								Payload: payload,
+							},
+						},
+					},
+				}, nil
 			},
 		}
-		response, err := m.Execute(ctx, req)
+		m, err := NewModule(mc, binary)
 		require.NoError(t, err)
 
-		require.Equal(t, anyNoDagExecId, response.Id)
-		switch output := response.Result.(type) {
-		case *wasmpb.ExecutionResult_Value:
-			valuePb := output.Value
-			value, err := values.FromProto(valuePb)
-			require.NoError(t, err)
-			unwrapped, err := value.Unwrap()
-			require.NoError(t, err)
-			require.Equal(t, "Hiresponse-1response-2"+"true", unwrapped)
-		default:
-			t.Fatalf("unexpected response type %T", output)
-		}
+		m.Start()
+		defer m.Close()
+
+		mockCapExecutor := NewMockCapabilityExecutor(t)
+
+		// mock does nothing, the responses are set in the awaitCapabilities call
+		mockCapExecutor.EXPECT().CallCapability(mock.Anything, mock.Anything).Run(func(ctx context.Context, request *sdkpb.CapabilityRequest) {
+			require.Equal(t, anyNoDagExecId, request.ExecutionId)
+			require.Equal(t, "basic-test-action@1.0.0", request.Id)
+		}).Return(&sdkpb.CapabilityResponse{}, nil)
+
+		require.NoError(t, m.SetCapabilityExecutor(mockCapExecutor))
+		executeTrigger(t, m, 0)
+
+		// Require that two values were placed into the capCall store
+		require.True(t, len(m.capabilityCallStore.m) == 2)
 	})
+}
+
+func executeTrigger(t *testing.T, m *module, triggerIndex int) {
+	t.Helper()
+
+	ctx := t.Context()
+	// When a TriggerEvent occurs, Engine calls Execute with that Event.
+	trigger := &basictrigger.Outputs{CoolOutput: "Hi"}
+	wrapped, err := anypb.New(trigger)
+	require.NoError(t, err)
+
+	// TODO test config
+	req := &wasmpb.ExecuteRequest{
+		Id: anyNoDagExecId,
+		Request: &wasmpb.ExecuteRequest_Trigger{
+			Trigger: &sdkpb.Trigger{
+				Id:      uint64(triggerIndex),
+				Payload: wrapped,
+			},
+		},
+	}
+	response, err := m.Execute(ctx, req)
+	require.NoError(t, err)
+
+	require.Equal(t, anyNoDagExecId, response.Id)
+	switch output := response.Result.(type) {
+	case *wasmpb.ExecutionResult_Value:
+		valuePb := output.Value
+		value, err := values.FromProto(valuePb)
+		require.NoError(t, err)
+		unwrapped, err := value.Unwrap()
+		require.NoError(t, err)
+		require.Equal(t, "Hiresponse-1response-2", unwrapped)
+	default:
+		t.Fatalf("unexpected response type %T", output)
+	}
 }
 
 func createNoDagMc(t *testing.T) *ModuleConfig {

c2739727620baaf6a66ebb10d1d7c45913fa0ad3
patrickhuie19 <patrick.huie@smartcontract.com>
2025-05-08 22:03:30 -0400

    Adding max spends to the v2 SDK

--- pkg/workflows/sdk/v2/handler.go
+++ pkg/workflows/sdk/v2/handler.go
@@ -13,6 +13,7 @@ type Handler[T any] interface {
 	Method() string
 	TriggerCfg() *anypb.Any
 	Callback() func(runtime T, payload *anypb.Any) (any, error)
+	MaxSpend() *MaxSpendLimits
 }
 
 // NewDonHandler creates a new Handler for a DonTrigger with a return value for the workflow
@@ -63,7 +64,8 @@ func newEmptyDonHandler[R any, M proto.Message, T Trigger[M]](trigger T, callbac
 
 type handler[R any, T proto.Message] struct {
 	Trigger[T]
-	fn func(runtime R, trigger *anypb.Any) (any, error)
+	fn       func(runtime R, trigger *anypb.Any) (any, error)
+	maxSpend *MaxSpendLimits
 }
 
 func (h *handler[R, T]) TriggerCfg() *anypb.Any {
@@ -72,6 +74,16 @@ func (h *handler[R, T]) TriggerCfg() *anypb.Any {
 
 func (h *handler[R, T]) Callback() func(runtime R, payload *anypb.Any) (any, error) {
 	return h.fn
+}
+
+func (h *handler[R, T]) MaxSpend() *MaxSpendLimits {
+	return h.maxSpend
+}
+
+// WithMaxSpend sets the max spend limits for this handler
+func (h *handler[R, T]) WithMaxSpend(limits *MaxSpendLimits) *handler[R, T] {
+	h.maxSpend = limits
+	return h
 }
 
 var _ Handler[any] = (*handler[any, proto.Message])(nil)
--- pkg/workflows/sdk/v2/workflow.go
+++ pkg/workflows/sdk/v2/workflow.go
@@ -2,4 +2,11 @@ package sdk
 
 type WorkflowArgs[T any] struct {
 	Handlers []Handler[T]
+	MaxSpend *MaxSpendLimits // Optional max spend limits for the entire workflow
+}
+
+// WithMaxSpend sets the max spend limits for the workflow
+func (w *WorkflowArgs[T]) WithMaxSpend(limits *MaxSpendLimits) *WorkflowArgs[T] {
+	w.MaxSpend = limits
+	return w
 }

0688acff79b15e40a98817a591e5306a4be380b0
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2025-05-19 10:15:24 -0500

    billing protos references protos repo

--- pkg/billing/pb/generate.go
+++
@@ -1,2 +0,0 @@
-//go:generate protoc --go_out=../../ --go_opt=paths=source_relative --go-grpc_out=../../ --go-grpc_opt=paths=source_relative --proto_path=../../ billing/pb/billing_service.proto capabilities/pb/capabilities.proto
-package pb

c681b639c1fcefdb8b2fcdd9b8f284503fedbd3b
jinhoonbang <jin.bang@smartcontract.com>
2025-05-22 06:12:48 +0900

    generate proto for gateway connect and handler services

---
+++ pkg/loop/internal/pb/gatewayconnector/generate.go
@@ -0,0 +1,4 @@
+//go:generate protoc --proto_path=.:.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative gateway_connector.proto
+//go:generate protoc --proto_path=.:.. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative gateway_connector_handler.proto
+
+package gatewayconnector
--- pkg/types/core/gateway_connector.go
+++ pkg/types/core/gateway_connector.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"context"
-	"net/url"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/services"
 	"github.com/smartcontractkit/chainlink-common/pkg/types/gateway"
@@ -11,10 +10,6 @@ import (
 // GatewayConnector is a component run by Nodes to connect to a set of Gateways.
 type GatewayConnector interface {
 	services.Service
-	// TODO: check if this can be removed
-	ConnectionInitiator
-
-	AddHandler(methods []string, handler GatewayConnectorHandler) error
 	// SendToGateway takes a signed message as argument and sends it to the specified gateway
 	SendToGateway(ctx context.Context, gatewayID string, msg *gateway.Message) error
 	// SignAndSendToGateway signs the message and sends the message to the specified gateway
@@ -31,25 +26,4 @@ type GatewayConnectorHandler interface {
 	Start(context.Context) error
 	Close() error
 	HandleGatewayMessage(ctx context.Context, gatewayID string, msg *gateway.Message)
-}
-
-// The handshake works as follows:
-//
-//	 Client (Initiator)                  Server (Acceptor)
-//
-//	NewAuthHeader()
-//	            -------auth header-------->
-//	                                      StartHandshake()
-//	            <-------challenge----------
-//
-// ChallengeResponse()
-//
-//	---------response--------->
-//	                        FinalizeHandshake()
-type ConnectionInitiator interface {
-	// Generate authentication header value specific to node and gateway
-	NewAuthHeader(url *url.URL) ([]byte, error)
-
-	// Sign challenge to prove identity.
-	ChallengeResponse(url *url.URL, challenge []byte) ([]byte, error)
 }

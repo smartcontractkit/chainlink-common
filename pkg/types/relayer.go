f4699dc526e7a98b7704dd3a493402b8f444022b
Pablo La Greca <pablolagreca@hotmail.com>
2025-06-17 17:13:52 -0400

    adding back commented code and adding TODO item

--- pkg/types/relayer.go
+++ pkg/types/relayer.go
@@ -101,6 +101,12 @@ type ChainService interface {
 
 // GethClient is the subset of go-ethereum client methods implemented by EVMService.
 type GethClient interface {
+	// CallContract reads a contract as specified in the call message at a block height defined by blockNumber where:
+	// blockNumber :
+	//   nil (default) or (-2) → use the latest mined block (“latest”)
+	//   FinalizedBlockNumber(-3) → last finalized block (“finalized”)
+	//
+	// Any positive value is treated as an explicit block height.
 }
 
 type EVMService interface {

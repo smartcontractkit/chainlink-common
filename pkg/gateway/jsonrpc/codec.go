e3da2ca6813d4e55a5d196949be840274d856880
jinhoonbang <jin.bang@smartcontract.com>
2025-06-18 17:18:32 +0900

    add gateway jsonrpc encoder and types

---
+++ pkg/gateway/jsonrpc/codec.go
@@ -0,0 +1,56 @@
+package jsonrpc
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+type Codec struct {
+}
+
+func (c *Codec) DecodeRequestWithAuthToken(requestBytes []byte, authTokenFromHeader string) (Request, error) {
+	req, err := c.DecodeRequest(requestBytes)
+	if err != nil {
+		return Request{}, err
+	}
+	if authTokenFromHeader == "" {
+		return Request{}, errors.New("missing auth token")
+	}
+	req.Auth = authTokenFromHeader
+	return req, nil
+}
+
+func (*Codec) DecodeRequest(requestBytes []byte) (Request, error) {
+	var request Request
+	err := json.Unmarshal(requestBytes, &request)
+	if err != nil {
+		return Request{}, err
+	}
+	if request.Version != "2.0" {
+		return Request{}, errors.New("incorrect jsonrpc version")
+	}
+	if request.Method == "" {
+		return Request{}, errors.New("empty method field")
+	}
+	if request.Params == nil {
+		return Request{}, errors.New("missing params attribute")
+	}
+	return request, nil
+}
+
+func (*Codec) EncodeRequest(request *Request) ([]byte, error) {
+	return json.Marshal(request)
+}
+
+func (*Codec) DecodeResponse(responseBytes []byte) (Response, error) {
+	var response Response
+	err := json.Unmarshal(responseBytes, &response)
+	if err != nil {
+		return Response{}, err
+	}
+	return response, nil
+}
+
+func (*Codec) EncodeResponse(response *Response) ([]byte, error) {
+	return json.Marshal(response)
+}

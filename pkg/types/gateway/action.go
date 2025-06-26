51f74fada1dbc87474e048624ce4163fb83b9851
jinhoonbang <jin.bang@smartcontract.com>
2025-06-26 22:13:19 +0900

    add cache settings

--- pkg/types/gateway/action.go
+++ pkg/types/gateway/action.go
@@ -4,12 +4,18 @@ const (
 	MethodHTTPAction = "http_action"
 )
 
+type CacheSettings struct {
+	Enabled bool  `json:"enabled,omitempty"` // Whether caching is enabled. Defaults to false.
+	TTLms   int32 `json:"ttlMs,omitempty"`   // Time-to-live for the cache entry in milliseconds.
+}
+
 type OutboundHTTPRequest struct {
-	URL       string            `json:"url"`                 // URL to query, only http and https protocols are supported.
-	Method    string            `json:"method,omitempty"`    // HTTP verb, defaults to GET.
-	Headers   map[string]string `json:"headers,omitempty"`   // HTTP headers, defaults to empty.
-	Body      []byte            `json:"body,omitempty"`      // HTTP request body
-	TimeoutMs uint32            `json:"timeoutMs,omitempty"` // Timeout in milliseconds
+	URL           string            `json:"url"`                     // URL to query, only http and https protocols are supported.
+	Method        string            `json:"method,omitempty"`        // HTTP verb, defaults to GET.
+	Headers       map[string]string `json:"headers,omitempty"`       // HTTP headers, defaults to empty.
+	Body          []byte            `json:"body,omitempty"`          // HTTP request body
+	TimeoutMs     uint32            `json:"timeoutMs,omitempty"`     // Timeout in milliseconds
+	CacheSettings CacheSettings     `json:"cacheSettings,omitempty"` // Best-effort cache control for the request
 
 	// Maximum number of bytes to read from the response body.  If the gateway max response size is smaller than this value, the gateway max response size will be used.
 	MaxResponseBytes uint32 `json:"maxBytes,omitempty"`

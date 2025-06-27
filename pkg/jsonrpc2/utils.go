2cbb7418aaa59b90512dd88ddb4a882743fc20eb
Prashant Yadav <34992934+prashantkumar1982@users.noreply.github.com>
2025-06-26 17:29:29 -0700

    jsonrpc2 library simplification (#1293)
    * Make auth optional
    
    * refactoring
    
    * json rpc library refactoring

--- pkg/jsonrpc2/utils.go
+++ pkg/jsonrpc2/utils.go
@@ -3,14 +3,9 @@ package jsonrpc2
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
-	"strings"
 )
 
-type Handler struct {
-}
-
-func (*Handler) DecodeRequest(requestBytes []byte, jwtTokenFromHeader string) (Request, error) {
+func DecodeRequest(requestBytes []byte, jwtTokenFromHeader string) (Request, error) {
 	var request Request
 	err := json.Unmarshal(requestBytes, &request)
 	if err != nil {
@@ -23,49 +18,36 @@ func (*Handler) DecodeRequest(requestBytes []byte, jwtTokenFromHeader string) (R
 		return Request{}, errors.New("empty method field")
 	}
 	if request.Params == nil {
-		return Request{}, ErrInvalidParams
+		return Request{}, errors.New("invalid params")
 	}
 	if request.Auth != "" {
 		return request, nil
 	}
-
-	if jwtTokenFromHeader == "" {
-		return request, errors.New("missing auth token")
-	}
-
 	request.Auth = jwtTokenFromHeader
-
 	return request, nil
 }
 
-func (*Handler) EncodeRequest(request *Request) ([]byte, error) {
+func EncodeRequest(request *Request) ([]byte, error) {
 	return json.Marshal(request)
 }
 
-func (*Handler) DecodeResponse(responseBytes []byte) (Response, error) {
+func DecodeResponse(responseBytes []byte) (Response, error) {
 	var response Response
 	err := json.Unmarshal(responseBytes, &response)
 	if err != nil {
 		return Response{}, err
 	}
-	if response.Error != nil {
-		return Response{}, fmt.Errorf("received non-empty error field: %v", response.Error)
-	}
 	return response, nil
 }
 
-func (*Handler) EncodeResponse(response *Response) ([]byte, error) {
+func EncodeResponse(response *Response) ([]byte, error) {
 	return json.Marshal(response)
 }
 
-func (r *Request) EncodeErrorReponse(err *WireError) ([]byte, error) {
+func EncodeErrorReponse(id string, err *WireError) ([]byte, error) {
 	return json.Marshal(Response{
 		Version: JsonRpcVersion,
-		ID:      r.ID,
+		ID:      id,
 		Error:   err,
 	})
-}
-
-func (r *Request) ServiceName() string {
-	return strings.Split(r.Method, ".")[0]
 }

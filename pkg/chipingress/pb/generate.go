df2c53c5fc027355f56f82a8272e36d494e56542
hendoxc <hagen.henderson@smartcontract.com>
2025-06-30 09:21:29 -0600

    Removes generate.go

--- pkg/chipingress/pb/generate.go
+++
@@ -1,2 +0,0 @@
-//go:generate protoc --proto_path . --proto_path ../../.. --proto_path ../../../vendor --go_out . --go_opt paths=source_relative --go-grpc_out . --go-grpc_opt paths=source_relative chip_ingress.proto
-package pb

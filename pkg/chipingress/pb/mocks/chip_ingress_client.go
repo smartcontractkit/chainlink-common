49f54f1c379d380b0279045068efb9a8f2b0a132
Hagen H <42331373+hendoxc@users.noreply.github.com>
2025-06-30 11:08:04 -0600

    Update chip ingress client (#1306)
    * Adds `NewEventWithAttributes`
    
    - marks `NewEvent` as deprecated
    
    * Removes default cfg func
    
    Removes default cfg func
    
    * Upgrades cloud events version
    
    * Regenerate chip protos
    
    - adds generate.go
    
    * Makes removes grpc client wrapper indirection
    
    * Updates chip-ingress in beholder client
    
    Removes mockery entry
    
    Fixes test
    
    Runs `fmt`
    
    * Removes generate.go

--- pkg/chipingress/pb/mocks/chip_ingress_client.go
+++ pkg/chipingress/pb/mocks/chip_ingress_client.go
@@ -249,6 +249,79 @@ func (_c *ChipIngressClient_PublishBatch_Call) RunAndReturn(run func(context.Con
 	return _c
 }
 
+// StreamEvents provides a mock function with given fields: ctx, opts
+func (_m *ChipIngressClient) StreamEvents(ctx context.Context, opts ...grpc.CallOption) (grpc.BidiStreamingClient[pb.StreamEventsRequest, pb.StreamEventsResponse], error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	if len(ret) == 0 {
+		panic("no return value specified for StreamEvents")
+	}
+
+	var r0 grpc.BidiStreamingClient[pb.StreamEventsRequest, pb.StreamEventsResponse]
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, ...grpc.CallOption) (grpc.BidiStreamingClient[pb.StreamEventsRequest, pb.StreamEventsResponse], error)); ok {
+		return rf(ctx, opts...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, ...grpc.CallOption) grpc.BidiStreamingClient[pb.StreamEventsRequest, pb.StreamEventsResponse]); ok {
+		r0 = rf(ctx, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(grpc.BidiStreamingClient[pb.StreamEventsRequest, pb.StreamEventsResponse])
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ChipIngressClient_StreamEvents_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'StreamEvents'
+type ChipIngressClient_StreamEvents_Call struct {
+	*mock.Call
+}
+
+// StreamEvents is a helper method to define mock.On call
+//   - ctx context.Context
+//   - opts ...grpc.CallOption
+func (_e *ChipIngressClient_Expecter) StreamEvents(ctx interface{}, opts ...interface{}) *ChipIngressClient_StreamEvents_Call {
+	return &ChipIngressClient_StreamEvents_Call{Call: _e.mock.On("StreamEvents",
+		append([]interface{}{ctx}, opts...)...)}
+}
+
+func (_c *ChipIngressClient_StreamEvents_Call) Run(run func(ctx context.Context, opts ...grpc.CallOption)) *ChipIngressClient_StreamEvents_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		variadicArgs := make([]grpc.CallOption, len(args)-1)
+		for i, a := range args[1:] {
+			if a != nil {
+				variadicArgs[i] = a.(grpc.CallOption)
+			}
+		}
+		run(args[0].(context.Context), variadicArgs...)
+	})
+	return _c
+}
+
+func (_c *ChipIngressClient_StreamEvents_Call) Return(_a0 grpc.BidiStreamingClient[pb.StreamEventsRequest, pb.StreamEventsResponse], _a1 error) *ChipIngressClient_StreamEvents_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *ChipIngressClient_StreamEvents_Call) RunAndReturn(run func(context.Context, ...grpc.CallOption) (grpc.BidiStreamingClient[pb.StreamEventsRequest, pb.StreamEventsResponse], error)) *ChipIngressClient_StreamEvents_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // NewChipIngressClient creates a new instance of ChipIngressClient. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewChipIngressClient(t interface {

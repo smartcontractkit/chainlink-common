6a64336cce57e81cb176207af0d3d56e37357b93
Ryan Tinianov <tinianov@live.com>
2025-07-02 13:08:28 -0400

    Update values in the main mod and respect the renamed funtion (#1325)

--- pkg/workflows/wasm/pb/generate/generate.go
+++ pkg/workflows/wasm/pb/generate/generate.go
@@ -5,7 +5,7 @@ import "github.com/smartcontractkit/chainlink-common/pkg/values/installer/pkg"
 func main() {
 	gen := &pkg.ProtocGen{}
 	gen.AddSourceDirectories("../../..", ".")
-	if err := gen.Generate("wasm.proto", "."); err != nil {
+	if err := gen.GenerateFile("wasm.proto", "."); err != nil {
 		panic(err)
 	}
 }

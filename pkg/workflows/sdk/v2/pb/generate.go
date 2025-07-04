9304dd291af5bace394c50c085bb3433640533bb
Ryan Tinianov <tinianov@live.com>
2025-07-04 16:28:24 -0400

    Add the metadata to the sdk module so it can be consumed with it always

--- pkg/workflows/sdk/v2/pb/generate.go
+++ pkg/workflows/sdk/v2/pb/generate.go
@@ -1,4 +1,6 @@
 //go:generate go run ./gen
 //go:generate mv sdk/v1alpha/sdk.pb.go sdk.pb.go
 //go:generate rm -rf sdk
+//go:generate mv tools/generator/v1alpha/cre_metadata.pb.go cre_metadata.pb.go
+//go:generate rm -rf tools
 package pb

4982d339809e38a1ac03a3e85d3b6963eef2e574
Ryan Tinianov <tinianov@live.com>
2025-07-04 15:35:33 -0400

    Use the new module (updates protos too)

--- pkg/capabilities/v2/chain-capabilities/evm/generate.go
+++
@@ -1,2 +0,0 @@
-//go:generate go run ../../gen --pkg=github.com/smartcontractkit/chainlink-common/pkg/capabilities/v2/chain-capabilities/evm --file=capabilities/blockchain/evm/v1alpha/client.proto
-package evm

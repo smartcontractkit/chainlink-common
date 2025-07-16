97bbd33adc43db45e9cfb9421c8237f0db70dd7f
jinhoonbang <jin.bang@smartcontract.com>
2025-07-16 22:41:56 +0900

    change auth metata to workflow metadata and include workflow selector

--- pkg/types/gateway/workflow_metadata.go
+++ pkg/types/gateway/workflow_metadata.go
@@ -8,12 +8,12 @@ const (
 
 type KeyType string
 
-// WorkflowAuthMetadata represents the metadata for workflow authorization
+// WorkflowMetadata represents the workflow metadata for HTTP triggers including auth data.
 // This type is used for communication between the gateway handler in the gateway node and
 // the gateway connector handler in the workflow node.
-type WorkflowAuthMetadata struct {
-	WorkflowID     string
-	AuthorizedKeys []AuthorizedKey
+type WorkflowMetadata struct {
+	WorkflowSelector WorkflowSelector
+	AuthorizedKeys   []AuthorizedKey
 }
 
 type AuthorizedKey struct {

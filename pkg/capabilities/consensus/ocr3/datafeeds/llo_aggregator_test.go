6bd054a1c42972d7d3b51a6747f1e7309e0a6bd4
cawthorne <gregory.cawthorne@smartcontract.com>
2025-07-18 18:08:28 +0100

    In LLO Aggregation skip Stream update when Observation is absent (#1411)
    * In LLO Aggregation use the stream old price when Observation is absent
    
    * Ensure we have a price for this stream in the prices map in case a heartbeat is triggered
    
    * Add re-used price to log
    
    * Add tests
    
    * Skip LLO Stream updates when Stream value is missing in Aggregator

--- pkg/capabilities/consensus/ocr3/datafeeds/llo_aggregator_test.go
+++ pkg/capabilities/consensus/ocr3/datafeeds/llo_aggregator_test.go
@@ -407,7 +407,83 @@ func TestLLOAggregator_Aggregate(t *testing.T) {
 			observations:         map[ocrcommon.OracleID][]values.Value{},
 			f:                    1,
 			expectedShouldReport: false,
-			expectError:          true, // Should error with empty observations
+			expectError:          true, // Should error with empty observations and no previous outcome
+		},
+
+		{
+			name: "previous outcome, empty observation, no update due to heartbeat",
+			config: datafeeds.LLOAggregatorConfig{
+				Streams: map[string]datafeeds.FeedConfig{
+					"1": {
+						Deviation:     decimal.NewFromFloat(0.1).String(), // 10%
+						Heartbeat:     3600,                               // 1 hour
+						RemappedIDHex: remappedHex1,
+					},
+					"2": {
+						Deviation:     decimal.NewFromFloat(0.1).String(), // 10%
+						Heartbeat:     3600,                               // 1 hour
+						RemappedIDHex: remappedHex2,
+					},
+				},
+			},
+			previousOutcome: createPreviousOutcome(t, map[uint32]struct {
+				price     decimal.Decimal
+				timestamp int64
+			}{
+				1: {
+					price:     decimal.NewFromFloat(100),
+					timestamp: testStartTime.Add(-61 * time.Minute).UnixNano(), // Over 1 the hour heartbeat
+				},
+				2: {
+					price:     decimal.NewFromFloat(101),
+					timestamp: testStartTime.Add(-10 * time.Minute).UnixNano(), // 10 minutes ago, under the 1 hour heartbeat
+				},
+			}),
+
+			observations: createObservations(t, testStartTime, map[uint32]decimal.Decimal{ //nolint: gosec // G115
+				2: decimal.NewFromFloat(101), // no change
+			}),
+			f:                    1,
+			expectedShouldReport: false, // No update despite heartbeat, as no observation for stream 1
+			expectedStreamIDs:    []uint32{},
+		},
+
+		{
+			name: "previous outcome, empty observation, no update before heartbeat",
+			config: datafeeds.LLOAggregatorConfig{
+				Streams: map[string]datafeeds.FeedConfig{
+					"1": {
+						Deviation:     decimal.NewFromFloat(0.1).String(), // 10%
+						Heartbeat:     3600,                               // 1 hour
+						RemappedIDHex: remappedHex1,
+					},
+					"2": {
+						Deviation:     decimal.NewFromFloat(0.1).String(), // 10%
+						Heartbeat:     3600,                               // 1 hour
+						RemappedIDHex: remappedHex2,
+					},
+				},
+			},
+			previousOutcome: createPreviousOutcome(t, map[uint32]struct {
+				price     decimal.Decimal
+				timestamp int64
+			}{
+				1: {
+					price:     decimal.NewFromFloat(100),
+					timestamp: testStartTime.Add(-10 * time.Minute).UnixNano(), // 10 minutes ago, under the 1 hour heartbeat
+				},
+				2: {
+					price:     decimal.NewFromFloat(101),
+					timestamp: testStartTime.Add(-10 * time.Minute).UnixNano(), // 10 minutes ago, under the 1 hour heartbeat
+				},
+			}),
+
+			observations: createObservations(t, testStartTime, map[uint32]decimal.Decimal{ //nolint: gosec // G115
+				2: decimal.NewFromFloat(101), // no change
+			}),
+			f:                    1,
+			expectedShouldReport: false,
+			expectedStreamIDs:    []uint32{},
 		},
 	}
 

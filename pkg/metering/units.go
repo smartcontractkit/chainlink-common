f42955000877a41acafb9add68f57141dddf4eb1
patrickhuie19 <patrick.huie@smartcontract.com>
2025-08-05 10:54:49 -0400

    switching metering units over to billing proto

--- pkg/metering/units.go
+++ pkg/metering/units.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 
 	chainselectors "github.com/smartcontractkit/chain-selectors"
+	billing "github.com/smartcontractkit/chainlink-protos/billing/go"
 )
 
 var (
-	PayloadUnit = unit{Name: "payload", Unit: "bytes"}
+	PayloadUnit = unit{
+		Name: billing.ResourceType_RESOURCE_TYPE_NETWORK.String(),
+		Unit: billing.MeasurementUnit_MEASUREMENT_UNIT_BYTES.String()}
 
-	// ComputeUnit is an example.
-	ComputeUnit = unit{Name: "compute", Unit: "ms"}
+
+	ComputeUnit = unit{
+		Name: billing.ResourceType_RESOURCE_TYPE_COMPUTE.String(),
+		Unit: billing.MeasurementUnit_MEASUREMENT_UNIT_MILLISECONDS.String()}
 )
 
 // unit provides exported Name and unit fields for

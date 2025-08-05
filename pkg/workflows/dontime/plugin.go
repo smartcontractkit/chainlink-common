d8c93f095b4dc09ee2be64d59001d15bda59e01c
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2025-08-05 14:42:15 -0400

    Log oracle ID

--- pkg/workflows/dontime/plugin.go
+++ pkg/workflows/dontime/plugin.go
@@ -33,6 +33,7 @@ type Plugin struct {
 var _ ocr3types.ReportingPlugin[[]byte] = (*Plugin)(nil)
 
 func NewPlugin(store *Store, config ocr3types.ReportingPluginConfig, lggr logger.Logger) (*Plugin, error) {
+	lggr.Infow("ORACLE ID", config.OracleID)
 	offchainCfg := &pb.Config{}
 	err := proto.Unmarshal(config.OffchainConfig, offchainCfg)
 	if err != nil {

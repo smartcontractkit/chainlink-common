80ca29d97e54692d79cc2c81190b0185cd987d56
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2025-08-05 12:26:44 -0400

    Update transmitter.go

--- pkg/workflows/dontime/transmitter.go
+++ pkg/workflows/dontime/transmitter.go
@@ -28,6 +28,7 @@ func NewTransmitter(lggr logger.Logger, store *Store, fromAccount types.Account)
 func (t *Transmitter) Transmit(_ context.Context, _ types.ConfigDigest, _ uint64, r ocr3types.ReportWithInfo[[]byte], _ []types.AttributedOnchainSignature) error {
 	outcome := &pb.Outcome{}
 	if err := proto.Unmarshal(r.Report, outcome); err != nil {
+		t.lggr.Errorf("failed to unmarshal report")
 		return err
 	}
 

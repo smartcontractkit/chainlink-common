20edd1ffee5a6dc58fe97cea92c3758915aa932d
Jordan Krage <jmank88@gmail.com>
2025-08-21 11:19:10 -0500

    add docs

--- pkg/utils/lazy.go
+++ pkg/utils/lazy.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 )
 
+// LazyLoad lazily loads a T when Get is called.
 type LazyLoad[T any] struct {
 	f     func() (T, error)
 	state T
@@ -12,10 +13,12 @@ type LazyLoad[T any] struct {
 	lock  sync.Mutex
 }
 
+// NewLazyLoad returns a new LazyLoad.
 func NewLazyLoad[T any](f func() (T, error)) *LazyLoad[T] {
 	return &LazyLoad[T]{f: f}
 }
 
+// Get returns the cached value, or loads it lazily if necessary.
 func (l *LazyLoad[T]) Get() (out T, err error) {
 	l.lock.Lock()
 	defer l.lock.Unlock()
@@ -34,6 +37,7 @@ func (l *LazyLoad[T]) Reset() {
 	l.ok = false
 }
 
+// LazyLoadCtx is like LazyLoad, but supports [context.Context]
 type LazyLoadCtx[T any] struct {
 	f     func(context.Context) (T, error)
 	state T
@@ -41,10 +45,12 @@ type LazyLoadCtx[T any] struct {
 	mu    chan struct{}
 }
 
+// NewLazyLoadCtx returns a new LazyLoadCtx.
 func NewLazyLoadCtx[T any](f func(context.Context) (T, error)) *LazyLoadCtx[T] {
 	return &LazyLoadCtx[T]{f: f, mu: make(chan struct{}, 1)}
 }
 
+// Get returns the cached value, or loads it lazily if necessary.
 func (l *LazyLoadCtx[T]) Get(ctx context.Context) (out T, err error) {
 	select {
 	case <-ctx.Done():

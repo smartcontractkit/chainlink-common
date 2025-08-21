7cd12a1d05e323a71ee1405009b7b02a6eaf5093
Jordan Krage <jmank88@gmail.com>
2025-08-20 19:18:59 -0500

    pkg/utils: add LazyLoadCtx variant

--- pkg/utils/lazy.go
+++ pkg/utils/lazy.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"context"
 	"sync"
 )
 
@@ -12,9 +13,7 @@ type LazyLoad[T any] struct {
 }
 
 func NewLazyLoad[T any](f func() (T, error)) *LazyLoad[T] {
-	return &LazyLoad[T]{
-		f: f,
-	}
+	return &LazyLoad[T]{f: f}
 }
 
 func (l *LazyLoad[T]) Get() (out T, err error) {
@@ -32,5 +31,40 @@ func (l *LazyLoad[T]) Get() (out T, err error) {
 func (l *LazyLoad[T]) Reset() {
 	l.lock.Lock()
 	defer l.lock.Unlock()
+	l.ok = false
+}
+
+type LazyLoadCtx[T any] struct {
+	f     func(context.Context) (T, error)
+	state T
+	ok    bool
+	mu    chan struct{}
+}
+
+func NewLazyLoadCtx[T any](f func(context.Context) (T, error)) *LazyLoadCtx[T] {
+	return &LazyLoadCtx[T]{f: f, mu: make(chan struct{}, 1)}
+}
+
+func (l *LazyLoadCtx[T]) Get(ctx context.Context) (out T, err error) {
+	select {
+	case <-ctx.Done():
+		return out, ctx.Err()
+	case l.mu <- struct{}{}: // lock
+	}
+	defer func() { <-l.mu }() // unlock
+
+	if l.ok {
+		return l.state, nil
+	}
+	l.state, err = l.f(ctx)
+	l.ok = err == nil
+	return l.state, err
+}
+
+func (l *LazyLoadCtx[T]) Reset() {
+	select {
+	case l.mu <- struct{}{}: // lock
+	}
+	defer func() { <-l.mu }() // unlock
 	l.ok = false
 }

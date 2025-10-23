b336d976ab536323dbf49c5153570afbd02d2e38
kirqz23 <kiryll.kuzniecow@gmail.com>
2025-10-23 13:39:23 +0200

    Add beholder_log_source attribute

--- pkg/beholder/attributes.go
+++ pkg/beholder/attributes.go
@@ -5,4 +5,5 @@ const (
 	AttrKeyEntity     = "beholder_entity"
 	AttrKeyDomain     = "beholder_domain"
 	AttrKeyDataType   = "beholder_data_type"
+	AttrKeyLogSource  = "beholder_log_source" // used to differentiate log sources (e.g. loop, core, etc.)
 )

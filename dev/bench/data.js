ba467418d833df40ffa62a8915a73fe24c130399
github-action-benchmark <github@users.noreply.github.com>
2026-01-02 21:51:49 +0000

    add Benchmark (go) benchmark result for 1c575649e460ae4be79bd1c202946cd213ac1293

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767189860148,
+  "lastUpdate": 1767390709123,
   "repoUrl": "https://github.com/smartcontractkit/chainlink-common",
   "entries": {
     "Benchmark": [
@@ -34140,6 +34140,66 @@ window.BENCHMARK_DATA = {
             "value": 141338,
             "unit": "ns/op",
             "extra": "8132 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "1416262+bolekk@users.noreply.github.com",
+            "name": "Bolek",
+            "username": "bolekk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1c575649e460ae4be79bd1c202946cd213ac1293",
+          "message": "[CRE] Adjust default limits (#1748)\n\n1. Bump capability concurrency up to 30. Relaying on concurrency limit can cause problems with OCR when different nodes throttle different requests. Let's rely on per-capability limits instead.\n2. Reduce consensus call limit to something sane.\n3. Reduce trigger event queus size. Usually queueing a lot of events makes them cross the expiration threshold anyway.",
+          "timestamp": "2026-01-02T21:44:18Z",
+          "tree_id": "8764b25488cc23eeedc3136971f4ab3047e7526e",
+          "url": "https://github.com/smartcontractkit/chainlink-common/commit/1c575649e460ae4be79bd1c202946cd213ac1293"
+        },
+        "date": 1767390707043,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkKeystore_Sign/nop/in-process",
+            "value": 721.5,
+            "unit": "ns/op",
+            "extra": "1425394 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/nop/out-of-process",
+            "value": 102175,
+            "unit": "ns/op",
+            "extra": "10000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/hex/in-process",
+            "value": 409.3,
+            "unit": "ns/op",
+            "extra": "2894988 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/hex/out-of-process",
+            "value": 101664,
+            "unit": "ns/op",
+            "extra": "10000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/ed25519/in-process",
+            "value": 26731,
+            "unit": "ns/op",
+            "extra": "45008 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/ed25519/out-of-process",
+            "value": 152993,
+            "unit": "ns/op",
+            "extra": "7125 times\n4 procs"
           }
         ]
       }

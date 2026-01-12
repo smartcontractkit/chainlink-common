06974e6ff7a269908b653a8e5d26129fc0d212aa
connorwstein <connor.stein@mail.mcgill.ca>
2026-01-12 15:29:10 -0500

    Simpler

--- keystore/kms/internal/asn1.go
+++ keystore/kms/internal/asn1.go
@@ -1,6 +1,16 @@
 package kms
 
-import "encoding/asn1"
+import (
+	"bytes"
+	"encoding/asn1"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+)
 
 // SPKI represents the SubjectPublicKeyInfo structure as defined in [RFC 5280] in ASN.1 format.
 //
@@ -31,4 +41,102 @@ type SPKIAlgorithmIdentifier struct {
 type ECDSASig struct {
 	R asn1.RawValue
 	S asn1.RawValue
+}
+
+var (
+	// secp256k1N is the N value of the secp256k1 curve, used to adjust the S value in signatures.
+	secp256k1N = crypto.S256().Params().N
+	// secp256k1HalfN is half of the secp256k1 N value, used to adjust the S value in signatures.
+	secp256k1HalfN = new(big.Int).Div(secp256k1N, big.NewInt(2))
+)
+
+// KMSToSEC1Sig converts a KMS signature (ASN.1 format) to SEC1 format (R || S || V). This follows this
+// example provided by AWS Guides. Notably Ethereum and most blockchain systems use the SEC1 format for signatures.
+//
+// [AWS Guides]: https://aws.amazon.com/blogs/database/part2-use-aws-kms-to-securely-manage-ethereum-accounts/
+func KMSToSEC1Sig(kmsSig, ecdsaPubKeyBytes, hash []byte) ([]byte, error) {
+	var ecdsaSig ECDSASig
+	if _, err := asn1.Unmarshal(kmsSig, &ecdsaSig); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal KMS signature: %w", err)
+	}
+
+	rBytes := ecdsaSig.R.Bytes
+	sBytes := ecdsaSig.S.Bytes
+
+	// Adjust S value from signature to match SEC1 standard.
+	//
+	// After we extract r and s successfully, we have to test if the value of s is greater than
+	// secp256k1n/2 as specified in EIP-2 and flip it if required.
+	sBigInt := new(big.Int).SetBytes(sBytes)
+	if sBigInt.Cmp(secp256k1HalfN) > 0 {
+		sBytes = new(big.Int).Sub(secp256k1N, sBigInt).Bytes()
+	}
+
+	return recoverSEC1Signature(ecdsaPubKeyBytes, hash, rBytes, sBytes)
+}
+
+// recoverSEC1Signature attempts to reconstruct the SEC1 signature by trying both possible recovery
+// IDs (v = 0 and v = 1). It compares the recovered public key with the expected public key bytes
+// to determine the correct signature.
+//
+// Returns the valid SEC1 signature if successful, or an error if neither recovery ID matches.
+func recoverSEC1Signature(expectedPublicKey, txHash, r, s []byte) ([]byte, error) {
+	// SEC1 signatures require r and s to be exactly 32 bytes each.
+	rsSig := append(padTo32Bytes(r), padTo32Bytes(s)...)
+	// SEC1 signatures have a 65th byte called the recovery ID (v), which can be 0 or 1.
+	// Here we append 0 to the signature to start with for the first recovery attempt.
+	sec1Sig := append(rsSig, []byte{0}...)
+
+	recoveredPublicKey, err := crypto.Ecrecover(txHash, sec1Sig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to recover signature with v=0: %w", err)
+	}
+
+	if hex.EncodeToString(recoveredPublicKey) != hex.EncodeToString(expectedPublicKey) {
+		// If the first recovery attempt failed, we try with v=1.
+		sec1Sig = append(rsSig, []byte{1}...)
+		recoveredPublicKey, err = crypto.Ecrecover(txHash, sec1Sig)
+		if err != nil {
+			return nil, fmt.Errorf("failed to recover signature with v=1: %w", err)
+		}
+
+		if hex.EncodeToString(recoveredPublicKey) != hex.EncodeToString(expectedPublicKey) {
+			return nil, errors.New("cannot reconstruct public key from sig")
+		}
+	}
+
+	return sec1Sig, nil
+}
+
+// ASN1ToSEC1PublicKey converts a KMS public key (ASN.1 DER-encoded SPKI format) to SEC1 format
+// (uncompressed: 0x04 || X || Y, 65 bytes).
+//
+// KMS returns public keys in ASN.1 DER-encoded SubjectPublicKeyInfo (SPKI) format as defined in
+// RFC 5280. This function extracts the public key and converts it to SEC1 uncompressed format.
+func ASN1ToSEC1PublicKey(asn1PublicKey []byte) ([]byte, error) {
+	var spki SPKI
+	if _, err := asn1.Unmarshal(asn1PublicKey, &spki); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal ASN.1 public key: %w", err)
+	}
+
+	pubKey, err := crypto.UnmarshalPubkey(spki.SubjectPublicKey.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal public key: %w", err)
+	}
+
+	// SEC1 uncompressed format: 0x04 || X || Y (65 bytes)
+	pubKeyBytes := secp256k1.S256().Marshal(pubKey.X, pubKey.Y)
+	return pubKeyBytes, nil
+}
+
+// padTo32Bytes pads the given byte slice to 32 bytes by trimming leading zeros and prepending
+// zeros.
+func padTo32Bytes(buffer []byte) []byte {
+	buffer = bytes.TrimLeft(buffer, "\x00")
+	for len(buffer) < 32 {
+		zeroBuf := []byte{0}
+		buffer = append(zeroBuf, buffer...)
+	}
+
+	return buffer
 }
--- keystore/kms/keystore.go
+++ keystore/kms/keystore.go
@@ -1,24 +1,20 @@
 package kms
 
 import (
-	"bytes"
 	"context"
-	"encoding/asn1"
-	"encoding/hex"
-	"errors"
 	"fmt"
-	"math/big"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	kmslib "github.com/aws/aws-sdk-go/service/kms"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/smartcontractkit/chainlink-common/keystore"
 	kms "github.com/smartcontractkit/chainlink-common/keystore/kms/internal"
 	kmsinternal "github.com/smartcontractkit/chainlink-common/keystore/kms/internal"
 )
 
+// KeystoreConfig is the configuration for the KMS keystore.
+// Struct for extensibility in the future.
+// Uses the default region from the AWS profile.
 type KeystoreConfig struct {
 	AWSProfile string
 }
@@ -29,9 +25,7 @@ type kmsKeystoreSignerReader struct {
 }
 
 func NewKMSKeystore(config KeystoreConfig) (keystore.KeystoreSignerReader, error) {
-	client, err := kmsinternal.NewClient(kmsinternal.ClientConfig{
-		AWSProfile: config.AWSProfile,
-	})
+	client, err := kmsinternal.NewClient(config.AWSProfile)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create KMS client: %w", err)
 	}
@@ -44,20 +38,19 @@ func NewKMSKeystore(config KeystoreConfig) (keystore.KeystoreSignerReader, error
 // keySpecToKeyType converts an AWS KMS KeySpec to a keystore KeyType.
 // AWS KMS supports:
 //   - ECC_SECG_P256K1 (secp256k1) -> ECDSA_S256
-//   - ECC_NIST_P256, ECC_NIST_P384, ECC_NIST_P521 -> not supported (different curves)
-//   - RSA_* -> not supported
-//   - SYMMETRIC_DEFAULT -> not supported
-//
-// Note: AWS KMS does not support Ed25519 keys.
 func keySpecToKeyType(keySpec string) (keystore.KeyType, error) {
 	switch keySpec {
-	case "ECC_SECG_P256K1":
+	case kmslib.KeySpecEccSecgP256k1:
 		return keystore.ECDSA_S256, nil
 	default:
 		return "", fmt.Errorf("unsupported KMS key spec: %s (only ECC_SECG_P256K1 is supported)", keySpec)
 	}
 }
 
+// GetKeys lists all keys in the KMS keystore.
+// Note: possible that ListKeys is not supported for a given AWS role
+// but specific GetPublicKey/DescribeKey are supported. We transparently
+// let the user use whatever they are permitted to given their AWS perms.
 func (k *kmsKeystoreSignerReader) GetKeys(ctx context.Context, req keystore.GetKeysRequest) (keystore.GetKeysResponse, error) {
 	if len(req.KeyNames) == 0 {
 		listResp, err := k.client.ListKeys(&kmslib.ListKeysInput{})
@@ -113,146 +106,59 @@ func (k *kmsKeystoreSignerReader) GetKeys(ctx context.Context, req keystore.GetK
 	return keystore.GetKeysResponse{Keys: keys}, nil
 }
 
-var (
-	// secp256k1N is the N value of the secp256k1 curve, used to adjust the S value in signatures.
-	secp256k1N = crypto.S256().Params().N
-	// secp256k1HalfN is half of the secp256k1 N value, used to adjust the S value in signatures.
-	secp256k1HalfN = new(big.Int).Div(secp256k1N, big.NewInt(2))
-)
-
 // Sign signs data using the KMS key specified by the key name.
 func (k *kmsKeystoreSignerReader) Sign(ctx context.Context, req keystore.SignRequest) (keystore.SignResponse, error) {
-	// TODO: Handle other key types
-	if len(req.Data) != 32 {
-		return keystore.SignResponse{}, fmt.Errorf("data must be 32 bytes for ECDSA_S256, got %d: %w", len(req.Data), keystore.ErrInvalidSignRequest)
-	}
 	key, err := k.client.GetPublicKey(&kmslib.GetPublicKeyInput{
 		KeyId: aws.String(req.KeyName),
 	})
 	if err != nil {
 		return keystore.SignResponse{}, fmt.Errorf("failed to get public key for key %s: %w", req.KeyName, err)
 	}
-	var spki kms.SPKI
-	if _, err = asn1.Unmarshal(key.PublicKey, &spki); err != nil {
-		return keystore.SignResponse{}, fmt.Errorf("cannot parse asn1 public key for KeyId=%s: %w", req.KeyName, err)
-	}
-	pubKey, err := crypto.UnmarshalPubkey(spki.SubjectPublicKey.Bytes)
-	if err != nil {
-		return keystore.SignResponse{}, fmt.Errorf("failed to unmarshal public key: %w", err)
-	}
-	pubKeyBytes := secp256k1.S256().Marshal(pubKey.X, pubKey.Y)
-
-	// MessageType is digest because its prehashed.
-	sig, err := k.client.Sign(&kmslib.SignInput{
-		KeyId:            aws.String(req.KeyName),
-		Message:          req.Data,
-		SigningAlgorithm: aws.String(string(kmslib.SigningAlgorithmSpecEcdsaSha256)),
-		MessageType:      aws.String(string(kmslib.MessageTypeDigest)),
+	describeKey, err := k.client.DescribeKey(&kmslib.DescribeKeyInput{
+		KeyId: aws.String(req.KeyName),
 	})
 	if err != nil {
-		return keystore.SignResponse{}, fmt.Errorf("failed to sign data: %w", err)
+		return keystore.SignResponse{}, fmt.Errorf("failed to describe key %s: %w", req.KeyName, err)
 	}
-	signature, err := kmsToEVMSig(sig.Signature, pubKeyBytes, req.Data)
+	keySpec := aws.StringValue(describeKey.KeyMetadata.KeySpec)
+	keyType, err := keySpecToKeyType(keySpec)
 	if err != nil {
-		return keystore.SignResponse{}, fmt.Errorf("failed to convert KMS signature to EVM signature: %w", err)
-	}
-	return keystore.SignResponse{
-		Signature: signature,
-	}, nil
-}
-
-// kmsToEVMSig converts a KMS signature to an Ethereum-compatible signature. This follows this
-// example provided by AWS Guides.
-//
-// [AWS Guides]: https://aws.amazon.com/blogs/database/part2-use-aws-kms-to-securely-manage-ethereum-accounts/
-func kmsToEVMSig(kmsSig, ecdsaPubKeyBytes, hash []byte) ([]byte, error) {
-	var ecdsaSig kms.ECDSASig
-	if _, err := asn1.Unmarshal(kmsSig, &ecdsaSig); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal KMS signature: %w", err)
+		return keystore.SignResponse{}, fmt.Errorf("key %s: %w", req.KeyName, err)
 	}
 
-	rBytes := ecdsaSig.R.Bytes
-	sBytes := ecdsaSig.S.Bytes
-
-	// Adjust S value from signature to match EVM standard.
-	//
-	// After we extract r and s successfully, we have to test if the value of s is greater than
-	// secp256k1n/2 as specified in EIP-2 and flip it if required.
-	sBigInt := new(big.Int).SetBytes(sBytes)
-	if sBigInt.Cmp(secp256k1HalfN) > 0 {
-		sBytes = new(big.Int).Sub(secp256k1N, sBigInt).Bytes()
-	}
-
-	return recoverEVMSignature(ecdsaPubKeyBytes, hash, rBytes, sBytes)
-}
-
-// recoverEVMSignature attempts to reconstruct the EVM signature by trying both possible recovery
-// IDs (v = 0 and v = 1). It compares the recovered public key with the expected public key bytes
-// to determine the correct signature.
-//
-// Returns the valid EVM signature if successful, or an error if neither recovery ID matches.
-func recoverEVMSignature(expectedPublicKey, txHash, r, s []byte) ([]byte, error) {
-	// Ethereum signatures require r and s to be exactly 32 bytes each.
-	rsSig := append(padTo32Bytes(r), padTo32Bytes(s)...)
-	// Ethereum signatures have a 65th byte called the recovery ID (v), which can be 0 or 1.
-	// Here we append 0 to the signature to start with for the first recovery attempt.
-	evmSig := append(rsSig, []byte{0}...)
-
-	recoveredPublicKey, err := crypto.Ecrecover(txHash, evmSig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to recover signature with v=0: %w", err)
-	}
-
-	if hex.EncodeToString(recoveredPublicKey) != hex.EncodeToString(expectedPublicKey) {
-		// If the first recovery attempt failed, we try with v=1.
-		evmSig = append(rsSig, []byte{1}...)
-		recoveredPublicKey, err = crypto.Ecrecover(txHash, evmSig)
+	switch keyType {
+	case keystore.ECDSA_S256:
+		if len(req.Data) != 32 {
+			return keystore.SignResponse{}, fmt.Errorf("data must be 32 bytes for ECDSA_S256, got %d: %w", len(req.Data), keystore.ErrInvalidSignRequest)
+		}
+		pubKeyBytes, err := kms.ASN1ToSEC1PublicKey(key.PublicKey)
 		if err != nil {
-			return nil, fmt.Errorf("failed to recover signature with v=1: %w", err)
+			return keystore.SignResponse{}, fmt.Errorf("failed to convert public key for KeyId=%s: %w", req.KeyName, err)
 		}
 
-		if hex.EncodeToString(recoveredPublicKey) != hex.EncodeToString(expectedPublicKey) {
-			return nil, errors.New("cannot reconstruct public key from sig")
+		// MessageType is digest because its prehashed.
+		sig, err := k.client.Sign(&kmslib.SignInput{
+			KeyId:            aws.String(req.KeyName),
+			Message:          req.Data,
+			SigningAlgorithm: aws.String(string(kmslib.SigningAlgorithmSpecEcdsaSha256)),
+			MessageType:      aws.String(string(kmslib.MessageTypeDigest)),
+		})
+		if err != nil {
+			return keystore.SignResponse{}, fmt.Errorf("failed to sign data: %w", err)
 		}
+		signature, err := kms.KMSToSEC1Sig(sig.Signature, pubKeyBytes, req.Data)
+		if err != nil {
+			return keystore.SignResponse{}, fmt.Errorf("failed to convert KMS signature to SEC1 signature: %w", err)
+		}
+		return keystore.SignResponse{
+			Signature: signature,
+		}, nil
+	default:
+		return keystore.SignResponse{}, fmt.Errorf("key %s: %w", req.KeyName, keystore.ErrInvalidSignRequest)
 	}
 
-	return evmSig, nil
-}
-
-// padTo32Bytes pads the given byte slice to 32 bytes by trimming leading zeros and prepending
-// zeros.
-func padTo32Bytes(buffer []byte) []byte {
-	buffer = bytes.TrimLeft(buffer, "\x00")
-	for len(buffer) < 32 {
-		zeroBuf := []byte{0}
-		buffer = append(zeroBuf, buffer...)
-	}
-
-	return buffer
 }
 
 func (k *kmsKeystoreSignerReader) Verify(ctx context.Context, req keystore.VerifyRequest) (keystore.VerifyResponse, error) {
-	if req.KeyType != keystore.ECDSA_S256 {
-		return keystore.VerifyResponse{}, fmt.Errorf("KMS keystore only supports ECDSA_S256, got %s: %w", req.KeyType, keystore.ErrInvalidVerifyRequest)
-	}
-
-	if len(req.Data) != 32 {
-		return keystore.VerifyResponse{}, fmt.Errorf("data must be 32 bytes for ECDSA_S256, got %d: %w", len(req.Data), keystore.ErrInvalidVerifyRequest)
-	}
-
-	// ECDSA_S256 public keys are in SEC1 (uncompressed) format
-	if len(req.PublicKey) != 65 {
-		return keystore.VerifyResponse{}, fmt.Errorf("public key must be 65 bytes for ECDSA_S256, got %d: %w", len(req.PublicKey), keystore.ErrInvalidVerifyRequest)
-	}
-
-	if len(req.Signature) != 65 {
-		return keystore.VerifyResponse{}, fmt.Errorf("signature must be 65 bytes for ECDSA_S256, got %d: %w", len(req.Signature), keystore.ErrInvalidVerifyRequest)
-	}
-
-	// VerifySignature expects 64 bytes [R || S] without the V byte
-	// Strip the V byte (last byte) from the 65-byte signature
-	valid := crypto.VerifySignature(req.PublicKey, req.Data, req.Signature[:64])
-	return keystore.VerifyResponse{
-		Valid: valid,
-	}, nil
+	return keystore.Verify(ctx, req)
 }

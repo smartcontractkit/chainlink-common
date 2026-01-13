e653fe07e0d569d608bb155d073a2406967a6079
cawthorne <cawthornegd@gmail.com>
2026-01-13 16:13:12 +0000

    feat: add Streams LLO NoDAG Trigger capability
    Add generated code and tests for the Streams LLO Trigger capability to
    support NoDAG CRE workflows.
    
    Files added:
    - trigger.pb.go: Generated protobuf Go code for streams trigger
    - server/trigger_server_gen.go: Generated server-side boilerplate
    - streams_test.go: Unit tests for capability interface
    - generate.go: Proto generation script
    
    This capability wraps the existing LLO transmitter to expose it via
    the NoDAG Capability API for CRE DON-to-DON workflows.
    
    Related: chainlink-protos#257 (proto definition)
    Capability ID: streams-trigger@2.0.0

---
+++ pkg/capabilities/v2/triggers/streams/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run ../../gen --pkg=github.com/smartcontractkit/chainlink-common/pkg/capabilities/v2/triggers/streams --file=capabilities/streams/v1/trigger.proto
+package streams
+

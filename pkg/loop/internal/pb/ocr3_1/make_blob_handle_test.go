51ef3ef0fbf55b034a1748d8a84f44c5ac58cf1e
Matthew Pendrey <matthew.pendrey@gmail.com>
2026-01-13 17:00:28 +0000

    wip

---
+++ pkg/loop/internal/pb/ocr3_1/make_blob_handle_test.go
@@ -0,0 +1,17 @@
+package ocr3_1pb
+
+import (
+	"fmt"
+
+	"github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3_1types"
+)
+
+func MakeBlobHandleTest() {
+
+	MakeBlobHandleTest()
+
+	bh := ocr3_1types.BlobHandle{}
+
+	fmt.Printf("BlobHandle: %+v\n", bh)
+
+}

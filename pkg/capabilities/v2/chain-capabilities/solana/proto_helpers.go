622393316e294a9096b496bca3250f9db6f5f8eb
Silas Lenihan <sjl@lenihan.net>
2026-02-06 09:08:28 -0500

    return error on comparator conversion

--- pkg/capabilities/v2/chain-capabilities/solana/proto_helpers.go
+++ pkg/capabilities/v2/chain-capabilities/solana/proto_helpers.go
@@ -9,66 +9,70 @@ import (
 )
 
 // ConvertComparisonOperatorFromProto converts a proto ComparisonOperator to primitives.ComparisonOperator
-func ConvertComparisonOperatorFromProto(op ComparisonOperator) primitives.ComparisonOperator {
+func ConvertComparisonOperatorFromProto(op ComparisonOperator) (primitives.ComparisonOperator, error) {
 	switch op {
 	case ComparisonOperator_COMPARISON_OPERATOR_EQ:
-		return primitives.Eq
+		return primitives.Eq, nil
 	case ComparisonOperator_COMPARISON_OPERATOR_NEQ:
-		return primitives.Neq
+		return primitives.Neq, nil
 	case ComparisonOperator_COMPARISON_OPERATOR_GT:
-		return primitives.Gt
+		return primitives.Gt, nil
 	case ComparisonOperator_COMPARISON_OPERATOR_LT:
-		return primitives.Lt
+		return primitives.Lt, nil
 	case ComparisonOperator_COMPARISON_OPERATOR_GTE:
-		return primitives.Gte
+		return primitives.Gte, nil
 	case ComparisonOperator_COMPARISON_OPERATOR_LTE:
-		return primitives.Lte
+		return primitives.Lte, nil
 	default:
-		return primitives.Eq
+		return 0, fmt.Errorf("unknown comparison operator: %s", op)
 	}
 }
 
 // ConvertComparisonOperatorToProto converts a primitives.ComparisonOperator to proto ComparisonOperator
-func ConvertComparisonOperatorToProto(op primitives.ComparisonOperator) ComparisonOperator {
+func ConvertComparisonOperatorToProto(op primitives.ComparisonOperator) (ComparisonOperator, error) {
 	switch op {
 	case primitives.Eq:
-		return ComparisonOperator_COMPARISON_OPERATOR_EQ
+		return ComparisonOperator_COMPARISON_OPERATOR_EQ, nil
 	case primitives.Neq:
-		return ComparisonOperator_COMPARISON_OPERATOR_NEQ
+		return ComparisonOperator_COMPARISON_OPERATOR_NEQ, nil
 	case primitives.Gt:
-		return ComparisonOperator_COMPARISON_OPERATOR_GT
+		return ComparisonOperator_COMPARISON_OPERATOR_GT, nil
 	case primitives.Lt:
-		return ComparisonOperator_COMPARISON_OPERATOR_LT
+		return ComparisonOperator_COMPARISON_OPERATOR_LT, nil
 	case primitives.Gte:
-		return ComparisonOperator_COMPARISON_OPERATOR_GTE
+		return ComparisonOperator_COMPARISON_OPERATOR_GTE, nil
 	case primitives.Lte:
-		return ComparisonOperator_COMPARISON_OPERATOR_LTE
+		return ComparisonOperator_COMPARISON_OPERATOR_LTE, nil
 	default:
-		return ComparisonOperator_COMPARISON_OPERATOR_EQ
+		return 0, fmt.Errorf("unknown comparison operator: %s", op)
 	}
 }
 
 // ConvertValueComparatorsFromProto converts proto ValueComparator slice to primitives.ValueComparator slice
-func ConvertValueComparatorsFromProto(comparers []*ValueComparator) []primitives.ValueComparator {
+func ConvertValueComparatorsFromProto(comparers []*ValueComparator) ([]primitives.ValueComparator, error) {
 	if comparers == nil {
-		return nil
+		return nil, nil
 	}
 	result := make([]primitives.ValueComparator, len(comparers))
 	for i, c := range comparers {
 		if c != nil {
+			operator, err := ConvertComparisonOperatorFromProto(c.Operator)
+			if err != nil {
+				return nil, fmt.Errorf("failed to convert comparison operator: %w", err)
+			}
 			result[i] = primitives.ValueComparator{
 				Value:    c.Value, // []byte is compatible with any
-				Operator: ConvertComparisonOperatorFromProto(c.Operator),
+				Operator: operator,
 			}
 		}
 	}
-	return result
+	return result, nil
 }
 
 // ConvertValueComparatorsToProto converts primitives.ValueComparator slice to proto ValueComparator slice
-func ConvertValueComparatorsToProto(comparers []primitives.ValueComparator) []*ValueComparator {
+func ConvertValueComparatorsToProto(comparers []primitives.ValueComparator) ([]*ValueComparator, error) {
 	if comparers == nil {
-		return nil
+		return nil, nil
 	}
 	result := make([]*ValueComparator, len(comparers))
 	for i, c := range comparers {
@@ -77,12 +81,16 @@ func ConvertValueComparatorsToProto(comparers []primitives.ValueComparator) []*V
 		if b, ok := c.Value.([]byte); ok {
 			valueBytes = b
 		}
+		operator, err := ConvertComparisonOperatorToProto(c.Operator)
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert comparison operator: %w", err)
+		}
 		result[i] = &ValueComparator{
 			Value:    valueBytes,
-			Operator: ConvertComparisonOperatorToProto(c.Operator),
+			Operator: operator,
 		}
 	}
-	return result
+	return result, nil
 }
 
 // ConvertLogFromProto converts a proto Log to typesolana.Log

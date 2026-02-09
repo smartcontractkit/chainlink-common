db7f9fb0d396013e6fe1d853b95f5b221591c56b
hendoxc <hagen.henderson@smartcontract.com>
2026-02-09 10:39:40 -0700

    Removes unused functions
    adds comment

--- pkg/chipingress/batch/stop_chan.go
+++ pkg/chipingress/batch/stop_chan.go
@@ -5,19 +5,13 @@ import (
 	"time"
 )
 
+/*
+NOTE:This is a copy of the services/stop.go file to avoid a circular dependency on the services package.
+*/
+
 // A stopCh signals when some work should stop.
 // Use StopChanR if you already have a read only <-chan.
 type stopCh chan struct{}
-
-// NewCtx returns a background [context.Context] that is cancelled when StopChan is closed.
-func (s stopCh) NewCtx() (context.Context, context.CancelFunc) {
-	return stopRchan((<-chan struct{})(s)).NewCtx()
-}
-
-// Ctx cancels a [context.Context] when StopChan is closed.
-func (s stopCh) Ctx(ctx context.Context) (context.Context, context.CancelFunc) {
-	return stopRchan((<-chan struct{})(s)).Ctx(ctx)
-}
 
 // CtxWithTimeout cancels a [context.Context] when StopChan is closed.
 func (s stopCh) CtxWithTimeout(timeout time.Duration) (context.Context, context.CancelFunc) {
@@ -33,21 +27,6 @@ func (s stopCh) CtxCancel(ctx context.Context, cancel context.CancelFunc) (conte
 // A stopRchan signals when some work should stop.
 // This is a receive-only version of StopChan, for casting an existing <-chan.
 type stopRchan <-chan struct{}
-
-// NewCtx returns a background [context.Context] that is cancelled when StopChan is closed.
-func (s stopRchan) NewCtx() (context.Context, context.CancelFunc) {
-	return s.Ctx(context.Background())
-}
-
-// Ctx cancels a [context.Context] when StopChan is closed.
-func (s stopRchan) Ctx(ctx context.Context) (context.Context, context.CancelFunc) {
-	return s.CtxCancel(context.WithCancel(ctx))
-}
-
-// CtxWithTimeout cancels a [context.Context] when StopChan is closed.
-func (s stopRchan) CtxWithTimeout(timeout time.Duration) (context.Context, context.CancelFunc) {
-	return s.CtxCancel(context.WithTimeout(context.Background(), timeout))
-}
 
 // CtxCancel cancels a [context.Context] when StopChan is closed.
 // Returns ctx and cancel unmodified, for convenience.

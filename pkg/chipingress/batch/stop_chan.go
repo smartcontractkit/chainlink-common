0d5f03b017626bfb05650ce2ec37948533314ed0
hendoxc <hagen.henderson@smartcontract.com>
2026-02-11 08:09:42 -0700

    Removes unused code

--- pkg/chipingress/batch/stop_chan.go
+++ pkg/chipingress/batch/stop_chan.go
@@ -21,16 +21,6 @@ func (s stopCh) CtxWithTimeout(timeout time.Duration) (context.Context, context.
 // CtxCancel cancels a [context.Context] when StopChan is closed.
 // Returns ctx and cancel unmodified, for convenience.
 func (s stopCh) CtxCancel(ctx context.Context, cancel context.CancelFunc) (context.Context, context.CancelFunc) {
-	return stopRchan((<-chan struct{})(s)).CtxCancel(ctx, cancel)
-}
-
-// A stopRchan signals when some work should stop.
-// This is a receive-only version of StopChan, for casting an existing <-chan.
-type stopRchan <-chan struct{}
-
-// CtxCancel cancels a [context.Context] when StopChan is closed.
-// Returns ctx and cancel unmodified, for convenience.
-func (s stopRchan) CtxCancel(ctx context.Context, cancel context.CancelFunc) (context.Context, context.CancelFunc) {
 	go func() {
 		select {
 		case <-s:

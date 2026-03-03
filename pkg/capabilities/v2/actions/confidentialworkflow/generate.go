2879e49d71bdcdd2aff3867d685a76ee77ed845a
Tejaswi Nadahalli <tejaswi.nadahalli@smartcontract.com>
2026-03-03 02:01:51 +0100

    Add confidential-workflows capability codegen (#1851)
    * Add confidential-workflows capability codegen
    
    Generated Go types and server from the new confidential-workflows
    proto in chainlink-protos. Types: SecretIdentifier, WorkflowExecution,
    ConfidentialWorkflowRequest, ConfidentialWorkflowResponse. Server
    implements ClientCapability interface with Execute method.
    
    * Bump chainlink-protos to v1alpha.22, regen confidential-workflows
    
    Removes owner field from SecretIdentifier per reviewer feedback.
    
    * Run make gomodtidy
    
    * Add enclaverelay package with relay DON protocol types
    
    Shared JSON-RPC wire types for the enclave <-> relay DON protocol,
    enabling both confidential-compute and chainlink to use the same
    struct definitions.
    
    * Rename enclaverelay to confidentialrelay
    
    Avoid exposing "enclave" as an implementation detail in
    the public API surface. Also update method/domain prefixes
    from "enclave." to "confidential.".

---
+++ pkg/capabilities/v2/actions/confidentialworkflow/generate.go
@@ -0,0 +1,2 @@
+//go:generate go run ../../gen --pkg=github.com/smartcontractkit/chainlink-common/pkg/capabilities/v2/actions/confidentialworkflow --file=capabilities/compute/confidentialworkflow/v1alpha/client.proto
+package confidentialworkflow

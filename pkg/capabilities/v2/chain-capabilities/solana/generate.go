b16a881711e7246669a3da8539da2c2f868f7696
Tejaswi Nadahalli <tejaswi.nadahalli@smartcontract.com>
2026-03-23 15:22:41 +0100

    Bump chainlink-protos to cre/go/v1alpha.26, remove dead solana chain-capability
    Solana protos were removed from chainlink-protos in #300. The generated
    code and generate.go in chain-capabilities/solana referenced protos that
    no longer exist, breaking make generate for anyone using current protos.

--- pkg/capabilities/v2/chain-capabilities/solana/generate.go
+++
@@ -1,2 +0,0 @@
-//go:generate go run ../../gen --pkg=github.com/smartcontractkit/chainlink-common/pkg/capabilities/v2/chain-capabilities/solana --file=capabilities/blockchain/solana/v1alpha/client.proto
-package solana

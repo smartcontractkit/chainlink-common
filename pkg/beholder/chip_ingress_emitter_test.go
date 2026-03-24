cc4cc197c2afdf6d3063f0de58abe45fa4850cfb
Tarcisio Ferraz <tarcisio.ferraz@smartcontract.com>
2026-03-24 10:41:19 -0300

    beholder - batch emit events

--- pkg/beholder/chip_ingress_emitter_test.go
+++ pkg/beholder/chip_ingress_emitter_test.go
@@ -190,7 +190,7 @@ func TestExtractSourceAndType(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			domain, entity, err := beholder.ExtractSourceAndType(tt.attrs...)
+			domain, entity, err := beholder.ExtractSourceAndType(beholder.ExtractAttributes(tt.attrs))
 
 			if tt.wantErr {
 				if err == nil {

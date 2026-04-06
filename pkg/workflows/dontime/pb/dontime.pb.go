ff66aade9f57c558e4e559addddd7825c7046b71
Bolek <1416262+bolekk@users.noreply.github.com>
2026-04-06 01:28:20 -0700

    [DONTime] Rollout flag for pruning fix (#1963)

--- pkg/workflows/dontime/pb/dontime.pb.go
+++ pkg/workflows/dontime/pb/dontime.pb.go
@@ -22,11 +22,14 @@ const (
 )
 
 type Observation struct {
-	state         protoimpl.MessageState `protogen:"open.v1"`
-	Timestamp     int64                  `protobuf:"varint,1,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
-	Requests      map[string]int64       `protobuf:"bytes,2,rep,name=requests,proto3" json:"requests,omitempty" protobuf_key:"bytes,1,opt,name=key" protobuf_val:"varint,2,opt,name=value"`
-	unknownFields protoimpl.UnknownFields
-	sizeCache     protoimpl.SizeCache
+	state     protoimpl.MessageState `protogen:"open.v1"`
+	Timestamp int64                  `protobuf:"varint,1,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
+	Requests  map[string]int64       `protobuf:"bytes,2,rep,name=requests,proto3" json:"requests,omitempty" protobuf_key:"bytes,1,opt,name=key" protobuf_val:"varint,2,opt,name=value"`
+	// Flag to roll out execution pruning fix.
+	// TODO(CRE-2497): Remove after rollout.
+	PruneExecutions bool `protobuf:"varint,3,opt,name=prune_executions,json=pruneExecutions,proto3" json:"prune_executions,omitempty"`
+	unknownFields   protoimpl.UnknownFields
+	sizeCache       protoimpl.SizeCache
 }
 
 func (x *Observation) Reset() {
@@ -71,6 +74,13 @@ func (x *Observation) GetRequests() map[string]int64 {
 		return x.Requests
 	}
 	return nil
+}
+
+func (x *Observation) GetPruneExecutions() bool {
+	if x != nil {
+		return x.PruneExecutions
+	}
+	return false
 }
 
 type Observations struct {
@@ -218,10 +228,11 @@ var File_dontime_proto protoreflect.FileDescriptor
 
 const file_dontime_proto_rawDesc = "" +
 	"\n" +
-	"\rdontime.proto\"\xa0\x01\n" +
+	"\rdontime.proto\"\xcb\x01\n" +
 	"\vObservation\x12\x1c\n" +
 	"\ttimestamp\x18\x01 \x01(\x03R\ttimestamp\x126\n" +
-	"\brequests\x18\x02 \x03(\v2\x1a.Observation.RequestsEntryR\brequests\x1a;\n" +
+	"\brequests\x18\x02 \x03(\v2\x1a.Observation.RequestsEntryR\brequests\x12)\n" +
+	"\x10prune_executions\x18\x03 \x01(\bR\x0fpruneExecutions\x1a;\n" +
 	"\rRequestsEntry\x12\x10\n" +
 	"\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n" +
 	"\x05value\x18\x02 \x01(\x03R\x05value:\x028\x01\"@\n" +

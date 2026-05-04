b26295d94017d68220bb22f8a95ccad3efcc52fa
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2026-05-04 10:50:19 -0400

    lint

--- pkg/capabilities/base_trigger_cre.go
+++ pkg/capabilities/base_trigger_cre.go
@@ -41,5 +41,5 @@ func NewBaseTriggerCapabilityWithCRESettings[T proto.Message](
 	if err := ValidateBaseTriggerRetryInterval(ctx, getter); err != nil {
 		return nil, err
 	}
-	return NewBaseTriggerCapability(store, newMsg, lggr, capabilityID, 0, 0, 0, getter), nil
+	return NewBaseTriggerCapability(store, newMsg, lggr, capabilityID, 0, getter), nil
 }

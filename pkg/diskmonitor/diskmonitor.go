80b8880585f547a81231628d2d262a91e635ad6c
mchain0 <maciej.wisniewski@smartcontract.com>
2026-05-14 14:12:41 +0200

    cre-4340: common diskmonitor

---
+++ pkg/diskmonitor/diskmonitor.go
@@ -0,0 +1,80 @@
+package diskmonitor
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"go.opentelemetry.io/otel/metric"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/beholder"
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+	"github.com/smartcontractkit/chainlink-common/pkg/services"
+)
+
+type int64Gauge interface {
+	Record(ctx context.Context, value int64, options ...metric.RecordOption)
+}
+
+// DiskMonitor measures dirPath on a fixed interval and records total file bytes to [GaugeVaultDiskUsageBytes].
+type DiskMonitor struct {
+	services.Service
+
+	eng          *services.Engine
+	tickInterval time.Duration
+	lggr         logger.Logger
+	sizeOfDir    func() (int64, error)
+	gauge        int64Gauge
+}
+
+// NewDiskMonitor returns a [DiskMonitor] for dirPath using gaugeName at tickInterval.
+func NewDiskMonitor(lggr logger.Logger, dirPath string, gaugeName string, tickInterval time.Duration) (*DiskMonitor, error) {
+	g, err := beholder.GetMeter().Int64Gauge(gaugeName)
+	if err != nil {
+		return nil, fmt.Errorf("int64 gauge %q: %w", gaugeName, err)
+	}
+
+	dm := &DiskMonitor{
+		gauge:        g,
+		tickInterval: tickInterval,
+		lggr: logger.With(
+			logger.Named(lggr, "DiskMonitor"),
+			"dirPath", dirPath,
+		),
+		sizeOfDir: func() (int64, error) {
+			var totalSize int64
+			walkErr := filepath.Walk(dirPath, func(_ string, info os.FileInfo, ierr error) error {
+				if ierr == nil && !info.IsDir() {
+					totalSize += info.Size()
+				}
+				return nil
+			})
+			return totalSize, walkErr
+		},
+	}
+
+	dm.Service, dm.eng = services.Config{
+		Name:  "DiskMonitor",
+		Start: dm.start,
+	}.NewServiceEngine(lggr)
+	return dm, nil
+}
+
+func (dm *DiskMonitor) start(ctx context.Context) error {
+	ticker := services.TickerConfig{}.NewTicker(dm.tickInterval)
+	dm.eng.GoTick(ticker, dm.emitDirSizeMetric)
+	return nil
+}
+
+func (dm *DiskMonitor) emitDirSizeMetric(ctx context.Context) {
+	totalSize, err := dm.sizeOfDir()
+	if err != nil {
+		dm.lggr.Errorw("Failed to measure vault directory size", "error", err)
+		return
+	}
+
+	dm.lggr.Debugw("Emitting vault directory size metric", "sizeBytes", totalSize)
+	dm.gauge.Record(ctx, totalSize)
+}

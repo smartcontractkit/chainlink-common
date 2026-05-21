4abd11f80654c02215d0b00a8fd55277f50b51a6
github-action-benchmark <github@users.noreply.github.com>
2026-05-21 03:19:00 +0000

    add Benchmark (go) benchmark result for b805098eaee97bf27f1a7665fd2eb8954412ca01

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779307164058,
+  "lastUpdate": 1779333540204,
   "repoUrl": "https://github.com/smartcontractkit/chainlink-common",
   "entries": {
     "Benchmark": [
@@ -47460,6 +47460,66 @@ window.BENCHMARK_DATA = {
             "value": 118214,
             "unit": "ns/op",
             "extra": "9910 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "1416262+bolekk@users.noreply.github.com",
+            "name": "Bolek",
+            "username": "bolekk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b805098eaee97bf27f1a7665fd2eb8954412ca01",
+          "message": "[LLO] Extra validations in consensus cap plugin (#2079)",
+          "timestamp": "2026-05-21T03:07:31Z",
+          "tree_id": "2390f03744d420abf05f7b57be3860401caf5c94",
+          "url": "https://github.com/smartcontractkit/chainlink-common/commit/b805098eaee97bf27f1a7665fd2eb8954412ca01"
+        },
+        "date": 1779333538437,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkKeystore_Sign/nop/in-process",
+            "value": 359.9,
+            "unit": "ns/op",
+            "extra": "3327588 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/nop/out-of-process",
+            "value": 88038,
+            "unit": "ns/op",
+            "extra": "13542 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/hex/in-process",
+            "value": 409.3,
+            "unit": "ns/op",
+            "extra": "2924976 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/hex/out-of-process",
+            "value": 87197,
+            "unit": "ns/op",
+            "extra": "13798 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/ed25519/in-process",
+            "value": 26923,
+            "unit": "ns/op",
+            "extra": "43192 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkKeystore_Sign/ed25519/out-of-process",
+            "value": 135755,
+            "unit": "ns/op",
+            "extra": "8421 times\n4 procs"
           }
         ]
       }

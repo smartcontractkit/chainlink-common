0b659faac69285155ff1c81645df08c2bbc54cee
pavel-raykov <pavel.raykov@smartcontract.com>
2026-06-09 15:16:17 +0200

    Minor.

--- keystore/ocr3/utils.go
+++ keystore/ocr3/utils.go
@@ -9,7 +9,7 @@ import (
 // OnchainKeyring2Genericless is a genericless counterpart of ocr3types.OnchainKeyring2. If generic RI does not matter,
 // then it is more convenient to implement this interface and then use OnchainKeyring2ToGenericAdapter to use it as
 // ocr3types.OnchainKeyring2.
-type OnchainKeyring2Genericless interface {
+type OnchainKeyring2 interface {
 	Sign(configDigest ocrtypes.ConfigDigest, seqNr uint64, report types.Report) (signature []byte, err error)
 	Verify(publicKey ocrtypes.OnchainPublicKey, configDigest ocrtypes.ConfigDigest, seqNr uint64, report types.Report, signature []byte) bool
 	Has(publicKey ocrtypes.OnchainPublicKey) bool
@@ -17,28 +17,32 @@ type OnchainKeyring2Genericless interface {
 	DebugIdentifier() string
 }
 
-var _ ocr3types.OnchainKeyring2[struct{}] = &OnchainKeyring2ToGenericAdapter[struct{}]{}
+var _ ocr3types.OnchainKeyring2[struct{}] = &onchainKeyring2[struct{}]{}
 
-type OnchainKeyring2ToGenericAdapter[RI any] struct {
-	K OnchainKeyring2Genericless
+type onchainKeyring2[RI any] struct {
+	k OnchainKeyring2
 }
 
-func (o *OnchainKeyring2ToGenericAdapter[RI]) Sign(c ocrtypes.ConfigDigest, seqNr uint64, r ocr3types.ReportWithInfo[RI]) (signature []byte, err error) {
-	return o.K.Sign(c, seqNr, r.Report)
+func (o *onchainKeyring2[RI]) Sign(c ocrtypes.ConfigDigest, seqNr uint64, r ocr3types.ReportWithInfo[RI]) (signature []byte, err error) {
+	return o.k.Sign(c, seqNr, r.Report)
 }
 
-func (o *OnchainKeyring2ToGenericAdapter[RI]) Verify(pk ocrtypes.OnchainPublicKey, c ocrtypes.ConfigDigest, seqNr uint64, r ocr3types.ReportWithInfo[RI], signature []byte) bool {
-	return o.K.Verify(pk, c, seqNr, r.Report, signature)
+func (o *onchainKeyring2[RI]) Verify(pk ocrtypes.OnchainPublicKey, c ocrtypes.ConfigDigest, seqNr uint64, r ocr3types.ReportWithInfo[RI], signature []byte) bool {
+	return o.k.Verify(pk, c, seqNr, r.Report, signature)
 }
 
-func (o *OnchainKeyring2ToGenericAdapter[RI]) Has(key ocrtypes.OnchainPublicKey) bool {
-	return o.K.Has(key)
+func (o *onchainKeyring2[RI]) Has(key ocrtypes.OnchainPublicKey) bool {
+	return o.k.Has(key)
 }
 
-func (o *OnchainKeyring2ToGenericAdapter[RI]) MaxSignatureLength() int {
-	return o.K.MaxSignatureLength()
+func (o *onchainKeyring2[RI]) MaxSignatureLength() int {
+	return o.k.MaxSignatureLength()
 }
 
-func (o *OnchainKeyring2ToGenericAdapter[RI]) DebugIdentifier() string {
-	return o.K.DebugIdentifier()
+func (o *onchainKeyring2[RI]) DebugIdentifier() string {
+	return o.k.DebugIdentifier()
+}
+
+func AsOCR3OnchainKeyring2[RI any](k OnchainKeyring2) ocr3types.OnchainKeyring2[RI] {
+	return &onchainKeyring2[RI]{k}
 }

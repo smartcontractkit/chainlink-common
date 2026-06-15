c81fae9154fa0b33921fbc7e2c162a37f7a845b7
cawthorne <cawthornegd@gmail.com>
2026-06-15 15:37:34 +0100

    Document why curveOrder is defined locally in starkkey.
    starknet.go v0.17 no longer exports the curve order, but we still need it for key generation and canonical OCR2 signatures.

--- keystore/corekeys/starkkey/curve_compat.go
+++ keystore/corekeys/starkkey/curve_compat.go
@@ -2,7 +2,11 @@ package starkkey
 
 import "math/big"
 
-// STARK curve subgroup order.
+// curveOrder is the STARK curve subgroup order (N).
+//
+// starknet.go v0.17 dropped the exported curve.Curve type that previously
+// exposed this value. We still need N for private-key sampling in
+// GenerateKey and for OCR2 canonical signatures (s <= N/2).
 var curveOrder = func() *big.Int {
 	n, ok := new(big.Int).SetString("3618502788666131213697322783095070105526743751716087489154079457884512865583", 10)
 	if !ok {

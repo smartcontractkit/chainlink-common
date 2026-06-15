a6dd71fe287f3c937f92c3e43f3a21a22a68b51e
cawthorne <cawthornegd@gmail.com>
2026-06-15 16:30:20 +0100

    Expand curveOrder comment with v0.17 migration context.
    Document gnark-crypto migration rationale and link to the Starknet protocol constant.

--- keystore/corekeys/starkkey/curve_compat.go
+++ keystore/corekeys/starkkey/curve_compat.go
@@ -4,9 +4,12 @@ import "math/big"
 
 // curveOrder is the STARK curve subgroup order (N).
 //
-// starknet.go v0.17 dropped the exported curve.Curve type that previously
-// exposed this value. We still need N for private-key sampling in
-// GenerateKey and for OCR2 canonical signatures (s <= N/2).
+// starknet.go v0.17 dropped the exported curve.Curve type when the curve pkg
+// migrated to gnark-crypto (https://github.com/NethermindEth/starknet.go/issues/710).
+// We still need N for private-key sampling in GenerateKey and for OCR2 canonical
+// signatures (s <= N/2).
+//
+// https://docs.starknet.io/learn/protocol/cryptography
 var curveOrder = func() *big.Int {
 	n, ok := new(big.Int).SetString("3618502788666131213697322783095070105526743751716087489154079457884512865583", 10)
 	if !ok {

e2f783fb35f644af5973ea93c9f91f883baf49b3
Ryan Tinianov <tinianov@live.com>
2026-09-10 15:34:21 -0400

    Fix typo

--- pkg/config/duration.go
+++ pkg/config/duration.go
@@ -25,7 +25,7 @@ func MustNewDuration(d time.Duration) *Duration {
 	return &rv
 }
 
-// ParseDuration uses [time.ParseDuration] to parse the time (see it's documentation for formatting)
+// ParseDuration uses [time.ParseDuration] to parse the time (see its documentation for formatting)
 // and ensures the result is non-negative.
 func ParseDuration(s string) (Duration, error) {
 	d, err := time.ParseDuration(s)
